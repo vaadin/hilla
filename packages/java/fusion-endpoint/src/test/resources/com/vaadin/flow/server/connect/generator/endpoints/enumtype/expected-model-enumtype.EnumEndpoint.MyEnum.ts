989c527c77eee88918e15e5606a8c28a3e6dd55d
Vlad Rindevich <vladrin@vaadin.com>
2021-04-30 16:08:33 +0300

    feat: use regular string enum instead of a const enum (#10853)
    Given a Java enum as
    ```java
    pubic enum Environment {
      DEV, TEST, PROD
    }
    ```
    the generated Typescript source will be
    ```typescript
    enum Environment {
      DEV = "DEV",
      TEST = "TEST",
      PROD = "PROD"
    }
    ```
    which translates to JavaScript in the following way:
    ```javascript
    var Environment;
    
    (function (Environment) {
      Environment["DEV"] = "DEV";
      Environment["TEST"] = "TEST";
      Environment["PROD"] = "PROD";
    })(Environment || (Environment = {}));
    ```
    So now it is possible to do any object action with the final `Environment` enum:
    ```javascript
    Object.keys(Environment).forEach(enumValue => console.log(enumValue));
    ```

--- packages/java/fusion-endpoint/src/test/resources/com/vaadin/flow/server/connect/generator/endpoints/enumtype/expected-model-enumtype.EnumEndpoint.MyEnum.ts
+++ packages/java/fusion-endpoint/src/test/resources/com/vaadin/flow/server/connect/generator/endpoints/enumtype/expected-model-enumtype.EnumEndpoint.MyEnum.ts
@@ -2,7 +2,7 @@
  * This module is generated from com.vaadin.flow.server.connect.generator.endpoints.enumtype.EnumEndpoint.MyEnum.
  * All changes to this file are overridden. Please consider to make changes in the corresponding Java file if necessary.
  */
-const enum MyEnum {
+enum MyEnum {
   ENUM1 = 'ENUM1',
   ENUM2 = 'ENUM2',
   ENUM_2 = 'ENUM_2',
