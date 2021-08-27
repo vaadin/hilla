237e6f5fffca8031c64ac582da0f7c252ec6c58d
Haijian Wang <30408303+haijian-vaadin@users.noreply.github.com>
2021-08-27 08:54:29 +0300

    test: add mobx dep via @NpmPackage vaadin/spring#885

--- packages/java/hilla-tests/hilla-spring-tests/test-spring-security-hilla/src/main/java/com/vaadin/flow/spring/fusionsecurity/Application.java
+++ packages/java/hilla-tests/hilla-spring-tests/test-spring-security-hilla/src/main/java/com/vaadin/flow/spring/fusionsecurity/Application.java
@@ -1,9 +1,15 @@
 package com.vaadin.flow.spring.fusionsecurity;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@NpmPackage(value = "@adobe/lit-mobx",
+            version = "2.0.0-rc.4")
+@NpmPackage(value = "mobx",
+            version = "^6.1.5")
 public class Application {
 
     public static void main(String[] args) {
