34712615a343a9c7470af920436ecc4e4ae80662
Vlad Rindevich <vladrin@vaadin.com>
2021-08-31 18:52:36 +0300

    chore(frontend): add package registrations

--- frontend/packages/common-frontend/src/index.ts
+++ frontend/packages/common-frontend/src/index.ts
@@ -1,2 +1,10 @@
 export * from './ConnectionState.js';
 export * from './ConnectionIndicator.js';
+
+const $wnd = window as any;
+$wnd.Vaadin = $wnd.Vaadin || {};
+$wnd.Vaadin.registrations = $wnd.Vaadin.registrations || [];
+$wnd.Vaadin.registrations.push({
+  is: '@vaadin/form',
+  version: /* updated-by-script */ '8.0-SNAPSHOT',
+});
