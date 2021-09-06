cd468b8917f39fcb9209f1fc4f6bb34fcce88816
Vlad Rindevich <vladrin@vaadin.com>
2021-09-06 14:56:07 +0300

    chore(frontend): more fixes in order to make the release runnable (#90)
    * chore(frontend): rename GITHUB_TOKEN -> RELEASE_TOKEN in case GitHub overwrites that env variable
    
    * chore(frontend): fail script if any error happens or any variable is unset
    
    * chore(frontend): address review comments
    
    * chore(frontend): fix secret

--- frontend/scripts/bump.sh
+++ frontend/scripts/bump.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+# Fails the script if any command failed or any variable is unset
+set -eu
+
 branch=main
 
 # shellcheck disable=SC2139
 alias ghr="curl https://api.github.com/repos/$REPO/branches/$branch/protection \
   -H 'Accept: application/vnd.github.v3+json' \
-  -H 'Authorization: token $GITHUB_TOKEN' \
+  -H 'Authorization: token $GITHUB_RELEASE_TOKEN' \
   -s"
 
 node scripts/update-package-versions.js "$VERSION_TAG"
@@ -23,6 +26,6 @@ remapped=$(node scripts/protection-remap.js "$protection_config")
 
 ghr -X PUT -d "$(echo "$remapped" | sed '$s/"enforce_admins":true/"enforce_admins":false/')" > /dev/null
 
-git push https://vaadin-bot:"$GITHUB_TOKEN"@github.com/"$REPO".git HEAD:$branch
+git push https://vaadin-bot:"$GITHUB_RELEASE_TOKEN"@github.com/"$REPO".git HEAD:$branch
 
 ghr -X PUT -d "$remapped" > /dev/null
