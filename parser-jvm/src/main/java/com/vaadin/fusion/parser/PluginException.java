3556488a3f4499fdcf254bee40dd309b28078d1e
Vlad Rindevich <vladrin@vaadin.com>
2021-10-05 14:35:18 +0300

    refactor(parser-jvm): remove unused PluginException

--- parser-jvm/src/main/java/com/vaadin/fusion/parser/PluginException.java
+++
@@ -1,7 +0,0 @@
-package com.vaadin.fusion.parser;
-
-public class PluginException extends RuntimeException {
-    public PluginException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
