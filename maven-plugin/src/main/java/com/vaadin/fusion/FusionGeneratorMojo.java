d7bad1a2f985ddb274fe4bae78b7cc060f9b8f81
Vlad Rindevich <vladrin@vaadin.com>
2021-10-07 10:53:09 +0300

    refactor(maven-plugin): update to the latest parser-jvm version

--- maven-plugin/src/main/java/com/vaadin/fusion/FusionGeneratorMojo.java
+++ maven-plugin/src/main/java/com/vaadin/fusion/FusionGeneratorMojo.java
@@ -1,44 +1,31 @@
 package com.vaadin.fusion;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.Map;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+
+import com.vaadin.fusion.parser.Parser;
+import com.vaadin.fusion.parser.ParserConfig;
 
 /**
  * Fusion plugin for Maven. Handles loading the parser and its plugins.
  */
 @Mojo(name = "fusion-generator", defaultPhase = LifecyclePhase.PROCESS_CLASSES)
 public class FusionGeneratorMojo extends AbstractMojo {
-  @Parameter
-  private Map<String, JVMParserMojoPlugin> plugins;
-  @Parameter(defaultValue = "${project}", required = true, readonly = true)
-  private MavenProject project;
+    @Parameter(required = true, readonly = true)
+    private String configPath;
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    private MavenProject project;
 
-  private FusionJVMParser parser;
-
-  public FusionGeneratorMojo() {
-    parser = new FusionJVMParser(project.getCompileClasspathElements());
-  }
-
-  public void execute() throws MojoExecutionException {
-    ClassLoader loader = getClass().getClassLoader();
-
-    try {
-      for (JVMParserMojoPlugin mojoPlugin : plugins.values()) {
-        Class<ParserJVMPlugin> pluginClass = loader.loadClass(mojoPlugin.getName());
-        ParserJVMPlugin plugin = pluginClass.getDeclaredConstructor().newInstance();
-        parser.addPlugin(plugin);
-      }
-
-      parser.execute();
-    } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
-      throw new MojoExecutionException("Loading plugin is failed", e);
+    public void execute() {
+        String classPath = (String) project.getCompileClasspathElements().stream().collect(Collectors.joining(";"));
+        ParserConfig config = ParserConfig.parse(Paths.get(configPath).toFile()).classPath(classPath);
+        Parser parser = new Parser(config);
+        parser.execute();
     }
-  }
 }
