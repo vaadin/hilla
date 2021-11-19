ff1d9dd84952c7ad9dc4c1c232f8b64fb92c287a
Vlad Rindevich <vladrin@vaadin.com>
2021-11-19 20:11:59 +0300

    refactor(java/parser-jvm): improve null security in config

--- packages/java/parser-jvm/src/main/java/com/vaadin/fusion/parser/core/ParserConfig.java
+++ packages/java/parser-jvm/src/main/java/com/vaadin/fusion/parser/core/ParserConfig.java
@@ -1,6 +1,5 @@
 package com.vaadin.fusion.parser.core;
 
-import javax.annotation.Nonnull;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -10,8 +9,10 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import javax.annotation.Nonnull;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -53,9 +54,10 @@ public class ParserConfig {
         }
 
         return Objects.equals(classPath, ((ParserConfig) another).classPath)
-            && Objects.equals(endpointAnnotationName, ((ParserConfig) another).endpointAnnotationName)
-            && Objects.equals(openAPI, ((ParserConfig) another).openAPI)
-            && Objects.equals(plugins, ((ParserConfig) another).plugins);
+                && Objects.equals(endpointAnnotationName,
+                        ((ParserConfig) another).endpointAnnotationName)
+                && Objects.equals(openAPI, ((ParserConfig) another).openAPI)
+                && Objects.equals(plugins, ((ParserConfig) another).plugins);
     }
 
     public static class Builder {
@@ -97,7 +99,8 @@ public class ParserConfig {
 
         @Nonnull
         public Builder endpointAnnotation(
-            @Nonnull String annotationFullyQualifiedName, boolean override) {
+                @Nonnull String annotationFullyQualifiedName,
+                boolean override) {
             Objects.requireNonNull(annotationFullyQualifiedName);
 
             actions.add(config -> {
@@ -110,19 +113,20 @@ public class ParserConfig {
 
         @Nonnull
         public Builder endpointAnnotation(
-            @Nonnull String annotationQualifiedName) {
+                @Nonnull String annotationQualifiedName) {
             return endpointAnnotation(annotationQualifiedName, true);
         }
 
         @Nonnull
         public Builder openAPISpec(@Nonnull String src, @Nonnull String ext) {
-            openAPISpec = new FileSpec(Objects.requireNonNull(src), Objects.requireNonNull(ext));
+            openAPISpec = new FileSpec(Objects.requireNonNull(src),
+                    Objects.requireNonNull(ext));
             return this;
         }
 
         @Nonnull
         public Builder plugins(@Nonnull Collection<String> plugins,
-                               boolean override) {
+                boolean override) {
             Objects.requireNonNull(plugins);
 
             actions.add(config -> {
@@ -140,25 +144,36 @@ public class ParserConfig {
 
         public ParserConfig finish() {
             var config = new ParserConfig();
-            var openAPIParser = new OpenAPIParser();
 
+            config.openAPI = prepareOpenAPI();
+
+            actions.forEach(action -> action.accept(config));
+
+            Objects.requireNonNull(config.classPath, "[JVM Parser] classPath is not provided");
+            Objects.requireNonNull(config.endpointAnnotationName, "[JVM Parser] endpointAnnotationName is not provided");
+
+            return config;
+        }
+
+        private OpenAPI prepareOpenAPI() {
             try {
-                String src = new String(Objects.requireNonNull(
-                    getClass().getResourceAsStream("OpenAPIStub.json")).readAllBytes());
+                var parser = new OpenAPIParser();
 
-                openAPIParser.parse(new FileSpec(src, "json"));
+                var src = new String(Objects
+                    .requireNonNull(getClass()
+                        .getResourceAsStream("OpenAPIStub.json"))
+                    .readAllBytes());
+
+                parser.parse(new FileSpec(src, "json"));
 
                 if (openAPISpec != null) {
-                    openAPIParser.parse(openAPISpec);
+                    parser.parse(openAPISpec);
                 }
 
-                config.openAPI = openAPIParser.getValue();
-
-                actions.forEach(action -> action.accept(config));
-
-                return config;
+                return parser.getValue();
             } catch (IOException e) {
-                throw new ParserException("Failed to parse openAPI specification", e);
+                throw new ParserException(
+                    "Failed to parse openAPI specification", e);
             }
         }
     }
@@ -176,7 +191,6 @@ public class ParserConfig {
             return extension;
         }
 
-
         public String getSrc() {
             return src;
         }
@@ -189,22 +203,22 @@ public class ParserConfig {
             return value;
         }
 
-        public void parse(FileSpec spec) throws JsonProcessingException {
+        public void parse(FileSpec spec) throws IOException {
             var mapper = createMapper(spec.getExtension());
             var reader = value != null ? mapper.readerForUpdating(value)
-                : mapper.reader();
-            value = reader.readValue(spec.getSrc());
+                    : mapper.reader();
+            value = reader.readValue(spec.getSrc(), OpenAPI.class);
         }
 
         private ObjectMapper createMapper(String extension) {
             switch (extension) {
-                case "yml":
-                case "yaml":
-                    return Yaml.mapper();
-                case "json":
-                    return Json.mapper();
-                default:
-                    throw new ParserException(String.format(
+            case "yml":
+            case "yaml":
+                return Yaml.mapper();
+            case "json":
+                return Json.mapper();
+            default:
+                throw new ParserException(String.format(
                         "The file format '.%s' is not supported", extension));
             }
         }
