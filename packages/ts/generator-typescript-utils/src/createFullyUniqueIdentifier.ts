868a5fee3abbec57d6bff5d13ac17424966a82da
Vlad Rindevich <vladrin@vaadin.com>
2021-12-10 10:54:55 +0300

    chore(ts): enable "no-bitwise" rule back

--- packages/ts/generator-typescript-utils/src/createFullyUniqueIdentifier.ts
+++ packages/ts/generator-typescript-utils/src/createFullyUniqueIdentifier.ts
@@ -4,6 +4,7 @@ import ts from 'typescript';
 export default function createFullyUniqueIdentifier(name: string, flags?: GeneratedIdentifierFlags) {
   return ts.factory.createUniqueName(
     name,
+    // eslint-disable-next-line no-bitwise
     flags ?? ts.GeneratedIdentifierFlags.Optimistic & ts.GeneratedIdentifierFlags.FileLevel,
   );
 }
