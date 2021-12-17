86d2c92057401aad24922eeae8d50cd23378e9a7
Vlad Rindevich <vladrin@vaadin.com>
2021-12-17 10:42:28 +0300

    refactor(java/parser-jvm-plugin-nonnull): update NonnullPluginConfig

--- packages/java/parser-jvm-plugin-nonnull/src/main/java/com/vaadin/fusion/parser/plugins/nonnull/NonnullPluginConfig.java
+++ packages/java/parser-jvm-plugin-nonnull/src/main/java/com/vaadin/fusion/parser/plugins/nonnull/NonnullPluginConfig.java
@@ -1,19 +1,42 @@
 package com.vaadin.fusion.parser.plugins.nonnull;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
-import com.vaadin.fusion.parser.core.PluginConfiguration;
-import com.vaadin.fusion.parser.utils.ConfigurationList;
+import javax.annotation.Nonnull;
 
-public class NonnullPluginConfig extends ConfigurationList<String>
-        implements PluginConfiguration {
-    public NonnullPluginConfig(Collection<String> use,
-            Collection<String> disable) {
-        super(use, disable);
+import com.vaadin.fusion.parser.core.PluginConfiguration;
+import com.vaadin.fusion.parser.utils.ConfigList;
+
+public class NonnullPluginConfig
+        implements ConfigList<String>, PluginConfiguration {
+    private final Set<String> disable = new HashSet<>();
+    private final boolean disableAllDefaults = false;
+    private final Set<String> use = new HashSet<>();
+
+    public NonnullPluginConfig(@Nonnull Collection<String> use,
+            @Nonnull Collection<String> disable) {
+        this.disable.addAll(disable);
+        this.use.addAll(use);
     }
 
-    static class Processor extends ConfigurationList.Processor<String> {
+    @Override
+    public Collection<String> getDisable() {
+        return disable;
+    }
+
+    @Override
+    public Collection<String> getUse() {
+        return use;
+    }
+
+    @Override
+    public boolean isDisableAllDefaults() {
+        return disableAllDefaults;
+    }
+
+    static class Processor extends ConfigList.Processor<String> {
         private static final Set<String> defaults = Set.of(
                 "javax.annotation.Nonnull", "org.jetbrains.annotations.NotNull",
                 "lombok.NonNull", "androidx.annotation.NonNull",
