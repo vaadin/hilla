c897ca36f2e7adf821c9238eed59e68a4aaeb8a5
Anton Platonov <anton@vaadin.com>
2022-01-07 18:58:33 +0200

    refactor!: rename Fusion to Hilla vaadin/spring#995
    * refactor: update to hilla-endpoint dependency name
    
    * refactor: update hilla-endpoint dependency groupId and version
    
    * refactor: update to hilla package name
    
    * refactor: rename FusionX classnames to HillaX
    
    * chore: rename fusionsecurity to hillasecurity
    
    * chore: rename fusionform to hillaform
    
    * chore: rename -fusion test modules to -hilla
    
    * chore: rename fusion variable names to hilla
    
    * chore: update arbitrary mentions of Fusion to Hilla

--- packages/java/hilla-tests/hilla-spring-tests/test-spring-security-hilla/src/main/java/com/vaadin/flow/spring/fusionsecurity/data/UserInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vaadin.flow.spring.fusionsecurity.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
-
-    public UserInfo findByUsername(String username);
-}
