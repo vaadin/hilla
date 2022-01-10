95c8fad5a1ae30302a222ab89f79d85561b016da
Vlad Rindevich <vladrin@vaadin.com>
2022-01-10 19:20:21 +0300

    test(java/parser-jvm-plugin-backbone): fix failing tests

--- packages/java/parser-jvm-plugin-backbone/src/test/java/com/vaadin/fusion/parser/plugins/backbone/validation/ValidationTest.java
+++ packages/java/parser-jvm-plugin-backbone/src/test/java/com/vaadin/fusion/parser/plugins/backbone/validation/ValidationTest.java
@@ -1,6 +1,6 @@
 package com.vaadin.fusion.parser.plugins.backbone.validation;
 
-import static com.vaadin.fusion.parser.plugins.testutils.OpenAPIAssertions.assertEquals;
+import static com.vaadin.fusion.parser.testutils.OpenAPIAssertions.assertEquals;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -15,16 +15,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vaadin.fusion.parser.core.Parser;
 import com.vaadin.fusion.parser.core.ParserConfig;
 import com.vaadin.fusion.parser.plugins.backbone.BackbonePlugin;
-import com.vaadin.fusion.parser.plugins.testutils.ResourceLoader;
+import com.vaadin.fusion.parser.testutils.ResourceLoader;
 
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.oas.models.OpenAPI;
 
 public class ValidationTest {
     private final ObjectMapper mapper = Json.mapper();
-    private final ResourceLoader resourceLoader = new ResourceLoader(
-            getClass());
+    private final ResourceLoader resourceLoader;
     private Path targetDir;
+
+    {
+        var target = getClass();
+        resourceLoader = new ResourceLoader(target::getResource,
+            target::getProtectionDomain);
+    }
 
     @BeforeEach
     public void setup() throws URISyntaxException {
