54d955f74e364349a033fbc67549477abd16a30c
Vlad Rindevich <vladrin@vaadin.com>
2022-01-31 16:00:54 +0300

    test(ts): update fixtures

--- packages/ts/generator-typescript-plugin-backbone/test/complex-hierarchy/fixtures/ComplexHierarchyEndpoint.snap.ts
+++ packages/ts/generator-typescript-plugin-backbone/test/complex-hierarchy/fixtures/ComplexHierarchyEndpoint.snap.ts
@@ -1,4 +1,4 @@
-import type ComplexHierarchyModel_1 from "./com/vaadin/fusion/parser/plugins/backbone/complexhierarchy/models/ComplexHierarchyModel";
 import client_1 from "./connect-client.default";
+import type ComplexHierarchyModel_1 from "./dev/hilla/parser/plugins/backbone/complexhierarchy/models/ComplexHierarchyModel";
 async function getModel_1(): Promise<ComplexHierarchyModel_1 | undefined> { return client_1.call("ComplexHierarchyEndpoint", "getModel"); }
 export { getModel_1 as getModel };
