2e55420f405e17bec34b03ed59b24666d98f6aac
Artur <artur@vaadin.com>
2022-03-04 14:56:17 +0200

    chore: Remove unused imports (#13213)
    Using find . -type f -name "*.java"|xargs java -jar google-java-format-1.14.0-all-deps.jar --fix-imports-only --skip-sorting-imports  -r

--- packages/java/hilla-tests/hilla-spring-tests/test-spring-security-hilla-urlmapping/src/main/java/com/vaadin/flow/spring/fusionsecurityurlmapping/Application.java
+++ packages/java/hilla-tests/hilla-spring-tests/test-spring-security-hilla-urlmapping/src/main/java/com/vaadin/flow/spring/fusionsecurityurlmapping/Application.java
@@ -5,23 +5,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.Ordered;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.vaadin.flow.spring.fusionsecurity.SecurityConfig;
 
 @SpringBootApplication
 public class Application
