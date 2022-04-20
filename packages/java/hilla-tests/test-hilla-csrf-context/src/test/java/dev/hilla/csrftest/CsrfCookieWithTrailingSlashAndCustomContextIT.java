f9163df467777049d3c9e77b3e1c47bca9fc43b8
Anton Platonov <platosha@gmail.com>
2022-04-20 19:19:38 +0300

    test: add ITs with Spring Boot and endpoints

--- packages/java/hilla-tests/test-hilla-csrf-context/src/test/java/dev/hilla/csrftest/CsrfCookieWithTrailingSlashAndCustomContextIT.java
+++ packages/java/hilla-tests/test-hilla-csrf-context/src/test/java/dev/hilla/csrftest/CsrfCookieWithTrailingSlashAndCustomContextIT.java
@@ -16,9 +16,47 @@
 
 package dev.hilla.csrftest;
 
+import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.flow.testutil.ChromeBrowserTest;
+
 public class CsrfCookieWithTrailingSlashAndCustomContextIT
-        extends CsrfCookieWithTrailingSlashIT {
+        extends ChromeBrowserTest {
+    @Test
+    // https://github.com/vaadin/fusion/issues/105
+    public void should_registerCsrfCookieToContextRoot_whenRequestFromSubViewAndUrlHasTrailingSlash()
+        throws IOException {
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+
+        open();
+
+        URL url = new URL(getTestURL());
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.getContent();
+        // Get CookieStore
+        CookieStore cookieStore = cookieManager.getCookieStore();
+
+        HttpCookie csrfCookie = cookieStore.getCookies().stream()
+            .filter(cookie -> "csrfToken".equals(cookie.getName())).findFirst().get();
+        Assert.assertEquals(getContextPath(), csrfCookie.getPath());
+    }
+
     @Override
+    protected String getTestPath() {
+        return getContextPath() + ("/".equals(getContextPath()) ? "" : "/") +
+            "hello/";
+    }
+
     protected String getContextPath() {
         return "/foo";
     }
