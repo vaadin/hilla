7e1bf52c37551cdeef5b9a549f0c5ec46fc72d19
Luciano Vernaschi <luciano@vaadin.com>
2022-05-12 10:09:42 +0200

    Use SystemUtils to detect OS and remove Linux-related code

--- packages/java/maven-plugin/src/main/java/dev/hilla/maven/GeneratorShellRunner.java
+++ packages/java/maven-plugin/src/main/java/dev/hilla/maven/GeneratorShellRunner.java
@@ -1,5 +1,8 @@
 package dev.hilla.maven;
 
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.maven.plugin.logging.Log;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -7,37 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.apache.maven.plugin.logging.Log;
-
 final class GeneratorShellRunner {
-    private static final boolean IS_MAC;
-    private static final boolean IS_UNIX;
-    private static final boolean IS_WINDOWS;
     private static final Pattern jsonEscapePattern = Pattern
             .compile("[\r\n\b\f\t\"']");
-
-    static {
-        var osName = System.getProperty("os.name").toLowerCase();
-        var isWin = false;
-        var isMac = false;
-        var isUnix = false;
-
-        if (osName.contains("win")) {
-            isWin = true;
-        } else if (osName.contains("mac")) {
-            isMac = true;
-        } else if (osName.contains("nix")) {
-            isUnix = true;
-        } else {
-            throw new GeneratorException(
-                    String.format("Platform '%s' is unsupported",
-                            System.getProperty("os.name")));
-        }
-
-        IS_WINDOWS = isWin;
-        IS_MAC = isMac;
-        IS_UNIX = isUnix;
-    }
 
     private final List<String> arguments = new ArrayList<>();
     private final Log logger;
@@ -45,16 +20,12 @@ final class GeneratorShellRunner {
     public GeneratorShellRunner(File baseDir, Log logger) {
         this.logger = logger;
 
-        if (IS_WINDOWS) {
+        if (SystemUtils.IS_OS_WINDOWS) {
             arguments.add("cmd.exe");
             arguments.add("/c");
-        } else if (IS_UNIX) {
-            // TODO: Mac does not need that. Need to check if Unix does.
-            arguments.add("/bin/bash");
-            arguments.add("-c");
         }
 
-        if (IS_WINDOWS) {
+        if (SystemUtils.IS_OS_WINDOWS) {
             arguments.add(Paths.get(baseDir.getAbsolutePath(), "node_modules",
                     ".bin", "tsgen.cmd").toString());
         } else {
@@ -64,7 +35,7 @@ final class GeneratorShellRunner {
     }
 
     public static String prepareJSONForCLI(String json) {
-        return IS_WINDOWS ? jsonEscapePattern.matcher(json)
+        return SystemUtils.IS_OS_WINDOWS ? jsonEscapePattern.matcher(json)
                 .replaceAll(match -> "\\\\" + match.group(0)) : json;
     }
 
