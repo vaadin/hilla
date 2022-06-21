bacd7da6478059dc721e048527d829e75d318067
Vlad Rindevich <vladrin@vaadin.com>
2022-06-21 18:17:53 +0300

    feat(parser-jvm-utils): add AnnotatedOwnerUtils

---
+++ packages/java/parser-jvm-utils/src/test/java/dev/hilla/parser/utils/annotatedowner/GLOB_Param.java
@@ -0,0 +1,15 @@
+package dev.hilla.parser.utils.annotatedowner;
+
+public class GLOB_Param<T> {
+    class DYN_CSimple {
+    }
+
+    class DYN_CParam<C> {
+    }
+
+    static class ST_CSimple {
+    }
+
+    static class ST_CParam<C> {
+    }
+}
