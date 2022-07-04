cca9744767c4a9e4b597e790ec039e9eecbe4cdc
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2022-07-04 08:18:28 +0200

    feat(generator): add `init` param to generated endpoints (#455)
    * Add `init` param to generated endpoints
    
    * Fix constants
    
    * Rename to `initTypeIdentifier`
    
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>
    
    * Simplify searching in parameter names
    
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>
    
    * Finish renaming
    
    * Extract parameter names list for clarity
    
    * Add missing test description
    
    * Add `static` modifier
    
    * Finish previous commit
    
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>
    Co-authored-by: Anton Platonov <platosha@gmail.com>

--- packages/ts/generator-typescript-plugin-backbone/src/EndpointMethodRequestBodyProcessor.ts
+++ packages/ts/generator-typescript-plugin-backbone/src/EndpointMethodRequestBodyProcessor.ts
@@ -18,54 +18,88 @@ export type EndpointMethodRequestBody = ReadonlyDeep<OpenAPIV3.RequestBodyObject
 export type EndpointMethodRequestBodyProcessingResult = Readonly<{
   parameters: readonly ParameterDeclaration[];
   packedParameters?: ObjectLiteralExpression;
+  initParam: ts.Identifier;
 }>;
 
 export default class EndpointMethodRequestBodyProcessor {
+  static readonly #defaultInitParamName = 'init';
+
   readonly #dependencies: DependencyManager;
   readonly #owner: Plugin;
   readonly #requestBody?: EndpointMethodRequestBody;
+  readonly #initTypeIdentifier: ts.Identifier;
 
   public constructor(
     requestBody: ReadonlyDeep<OpenAPIV3.ReferenceObject | OpenAPIV3.RequestBodyObject> | undefined,
     dependencies: DependencyManager,
     owner: Plugin,
+    initTypeIdentifier: ts.Identifier,
   ) {
     this.#owner = owner;
     this.#dependencies = dependencies;
     this.#requestBody = requestBody ? owner.resolver.resolve(requestBody) : undefined;
+    this.#initTypeIdentifier = initTypeIdentifier;
   }
 
   public process(): EndpointMethodRequestBodyProcessingResult {
     if (!this.#requestBody) {
       return {
-        parameters: [],
+        parameters: [
+          ts.factory.createParameterDeclaration(
+            undefined,
+            undefined,
+            undefined,
+            EndpointMethodRequestBodyProcessor.#defaultInitParamName,
+            ts.factory.createToken(ts.SyntaxKind.QuestionToken),
+            ts.factory.createTypeReferenceNode(this.#initTypeIdentifier),
+          ),
+        ],
+        packedParameters: ts.factory.createObjectLiteralExpression(),
+        initParam: ts.factory.createIdentifier(EndpointMethodRequestBodyProcessor.#defaultInitParamName),
       };
     }
 
     const parameterData = this.#extractParameterData(this.#requestBody.content[defaultMediaType]?.schema);
+    const parameterNames = parameterData.map(([name]) => name);
+    let initParamName = EndpointMethodRequestBodyProcessor.#defaultInitParamName;
+
+    while (parameterNames.includes(initParamName)) {
+      initParamName = `_${initParamName}`;
+    }
 
     return {
-      parameters: parameterData.map(([name, schema]) => {
-        const nodes = new TypeSchemaProcessor(schema, this.#dependencies).process();
+      parameters: [
+        ...parameterData.map(([name, schema]) => {
+          const nodes = new TypeSchemaProcessor(schema, this.#dependencies).process();
 
-        return ts.factory.createParameterDeclaration(
+          return ts.factory.createParameterDeclaration(
+            undefined,
+            undefined,
+            undefined,
+            name,
+            undefined,
+            ts.factory.createUnionTypeNode(nodes),
+          );
+        }),
+        ts.factory.createParameterDeclaration(
           undefined,
           undefined,
           undefined,
-          name,
-          undefined,
-          ts.factory.createUnionTypeNode(nodes),
-        );
-      }),
+          initParamName,
+          ts.factory.createToken(ts.SyntaxKind.QuestionToken),
+          ts.factory.createTypeReferenceNode(this.#initTypeIdentifier),
+        ),
+      ],
       packedParameters: ts.factory.createObjectLiteralExpression(
         parameterData.map(([name]) => ts.factory.createShorthandPropertyAssignment(name)),
       ),
+      initParam: ts.factory.createIdentifier(initParamName),
     };
   }
 
   #extractParameterData(
     basicSchema?: ReadonlyDeep<OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject>,
-  ): ReadonlyArray<readonly [string, Schema]> {
+  ): Array<readonly [string, Schema]> {
     if (!basicSchema) {
       return [];
     }
--- packages/ts/generator-typescript-plugin-backbone/test/complex-type/fixtures/ComplexTypeEndpoint.snap.ts
+++ packages/ts/generator-typescript-plugin-backbone/test/complex-type/fixtures/ComplexTypeEndpoint.snap.ts
@@ -1,4 +1,5 @@
+import { EndpointRequestInit as EndpointRequestInit_1 } from "@hilla/frontend";
 import client_1 from "./connect-client.default";
 import type ComplexTypeModel_1 from "./dev/hilla/parser/plugins/backbone/complextype/ComplexTypeEndpoint/ComplexTypeModel";
-async function getComplexTypeModel_1(data: Array<Record<string, string | undefined> | undefined> | undefined): Promise<ComplexTypeModel_1 | undefined> { return client_1.call("ComplexTypeEndpoint", "getComplexTypeModel", { data }); }
+async function getComplexTypeModel_1(data: Array<Record<string, string | undefined> | undefined> | undefined, init?: EndpointRequestInit_1): Promise<ComplexTypeModel_1 | undefined> { return client_1.call("ComplexTypeEndpoint", "getComplexTypeModel", { data }, init); }
 export { getComplexTypeModel_1 as getComplexTypeModel };
