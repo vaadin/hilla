3e019d9b1cc210aba4f78becfeb57532932e8d50
Luciano Vernaschi <luciano@vaadin.com>
2022-07-12 13:11:14 +0200

    Subscriptions are not Promises

--- packages/ts/generator-typescript-plugin-backbone/test/push-type/fixtures/PushTypeEndpoint.snap.ts
+++ packages/ts/generator-typescript-plugin-backbone/test/push-type/fixtures/PushTypeEndpoint.snap.ts
@@ -1,6 +1,6 @@
 import { EndpointRequestInit as EndpointRequestInit_1, Subscription as Subscription_1 } from "@hilla/frontend";
 import client_1 from "./connect-client.default";
 import type UserEntity_1 from "./dev/hilla/parser/plugins/backbone/pushtype/PushTypeEndpoint/UserEntity";
-function getAllUsers_1(init?: EndpointRequestInit_1): Promise<Subscription_1<UserEntity_1 | undefined> | undefined> { return client_1.subscribe("PushTypeEndpoint", "getAllUsers", {}); }
-function getAllUsersNonnull_1(init?: EndpointRequestInit_1): Promise<Subscription_1<UserEntity_1>> { return client_1.subscribe("PushTypeEndpoint", "getAllUsersNonnull", {}); }
+function getAllUsers_1(init?: EndpointRequestInit_1): Subscription_1<UserEntity_1 | undefined> | undefined { return client_1.subscribe("PushTypeEndpoint", "getAllUsers", {}); }
+function getAllUsersNonnull_1(init?: EndpointRequestInit_1): Subscription_1<UserEntity_1> { return client_1.subscribe("PushTypeEndpoint", "getAllUsersNonnull", {}); }
 export { getAllUsers_1 as getAllUsers, getAllUsersNonnull_1 as getAllUsersNonnull };
