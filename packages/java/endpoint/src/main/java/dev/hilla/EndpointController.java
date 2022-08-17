c710d26fa65467e067d938f7bbe2495e2cd813d3
Anton Platonov <platosha@gmail.com>
2022-08-17 10:31:36 +0300

    chore: update @NpmPackage versions

--- packages/java/endpoint/src/main/java/dev/hilla/EndpointController.java
+++ packages/java/endpoint/src/main/java/dev/hilla/EndpointController.java
@@ -65,8 +65,8 @@ import dev.hilla.exception.EndpointException;
  */
 @RestController
 @Import({ EndpointControllerConfiguration.class, EndpointProperties.class })
-@NpmPackage(value = "@hilla/frontend", version = "1.2.0-alpha1")
-@NpmPackage(value = "@hilla/form", version = "1.2.0-alpha1")
+@NpmPackage(value = "@hilla/frontend", version = "1.2.0-beta1")
+@NpmPackage(value = "@hilla/form", version = "1.2.0-beta1")
 public class EndpointController {
     static final String ENDPOINT_METHODS = "/{endpoint}/{method}";
 
