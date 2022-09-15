f13f6efd335e5093e745be804c0c46b1aa1f99e7
Anton Platonov <platosha@gmail.com>
2022-09-15 20:36:59 +0300

    fix(parser-jvm-core): revert to allow null plugin configuration by default

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/AbstractPlugin.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/AbstractPlugin.java
@@ -16,21 +16,30 @@ public abstract class AbstractPlugin<C extends PluginConfiguration>
     }
 
     @Override
-    @Nonnull
     public C getConfiguration() {
         return configuration;
     }
 
     @Override
     @SuppressWarnings("unchecked")
-    public void setConfiguration(@Nonnull PluginConfiguration configuration) {
+    public void setConfiguration(PluginConfiguration configuration) {
+        if (configuration == null) {
+            this.configuration = null;
+            return;
+        }
+
         var configClass = (Class<C>) ((ParameterizedType) getClass()
                 .getGenericSuperclass()).getActualTypeArguments()[0];
-        Objects.requireNonNull(configuration);
+        if (configClass.equals(PluginConfiguration.class)) {
+            throw new IllegalArgumentException(String.format(
+                "The '%s' plugin does not expect configuration set",
+                getClass().getName()));
+        }
+
         if (!configClass.isAssignableFrom(configuration.getClass())) {
             throw new IllegalArgumentException(
-                    String.format("Requires instance of %s " + ", but got %s",
-                            configClass, configuration.getClass()));
+                String.format("Requires instance of %s " + ", but got %s",
+                    configClass, configuration.getClass()));
         }
 
         this.configuration = (C) configuration;
