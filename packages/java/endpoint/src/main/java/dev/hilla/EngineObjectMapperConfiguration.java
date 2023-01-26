b4ec3cf9d4ae9656d7858e17c8c5bbe19ffe1d70
Vlad Rindevich <vladrin@vaadin.com>
2023-01-26 14:09:58 +0200

    fix(parser-jvm): add Jackson 2 modules to work with JDK8+ code

--- packages/java/endpoint/src/main/java/dev/hilla/EngineObjectMapperConfiguration.java
+++ packages/java/endpoint/src/main/java/dev/hilla/EngineObjectMapperConfiguration.java
@@ -1,4 +1,4 @@
-package dev.hilla.generator;
+package dev.hilla;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import dev.hilla.ByteArrayModule;
-import dev.hilla.ConditionalOnFeatureFlag;
-import dev.hilla.EndpointController;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 @Configuration
 @ConditionalOnFeatureFlag(EngineObjectMapperConfiguration.HILLA_ENGINE_FEATURE_FLAG)
@@ -30,6 +29,8 @@ public class EngineObjectMapperConfiguration {
 
         logger.debug("Using Hilla Multi-Module Engine ObjectMapper");
 
-        return new ObjectMapper();
+        return new ObjectMapper().registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
     }
 }
--- packages/java/parser-jvm-plugin-backbone/src/main/java/dev/hilla/parser/plugins/backbone/BackbonePlugin.java
+++ packages/java/parser-jvm-plugin-backbone/src/main/java/dev/hilla/parser/plugins/backbone/BackbonePlugin.java
@@ -1,6 +1,9 @@
 package dev.hilla.parser.plugins.backbone;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 import dev.hilla.parser.core.AbstractCompositePlugin;
 import dev.hilla.parser.core.PluginConfiguration;
@@ -10,7 +13,11 @@ public final class BackbonePlugin
     public BackbonePlugin() {
         super(new EndpointPlugin(), new EndpointExposedPlugin(),
                 new MethodPlugin(), new MethodParameterPlugin(),
-                new EntityPlugin(), new PropertyPlugin(new ObjectMapper()),
+                new EntityPlugin(),
+                new PropertyPlugin(new ObjectMapper()
+                        .registerModule(new ParameterNamesModule())
+                        .registerModule(new Jdk8Module())
+                        .registerModule(new JavaTimeModule())),
                 new TypeSignaturePlugin());
     }
 }
