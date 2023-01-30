c182a784e8e09f0168da93bf3f2b00df8219fa68
Vlad Rindevich <vladrin@vaadin.com>
2023-01-30 19:19:26 +0200

    refactor(parser-jvm-core): add setter support for JacksonPropertyModel

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/models/jackson/JacksonPropertyModel.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/models/jackson/JacksonPropertyModel.java
@@ -1,17 +1,20 @@
 package dev.hilla.parser.models.jackson;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import java.lang.reflect.AnnotatedElement;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 
 import dev.hilla.parser.models.AnnotatedAbstractModel;
 import dev.hilla.parser.models.AnnotationInfoModel;
 import dev.hilla.parser.models.ClassInfoModel;
+import dev.hilla.parser.models.ClassMemberModel;
 import dev.hilla.parser.models.FieldInfoModel;
 import dev.hilla.parser.models.MethodInfoModel;
 import dev.hilla.parser.models.Model;
@@ -26,6 +29,7 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
     private final BeanPropertyDefinition origin;
     private Optional<FieldInfoModel> field;
     private Optional<MethodInfoModel> getter;
+    private Optional<MethodInfoModel> setter;
     private SignatureModel type;
 
     private JacksonPropertyModel(BeanPropertyDefinition origin) {
@@ -35,6 +39,10 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
     public static JacksonPropertyModel of(
             @Nonnull BeanPropertyDefinition origin) {
         return new JacksonPropertyModel(origin);
+    }
+
+    public boolean couldDeserialize() {
+        return origin.couldDeserialize();
     }
 
     @Override
@@ -56,6 +64,16 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
     @Override
     public BeanPropertyDefinition get() {
         return origin;
+    }
+
+    public Optional<? extends ClassMemberModel> getAccessor() {
+        if (origin.hasField()) {
+            return getField();
+        } else if (origin.hasGetter()) {
+            return getGetter();
+        }
+
+        return Optional.empty();
     }
 
     @Override
@@ -85,6 +103,16 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
         return getter;
     }
 
+    public Optional<? extends ClassMemberModel> getMutator() {
+        if (origin.hasField()) {
+            return getField();
+        } else if (origin.hasSetter()) {
+            return getSetter();
+        }
+
+        return Optional.empty();
+    }
+
     @Override
     public String getName() {
         return origin.getName();
@@ -92,19 +120,42 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
 
     @Override
     public ClassInfoModel getOwner() {
-        return ClassInfoModel.of(origin.getAccessor().getDeclaringClass());
+        return getPrimaryMember().getOwner();
     }
 
-    public SignatureModel getType() {
-        var accessor = origin.getAccessor().getAnnotated();
+    public ClassMemberModel getPrimaryMember() {
+        if (origin.hasField()) {
+            return getField().get();
+        } else if (origin.hasGetter()) {
+            return getGetter().get();
+        }
 
+        return getSetter().get();
+    }
+
+    public SignatureModel getPrimaryType() {
         if (type == null) {
-            type = SignatureModel.of(accessor instanceof Method
-                    ? ((Method) accessor).getAnnotatedReturnType()
-                    : ((Field) accessor).getAnnotatedType());
+            if (origin.hasField()) {
+                return getField().get().getType();
+            } else if (origin.hasGetter()) {
+                return getGetter().get().getResultType();
+            }
+
+            return getSetter().get().getParameters().get(0).getType();
         }
 
         return type;
+    }
+
+    public Optional<MethodInfoModel> getSetter() {
+        if (setter == null) {
+            setter = origin.hasSetter()
+                    ? Optional.of(MethodInfoModel
+                            .of(origin.getSetter().getAnnotated()))
+                    : Optional.empty();
+        }
+
+        return setter;
     }
 
     public boolean hasField() {
@@ -115,10 +166,18 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
         return origin.hasGetter();
     }
 
+    public boolean hasSetter() {
+        return origin.hasSetter();
+    }
+
     @Override
     public int hashCode() {
         return (origin.getName().hashCode() + getOwner().hashCode())
                 ^ 0x73448be4;
+    }
+
+    public boolean isExplicitlyIncluded() {
+        return origin.isExplicitlyIncluded();
     }
 
     @Override
@@ -127,9 +186,16 @@ public final class JacksonPropertyModel extends AnnotatedAbstractModel
     }
 
     @Override
+    public String toString() {
+        return "JacksonPropertyModel[" + origin + "]";
+    }
+
+    @Override
     protected List<AnnotationInfoModel> prepareAnnotations() {
-        return Arrays
-                .stream(origin.getAccessor().getAnnotated().getAnnotations())
+        return Stream
+                .of(origin.getField(), origin.getGetter(), origin.getSetter())
+                .filter(Objects::nonNull).map(Annotated::getAnnotated)
+                .map(AnnotatedElement::getAnnotations).flatMap(Arrays::stream)
                 .map(AnnotationInfoModel::of).collect(Collectors.toList());
     }
 }
