255be986ac5f7de1f88a6295f72d05389af3f9ca
Vlad Rindevich <vladrin@vaadin.com>
2023-02-07 22:19:22 +0200

    docs(parser-jvm): add documentation for Parser class.

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/Parser.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/Parser.java
@@ -19,6 +19,10 @@ import org.slf4j.LoggerFactory;
 import io.github.classgraph.ClassGraph;
 import io.swagger.v3.oas.models.OpenAPI;
 
+/**
+ * The entrypoint class. It searches for the endpoint classes in the classpath
+ * and produces an OpenAPI definition.
+ */
 public final class Parser {
     private static final Logger logger = LoggerFactory.getLogger(Parser.class);
     private final Config config;
@@ -38,36 +42,76 @@ public final class Parser {
         }
     }
 
-    private static OpenAPI parseOpenAPIFile(@Nonnull String contents,
+    private static OpenAPI parseOpenAPIFile(@Nonnull String source,
             @Nonnull OpenAPIFileType type, OpenAPI origin) {
         try {
             var mapper = type.getMapper();
             var reader = origin != null ? mapper.readerForUpdating(origin)
                     : mapper.reader();
-            return reader.readValue(contents, OpenAPI.class);
+            return reader.readValue(source, OpenAPI.class);
         } catch (IOException e) {
             throw new ParserException("Failed to parse openAPI specification",
                     e);
         }
     }
 
+    /**
+     * Adds a parser {@link Plugin}.
+     *
+     * <p>
+     * Note that the order will be defined by {@link Plugin#getOrder} method,
+     * not by order of the method calls.
+     *
+     * @param plugin
+     *            An instance of the parser plugin.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser addPlugin(@Nonnull Plugin plugin) {
         config.plugins.add(Objects.requireNonNull(plugin));
         return this;
     }
 
+    /**
+     * Allows to programmatically change the default OpenAPI definition.
+     *
+     * @param action
+     *            a consumer lambda that accepts an OpenAPI instance.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser adjustOpenAPI(@Nonnull Consumer<OpenAPI> action) {
         action.accept(config.openAPI);
         return this;
     }
 
+    /**
+     * Specifies the classpath where the parser will scan for endpoints.
+     * Specifying the classpath is required.
+     *
+     * <p>
+     * If the classpath is already set, it will be overridden.
+     *
+     * @param classPathElements
+     *            a list of paths forming the classpath.
+     * @return this (for method chaining).
+     */
     @Nonnull
-    public Parser classPath(@Nonnull String[] classPathElements) {
+    public Parser classPath(@Nonnull String... classPathElements) {
         return classPath(classPathElements, true);
     }
 
+    /**
+     * Specifies the classpath where the parser will scan for endpoints.
+     * Specifying the classpath is required.
+     *
+     * @param classPathElements
+     *            a list of paths forming the classpath.
+     * @param override
+     *            specifies if the parser should override the classpath if it is
+     *            already specified.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser classPath(@Nonnull String[] classPathElements,
             boolean override) {
@@ -76,6 +120,33 @@ public final class Parser {
                 override);
     }
 
+    /**
+     * Specifies the classpath where the parser will scan for endpoints.
+     * Specifying the classpath is required.
+     *
+     * <p>
+     * If the classpath is already set, it will be overridden.
+     *
+     * @param classPathElements
+     *            a list of paths forming the classpath.
+     * @return this (for method chaining).
+     */
+    @Nonnull
+    public Parser classPath(@Nonnull Collection<String> classPathElements) {
+        return classPath(classPathElements, true);
+    }
+
+    /**
+     * Specifies the classpath where the parser will scan for endpoints.
+     * Specifying the classpath is required.
+     *
+     * @param classPathElements
+     *            a list of paths forming the classpath.
+     * @param override
+     *            specifies if the parser should override the classpath if it is
+     *            already specified.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser classPath(@Nonnull Collection<String> classPathElements,
             boolean override) {
@@ -86,11 +157,36 @@ public final class Parser {
         return this;
     }
 
+    /**
+     * Specifies the name of the endpoint annotation by which the parser will
+     * search for the endpoints. Only classes with this annotation will be
+     * chosen.
+     *
+     * <p>
+     * If the annotation name is already set, it will be overridden.
+     *
+     * @param annotationFullyQualifiedName
+     *            The fully qualified name of the annotation
+     * @return this (for method chaining).
+     */
     @Nonnull
-    public Parser classPath(@Nonnull Collection<String> classPathElements) {
-        return classPath(classPathElements, true);
+    public Parser endpointAnnotation(
+            @Nonnull String annotationFullyQualifiedName) {
+        return endpointAnnotation(annotationFullyQualifiedName, true);
     }
 
+    /**
+     * Specifies the name of the endpoint annotation by which the parser will
+     * search for the endpoints. Only classes with this annotation will be
+     * chosen.
+     *
+     * @param annotationFullyQualifiedName
+     *            The fully qualified name of the annotation
+     * @param override
+     *            specifies if the parser should override the annotation name if
+     *            it is already specified.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser endpointAnnotation(
             @Nonnull String annotationFullyQualifiedName, boolean override) {
@@ -101,11 +197,38 @@ public final class Parser {
         return this;
     }
 
+    /**
+     * Specifies the name of the `EndpointExposed` annotation by which the
+     * parser will detect if the endpoint superclass should be considered as the
+     * part of the endpoint. Any superclass in the endpoint's inheritance chain
+     * will be skipped if it doesn't have this annotation.
+     *
+     * <p>
+     * If the annotation name is already set, it will be overridden.
+     *
+     * @param annotationFullyQualifiedName
+     *            The fully qualified name of the annotation
+     * @return this (for method chaining).
+     */
     @Nonnull
-    public Parser endpointAnnotation(@Nonnull String annotationQualifiedName) {
-        return endpointAnnotation(annotationQualifiedName, true);
+    public Parser endpointExposedAnnotation(
+            @Nonnull String annotationFullyQualifiedName) {
+        return endpointExposedAnnotation(annotationFullyQualifiedName, true);
     }
 
+    /**
+     * Specifies the name of the `EndpointExposed` annotation by which the
+     * parser will detect if the endpoint superclass should be considered as the
+     * part of the endpoint. Any superclass in the endpoint's inheritance chain
+     * will be skipped if it doesn't have this annotation.
+     *
+     * @param annotationFullyQualifiedName
+     *            The fully qualified name of the annotation
+     * @param override
+     *            specifies if the parser should override the annotation name if
+     *            it is already specified.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser endpointExposedAnnotation(
             @Nonnull String annotationFullyQualifiedName, boolean override) {
@@ -116,12 +239,12 @@ public final class Parser {
         return this;
     }
 
+    /**
+     * Scans the classpath, blocking until the scan is complete.
+     *
+     * @return A result OpenAPI object.
+     */
     @Nonnull
-    public Parser endpointExposedAnnotation(
-            @Nonnull String annotationQualifiedName) {
-        return endpointExposedAnnotation(annotationQualifiedName, true);
-    }
-
     public OpenAPI execute() {
         Objects.requireNonNull(config.classPathElements,
                 "[JVM Parser] classPath is not provided.");
@@ -149,23 +272,73 @@ public final class Parser {
         return storage.getOpenAPI();
     }
 
+    /**
+     * Gets the internal configuration object.
+     *
+     * @return configuration object.
+     */
+    @Nonnull
     public Config getConfig() {
         return config;
     }
 
+    /**
+     * Parses the OpenAPI source string with the provided parser and merges the
+     * result into the current OpenAPI object. This method is useful if you want
+     * to adjust some basic parts of the OpenAPI object like the application
+     * title, version, server description or URL.
+     *
+     * <p>
+     * If the method is used once, all the changes will be applied to the
+     * default OpenAPI object. Called multiple time, this function applies
+     * changes one by one in the order of method calls.
+     *
+     * @param source
+     *            The OpenAPI definition in the JSON or YAML format. You don't
+     *            have to specify all the fields required by the schema; the
+     *            result definition will use default fields.
+     * @param type
+     *            The parser for the OpenAPI definition
+     * @return this (for method chaining).
+     */
     @Nonnull
-    public Parser openAPISource(@Nonnull String path,
+    public Parser openAPISource(@Nonnull String source,
             @Nonnull OpenAPIFileType type) {
-        config.openAPI = parseOpenAPIFile(Objects.requireNonNull(path),
+        config.openAPI = parseOpenAPIFile(Objects.requireNonNull(source),
                 Objects.requireNonNull(type), config.openAPI);
 
         return this;
     }
 
-    public Parser plugins(Plugin... plugins) {
+    /**
+     * Adds a collection of parser {@link Plugin}s. If there are plugins already
+     * specified, they will be removed before addition.
+     *
+     * <p>
+     * Note that the order will be defined by {@link Plugin#getOrder} method,
+     * not by order of plugins in the collection.
+     *
+     * @param plugins
+     *            a collection of parser plugins.
+     * @return this (for method chaining).
+     */
+    @Nonnull
+    public Parser plugins(@Nonnull Plugin... plugins) {
         return plugins(Arrays.asList(plugins));
     }
 
+    /**
+     * Adds a collection of parser {@link Plugin}s. If there are already
+     * specified plugins, they will be removed before addition.
+     *
+     * <p>
+     * Note that the order will be defined by {@link Plugin#getOrder} method,
+     * not by order of plugins in the collection.
+     *
+     * @param plugins
+     *            a collection of parser plugins.
+     * @return this (for method chaining).
+     */
     @Nonnull
     public Parser plugins(@Nonnull Collection<? extends Plugin> plugins) {
         config.plugins.clear();
@@ -173,6 +346,10 @@ public final class Parser {
         return this;
     }
 
+    /**
+     * An immutable parser configuration object. It allows to peek into the
+     * initial configuration of the parser during the scan.
+     */
     public static final class Config {
         private final SortedSet<Plugin> plugins = new TreeSet<>(
                 Comparator.comparingInt(Plugin::getOrder));
@@ -185,26 +362,54 @@ public final class Parser {
             this.openAPI = openAPI;
         }
 
+        /**
+         * Gets the collection of classpath elements.
+         *
+         * @return the collection of classpath elements.
+         */
         @Nonnull
         public Set<String> getClassPathElements() {
             return classPathElements;
         }
 
+        /**
+         * Gets the name of endpoint annotation.
+         *
+         * @return the annotation name.
+         */
         @Nonnull
         public String getEndpointAnnotationName() {
             return endpointAnnotationName;
         }
 
+        /**
+         * Gets the name of `EndpointExposed` annotation.
+         *
+         * @return the annotation name.
+         */
         @Nonnull
         public String getEndpointExposedAnnotationName() {
             return endpointExposedAnnotationName;
         }
 
+        /**
+         * Gets the OpenAPI object.
+         *
+         * <p>
+         * Note that the object is mutable.
+         *
+         * @return OpenAPI object.
+         */
         @Nonnull
         public OpenAPI getOpenAPI() {
             return openAPI;
         }
 
+        /**
+         * Returns a collection of parser plugins.
+         *
+         * @return the collection of parser plugins.
+         */
         @Nonnull
         public SortedSet<Plugin> getPlugins() {
             return plugins;
