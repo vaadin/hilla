1b2074942a46ae5ebb7fce484a714cb5637af712
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-02-27 16:47:07 +0100

    fix: use Maven Wrapper if available (#792)

---
+++ packages/java/engine-runtime/src/main/java/dev/hilla/internal/runner/MavenRunner.java
@@ -0,0 +1,123 @@
+package dev.hilla.internal.runner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Runs a Maven command.
+ */
+public class MavenRunner implements CommandRunner {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(MavenRunner.class);
+
+    private final Path projectDir;
+    private final boolean windowsOS;
+    private final String[] args;
+
+    /**
+     * Creates a Maven runner.
+     *
+     * @param projectDir
+     *            the project directory
+     * @param windowsOS
+     *            whether the OS is Windows
+     * @param args
+     *            the arguments to pass to the Maven command
+     */
+    public MavenRunner(Path projectDir, boolean windowsOS, String... args) {
+        this.projectDir = projectDir;
+        this.windowsOS = windowsOS;
+        this.args = args;
+    }
+
+    /**
+     * Creates a Maven runner for the given project directory.
+     *
+     * @param projectDir
+     *            the project directory
+     * @param args
+     *            the arguments to pass to the Maven command
+     * @return a Maven runner if the project directory contains a Maven project,
+     *         an empty optional otherwise
+     */
+    public static Optional<CommandRunner> forProject(Path projectDir,
+            String... args) {
+        if (Files.exists(projectDir.resolve("pom.xml"))) {
+            var os = System.getProperty("os.name").toLowerCase();
+            var windowsOS = os.contains("windows");
+            return Optional.of(new MavenRunner(projectDir, windowsOS, args));
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Returns the name of the maven executable, depending on the OS.
+     */
+    public String mavenExecutable() {
+        return windowsOS ? "mvn.cmd" : "mvn";
+    }
+
+    /**
+     * Returns the name of the maven wrapper executable, depending on the OS.
+     */
+    public String wrapperExecutable() {
+        return windowsOS ? ".\\mvnw.cmd" : "./mvnw";
+    }
+
+    /**
+     * Selects the maven executable to use. Prefer the maven wrapper if it
+     * exists.
+     */
+    public String chooseExecutable() {
+        var command = wrapperExecutable();
+
+        if (Files.exists(projectDir.resolve(command))) {
+            return command;
+        } else {
+            LOGGER.debug("No maven wrapper found");
+            return mavenExecutable();
+        }
+    }
+
+    @Override
+    public void run() throws RunnerException {
+        var command = new ArrayList<String>();
+        command.add(chooseExecutable());
+        command.addAll(Arrays.asList(args));
+
+        var exitCode = 0;
+        try {
+            LOGGER.debug("Running command: {}", command);
+            ProcessBuilder builder = new ProcessBuilder(command)
+                    .directory(projectDir.toFile()).inheritIO();
+            exitCode = builder.start().waitFor();
+        } catch (IOException e) {
+            if (e.getCause() != null && e.getCause().getMessage()
+                    .contains("No such file or directory")) {
+                LOGGER.error(
+                        """
+                                No maven executable found. You can install the Maven Wrapper by
+                                running the following command in your project directory:
+                                `mvn wrapper:wrapper` where `mvn` is the path to your Maven executable.
+                                Alternatively, you can add the Maven executable to your PATH environment variable.
+                                """);
+            }
+            throw new RunnerException("Maven not found", e);
+        } catch (Exception e) {
+            throw new RunnerException("Maven command execution failed", e);
+        }
+        if (exitCode != 0) {
+            throw new RunnerException(
+                    "Maven command execution failed with exit code" + exitCode);
+        }
+    }
+}
