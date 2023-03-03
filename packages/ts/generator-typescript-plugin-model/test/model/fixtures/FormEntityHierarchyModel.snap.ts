6dbd2f7918945b11cc19d9f7d1d43712ba1eb133
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-03-03 13:15:22 +0100

    fix(engine): change how model checks for nullable fields (#797)
    * Change how model checks for nullable fields
    
    * Simplify code
    
    * Remove unused constructor parameter

--- packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormEntityHierarchyModel.snap.ts
+++ packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormEntityHierarchyModel.snap.ts
@@ -4,10 +4,10 @@ import FormEntityIdModel_1 from "./FormEntityIdModel";
 class FormEntityHierarchyModel<T extends FormEntityHierarchy_1 = FormEntityHierarchy_1> extends FormEntityIdModel_1<T> {
     declare static createEmptyValue: () => FormEntityHierarchy_1;
     get lorem(): StringModel_1 {
-        return this[_getPropertyModel_1]("lorem", StringModel_1, [true]) as StringModel_1;
+        return this[_getPropertyModel_1]("lorem", StringModel_1, [false]) as StringModel_1;
     }
     get ipsum(): NumberModel_1 {
-        return this[_getPropertyModel_1]("ipsum", NumberModel_1, [true]) as NumberModel_1;
+        return this[_getPropertyModel_1]("ipsum", NumberModel_1, [false]) as NumberModel_1;
     }
 }
 export default FormEntityHierarchyModel;
--- packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormOptionalTypesModel.snap.ts
+++ packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormOptionalTypesModel.snap.ts
@@ -4,16 +4,16 @@ import type FormOptionalTypes_1 from "./FormOptionalTypes";
 class FormOptionalTypesModel<T extends FormOptionalTypes_1 = FormOptionalTypes_1> extends ObjectModel_1<T> {
     declare static createEmptyValue: () => FormOptionalTypes_1;
     get optionalString(): StringModel_1 {
-        return this[_getPropertyModel_1]("optionalString", StringModel_1, [true]) as StringModel_1;
+        return this[_getPropertyModel_1]("optionalString", StringModel_1, [false]) as StringModel_1;
     }
     get optionalEntity(): FormEntityModel_1 {
-        return this[_getPropertyModel_1]("optionalEntity", FormEntityModel_1, [true]) as FormEntityModel_1;
+        return this[_getPropertyModel_1]("optionalEntity", FormEntityModel_1, [false]) as FormEntityModel_1;
     }
     get optionalList(): ArrayModel_1<string, StringModel_1> {
-        return this[_getPropertyModel_1]("optionalList", ArrayModel_1, [true, StringModel_1, [true]]) as ArrayModel_1<string, StringModel_1>;
+        return this[_getPropertyModel_1]("optionalList", ArrayModel_1, [false, StringModel_1, [true]]) as ArrayModel_1<string, StringModel_1>;
     }
     get optionalMatrix(): ArrayModel_1<ReadonlyArray<string>, ArrayModel_1<string, StringModel_1>> {
-        return this[_getPropertyModel_1]("optionalMatrix", ArrayModel_1, [true, ArrayModel_1, [true, StringModel_1, [true]]]) as ArrayModel_1<ReadonlyArray<string>, ArrayModel_1<string, StringModel_1>>;
+        return this[_getPropertyModel_1]("optionalMatrix", ArrayModel_1, [false, ArrayModel_1, [true, StringModel_1, [true]]]) as ArrayModel_1<ReadonlyArray<string>, ArrayModel_1<string, StringModel_1>>;
     }
 }
 export default FormOptionalTypesModel;
