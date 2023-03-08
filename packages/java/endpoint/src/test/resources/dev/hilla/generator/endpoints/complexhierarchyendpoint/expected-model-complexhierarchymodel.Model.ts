d42d9ec7f304d9c112f2fb58573f49a06b85ec79
Vlad Rindevich <vladrin@vaadin.com>
2023-03-08 23:52:11 +0200

    chrome(endpoint): remove unnecessary resources and old generator removal leftovers

--- packages/java/endpoint/src/test/resources/dev/hilla/generator/endpoints/complexhierarchyendpoint/expected-model-complexhierarchymodel.Model.ts
+++
@@ -1,9 +0,0 @@
-import ParentModel from './ParentModel';
-
-/**
- * This module is generated from dev.hilla.generator.endpoints.complexhierarchymodel.Model.
- * All changes to this file are overridden. Consider editing the corresponding Java file if necessary.
- */
-export default interface Model extends ParentModel {
-  name?: string;
-}
--- packages/java/endpoint/src/test/resources/dev/hilla/generator/endpoints/pageendpoint/expected-model-pageendpoint.PageEndpoint.Foo.ts
+++
@@ -1,7 +0,0 @@
-/**
-* This module is generated from dev.hilla.generator.endpoints.pageendpoint.PageEndpoint.Foo.
-* All changes to this file are overridden. Consider editing the corresponding Java file if necessary.
-*/
-export default interface Foo {
-  value?: string;
-}
