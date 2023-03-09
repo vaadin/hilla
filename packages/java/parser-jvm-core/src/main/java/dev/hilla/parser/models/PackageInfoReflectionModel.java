6a9b15791af59bd2c0cda4c1c829dbbd0c2613c1
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-03-09 15:27:42 +0100

    fix: use classloader from current class to find packages (#818)

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/models/PackageInfoReflectionModel.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/models/PackageInfoReflectionModel.java
@@ -1,9 +1,6 @@
 package dev.hilla.parser.models;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class PackageInfoReflectionModel extends PackageInfoModel {
     private final Package origin;
@@ -25,27 +22,5 @@ class PackageInfoReflectionModel extends PackageInfoModel {
     @Override
     protected List<AnnotationInfoModel> prepareAnnotations() {
         return processAnnotations(origin.getDeclaredAnnotations());
-    }
-
-    @Override
-    public List<PackageInfoModel> getAncestors() {
-        var classLoader = getClass().getClassLoader();
-        return getAllAncestorPackageNames(origin.getName())
-                .map(classLoader::getDefinedPackage).filter(Objects::nonNull)
-                .map(PackageInfoModel::of).collect(Collectors.toList());
-    }
-
-    /**
-     * Returns a stream of all ancestor package names, starting with the
-     * immediate parent package.
-     *
-     * @param packageName
-     *            the package name
-     * @return the stream of all ancestor package names
-     */
-    private static Stream<String> getAllAncestorPackageNames(
-            String packageName) {
-        return Stream.iterate(packageName, p -> p.contains("."),
-                p -> p.substring(0, p.lastIndexOf('.')));
     }
 }
