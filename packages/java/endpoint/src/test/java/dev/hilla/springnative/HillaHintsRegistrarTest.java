18e1d9965f65430a944fa83e1197e77cb4d64b68
Artur <artur@vaadin.com>
2023-03-14 14:13:35 +0200

    fix: Fix openapi parsing when there are no custom types (#810)
    * fix: Fix parsing when there are no custom types
    
    * Use Set instead of List as order does not matter
    
    ---------
    
    Co-authored-by: Luciano Vernaschi <cromoteca@users.noreply.github.com>

---
+++ packages/java/endpoint/src/test/java/dev/hilla/springnative/HillaHintsRegistrarTest.java
@@ -0,0 +1,49 @@
+package dev.hilla.springnative;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Set;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HillaHintsRegistrarTest {
+
+    @Test
+    public void emptySchemaReturnsNoComponents() throws IOException {
+        Assert.assertEquals(Collections.emptySet(),
+                parse("openapi-nocustomtypes.json"));
+    }
+
+    @Test
+    public void singleType() throws IOException {
+        Assert.assertEquals(Set
+                .of("com.example.application.endpoints.helloreact.MyOtherType"),
+                parse("openapi-customtype.json"));
+    }
+
+    @Test
+    public void referringTypes() throws IOException {
+        Assert.assertEquals(Set.of(
+                "com.example.application.endpoints.helloreact.MyType",
+                "com.example.application.endpoints.helloreact.MyOtherType"),
+                parse("openapi-referring-customtypes.json"));
+    }
+
+    @Test
+    public void nestedType() throws IOException {
+        Assert.assertEquals(Set.of(
+                "com.example.application.endpoints.helloreact.HelloReactEndpoint$MyInnerType"),
+                parse("openapi-innertype.json"));
+    }
+
+    private Set<String> parse(String openapiFilename) throws IOException {
+        String openApi = IOUtils.toString(
+                getClass().getResourceAsStream(openapiFilename),
+                StandardCharsets.UTF_8);
+        return HillaHintsRegistrar.parseOpenApi(openApi);
+    }
+
+}
