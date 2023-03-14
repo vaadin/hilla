c4933c9116b4394dd8a4cbd60d82d19b93e8d988
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-03-14 17:45:48 +0200

    feat: support ES-module application by adding .js to generated imports (#817)
    * Add .js to imports
    
    * Fix formatting
    
    * Add missing .js
    
    * Add type:module to integration tests
    
    * Fix .js added twice to some imports
    
    * Remove leaked dev logs
    
    * test(generator): update snapshots for other plugins
    
    ---------
    
    Co-authored-by: Anton Platonov <platosha@gmail.com>
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>

--- packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormEntityIdModel.snap.ts
+++ packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormEntityIdModel.snap.ts
@@ -1,5 +1,5 @@
 import { _getPropertyModel as _getPropertyModel_1, NotNull as NotNull_1, NumberModel as NumberModel_1, ObjectModel as ObjectModel_1 } from "@hilla/form";
-import type FormEntityId_1 from "./FormEntityId";
+import type FormEntityId_1 from "./FormEntityId.js";
 class FormEntityIdModel<T extends FormEntityId_1 = FormEntityId_1> extends ObjectModel_1<T> {
     declare static createEmptyValue: () => FormEntityId_1;
     get Id(): NumberModel_1 {
