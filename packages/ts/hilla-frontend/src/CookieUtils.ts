dcfa7c2208a0ae115ca514a3f0be0353befe96a8
Vlad Rindevich <vladrin@vaadin.com>
2023-03-15 13:41:40 +0200

    fix(hilla-frontend): add length condition for path checking

--- packages/ts/hilla-frontend/src/CookieUtils.ts
+++ packages/ts/hilla-frontend/src/CookieUtils.ts
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 const { pathname } = new URL(document.baseURI);
 
 const _Cookies: Cookies.CookiesStatic<string> = Cookies.withAttributes({
-  path: pathname.endsWith('/') ? pathname.slice(0, -1) : pathname,
+  path: pathname.length > 1 && pathname.endsWith('/') ? pathname.slice(0, -1) : pathname,
 });
 
 export default _Cookies;
