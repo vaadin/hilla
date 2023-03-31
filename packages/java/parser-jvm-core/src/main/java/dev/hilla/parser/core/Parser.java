5d6c3f124637762a5d8e566b173ab489f9bb594a
Anton Platonov <platosha@gmail.com>
2023-03-31 13:40:50 +0300

    fix(parser-jvm-core): missing import

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/Parser.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/Parser.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
