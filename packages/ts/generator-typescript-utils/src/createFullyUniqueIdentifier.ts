7417b8ce15b6b6ed5cb2f6fa4e79e39ece01d956
Vlad Rindevich <vladrin@vaadin.com>
2023-04-16 19:50:45 +0300

    style(generator-typescript-utils): apply new eslint rules

--- packages/ts/generator-typescript-utils/src/createFullyUniqueIdentifier.ts
+++ packages/ts/generator-typescript-utils/src/createFullyUniqueIdentifier.ts
@@ -1,7 +1,7 @@
-import type { GeneratedIdentifierFlags } from 'typescript';
+import type { GeneratedIdentifierFlags, Identifier } from 'typescript';
 import ts from 'typescript';
 
-export default function createFullyUniqueIdentifier(name: string, flags?: GeneratedIdentifierFlags) {
+export default function createFullyUniqueIdentifier(name: string, flags?: GeneratedIdentifierFlags): Identifier {
   return ts.factory.createUniqueName(
     name,
     // eslint-disable-next-line no-bitwise
