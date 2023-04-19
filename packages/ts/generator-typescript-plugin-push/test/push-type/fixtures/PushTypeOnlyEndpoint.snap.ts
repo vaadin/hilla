2d13fc5f85b41da4eff89c30f2f545b8484e2957
Luciano Vernaschi <luciano@vaadin.com>
2023-04-19 10:42:51 +0200

    Add missing .js

--- packages/ts/generator-typescript-plugin-push/test/push-type/fixtures/PushTypeOnlyEndpoint.snap.ts
+++ packages/ts/generator-typescript-plugin-push/test/push-type/fixtures/PushTypeOnlyEndpoint.snap.ts
@@ -1,5 +1,5 @@
 import { Subscription as Subscription_1 } from "@hilla/frontend";
-import client_1 from "./connect-client.default";
+import client_1 from "./connect-client.default.js";
 function getMessageFlux_1(): Subscription_1<string | undefined> { return client_1.subscribe("PushTypeOnlyEndpoint", "getMessageFlux", {}); }
 function getNonNullMessageFlux_1(): Subscription_1<string> { return client_1.subscribe("PushTypeOnlyEndpoint", "getNonNullMessageFlux", {}); }
 function getSubscription_1(): Subscription_1<string | undefined> { return client_1.subscribe("PushTypeOnlyEndpoint", "getSubscription", {}); }
