eed6059ea1a4f39473c064d3b3a1ba5f6941bc59
taefi <taefi.soroosh@gmail.com>
2023-05-12 04:18:05 +0300

    test: add IT for verifying production build output

---
+++ packages/java/tests/gradle/single-module-tests/src/test/java/dev/hilla/gradle/plugin/test/ProductionBuildFunctionalTest.java
@@ -0,0 +1,127 @@
+package dev.hilla.gradle.plugin.test;
+
+
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import dev.hilla.engine.commandrunner.CommandNotFoundException;
+import dev.hilla.engine.commandrunner.CommandRunnerException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * These tests semantically are unit tests, but since a production build
+ * takes a while to complete, they are performed as ITs, which makes it
+ * also easier to keep the package-lock.json file updated.
+ *
+ * NOTE:
+ * This test is working on the output of production mode build of single-module.
+ */
+public class ProductionBuildFunctionalTest {
+    @Test
+    public void validateProductionBuildOutput() throws IOException {
+        afterProductionBuild_openApiJson_hasCorrectEndpoints();
+        afterProductionBuild_endpointsTs_hasCorrectEndpoints();
+        afterProductionBuild_jarArchiveIsCreated();
+    }
+    public void afterProductionBuild_openApiJson_hasCorrectEndpoints() throws IOException {
+        var openApiJsonPath = getBuildDirPath().resolve("classes/dev/hilla/openapi.json").toFile();
+        var openApiJson = Json.mapper().readValue(openApiJsonPath, OpenAPI.class);
+        assertTrue(openApiJson.getPaths().containsKey("/HelloReactEndpoint/sayHello"));
+    }
+    public void afterProductionBuild_endpointsTs_hasCorrectEndpoints() throws IOException {
+        var endpointTsPath = getFrontendGeneratedPath()
+            .resolve("endpoints.ts");
+        var endpointTsContent = String.join("", Files.readAllLines(endpointTsPath));
+        assertTrue(endpointTsContent.contains("import * as HelloReactEndpoint"));
+    }
+    public void afterProductionBuild_jarArchiveIsCreated() {
+        var executableJarFile = getBuildDirPath().resolve("libs/single-module.jar").toFile();
+        assertTrue(executableJarFile.exists());
+    }
+    @BeforeEach
+    public void runProductionBuild() throws CommandRunnerException {
+        runGradleCommand("./gradlew -Pvaadin.productionMode=true build");
+    }
+
+    private void runGradleCommand(String executable) throws CommandRunnerException {
+
+        var exitCode = 0;
+        var projectRootDir = getProjectRootPath().toFile();
+        try {
+            var builder = new ProcessBuilder(executable.split("\\s+"))
+                .directory(projectRootDir)
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectError(ProcessBuilder.Redirect.INHERIT);
+            var process = builder.start();
+
+            exitCode = process.waitFor();
+
+            if (exitCode != 0) {
+                throw new RuntimeException(
+                    String.format("Process exit code for executing %s is: %d", executable, exitCode));
+            }
+        } catch (IOException | InterruptedException e) {
+            // Tries to figure out if the command is not found. This is not a
+            // 100% reliable way to do it, but an exception will be thrown
+            // anyway.
+            if (e.getCause() != null && e.getCause().getMessage() != null
+                && e.getCause().getMessage()
+                .contains("No such file or directory")) {
+                throw new CommandNotFoundException(String.format(
+                    "Command or file not found in %s: %s", projectRootDir.getPath(), executable), e);
+            }
+
+            throw new CommandRunnerException(
+                "Failed to execute command: " + executable, e);
+        } catch (RuntimeException e) {
+            fail(e);
+        }
+    }
+
+    @AfterEach
+    public void cleanUp() throws CommandRunnerException {
+        try {
+            FileUtils.deleteDirectory(getBuildDirPath().toFile());
+            FileUtils.deleteDirectory(getFrontendGeneratedPath().toFile());
+        } catch (Throwable t) {
+            // suppress possible error while deleting
+            t.printStackTrace();
+        }
+
+    }
+
+    private Path getProjectRootPath() {
+        try {
+            return Path.of(Objects.requireNonNull(
+                            getClass().getClassLoader().getResource(""))
+                        .toURI()) // test
+                    .getParent()  // java
+                    .getParent()  // classes
+                    .getParent()  // build
+                    .getParent()  // single-module-test
+                    .getParent()  // gradle
+                    .resolve("single-module");
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Path getBuildDirPath() {
+        return getProjectRootPath().resolve("build");
+    }
+
+    private Path getFrontendGeneratedPath() {
+        return getProjectRootPath().resolve("frontend/generated");
+    }
+}
