e95e29c806cd1bd74a5f5b68f5701b75564773a5
Soroosh Taefi <taefi.soroosh@gmail.com>
2023-06-13 10:53:32 +0300

    fix: enable overriding the plugins (#1019)
    * fix: enable overriding the plugins
    
    * Add default constructor to AnnotationMatcher to enable maven initialize it properly
    * Enable PluginConfiguration to be serialized properly into configuration file
    Fixes #923
    
    * add tests to verify the changes

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/PluginConfiguration.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/PluginConfiguration.java
@@ -1,4 +1,7 @@
 package dev.hilla.parser.core;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "class")
 public interface PluginConfiguration {
 }
