5a8cc46cddac4f6fae33d481df67d682ec0c3efd
taefi <taefi.soroosh@gmail.com>
2023-06-16 13:42:05 +0300

    fix: create CommandRunner instance if build.gradle.kts was available

--- packages/java/engine-core/src/test/java/dev/hilla/engine/commandrunner/GradleRunnerTest.java
+++ packages/java/engine-core/src/test/java/dev/hilla/engine/commandrunner/GradleRunnerTest.java
@@ -11,6 +11,46 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GradleRunnerTest {
 
     @Test
+    void shouldRunIfBuildGradleIsAvailable() throws IOException {
+        Path tmpDir = null;
+
+        try {
+            tmpDir = Files.createTempDirectory("prepareCommandGradle");
+            Files.createFile(tmpDir.resolve("build.gradle"));
+            var opt = GradleRunner.forProject(tmpDir.toFile(), "-v");
+            assertTrue(opt.isPresent());
+            var runner = opt.orElseThrow();
+            assertEquals(1, runner.arguments().length);
+            assertDoesNotThrow(() -> runner.run(null));
+        } finally {
+            if (tmpDir != null) {
+                Files.deleteIfExists(tmpDir.resolve("build.gradle"));
+                Files.deleteIfExists(tmpDir);
+            }
+        }
+    }
+
+    @Test
+    void shouldRunIfBuildGradleKtsIsAvailable() throws IOException {
+        Path tmpDir = null;
+
+        try {
+            tmpDir = Files.createTempDirectory("prepareCommandGradle");
+            Files.createFile(tmpDir.resolve("build.gradle.kts"));
+            var opt = GradleRunner.forProject(tmpDir.toFile(), "-v");
+            assertTrue(opt.isPresent());
+            var runner = opt.orElseThrow();
+            assertEquals(1, runner.arguments().length);
+            assertDoesNotThrow(() -> runner.run(null));
+        } finally {
+            if (tmpDir != null) {
+                Files.deleteIfExists(tmpDir.resolve("build.gradle.kts"));
+                Files.deleteIfExists(tmpDir);
+            }
+        }
+    }
+
+    @Test
     void shouldNotCreateRunnerForUnknownProjectType() throws IOException {
         Path tmpDir = null;
 
