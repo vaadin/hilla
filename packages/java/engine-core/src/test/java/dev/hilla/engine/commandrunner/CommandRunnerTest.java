47320ee681bf552782e2368fc0ac29723b4cec34
Soroosh Taefi <taefi.soroosh@gmail.com>
2023-06-21 13:50:14 +0300

    fix: use Java path of currently running java process to execute configure goal/task (#1044)
    * fix: use Java path of currently running java process to execute configure goal/task
    
    This helps users with older java versions set
    in their JAVA_HOME globally, but having correct
    java version set in their project build files
    and/or in their IDEs configurations.
    
    Now the same Java executable path that is used
    to run the project (e.g. from IDE) will be used
    to run the hilla:configure (Maven) or hillaConfiure
    (Gradle).
    
    Fixes: #1012
    
    * apply suggestion from code review
    
    * rewording javadoc
    
    * rewording javadoc
    
    * consider windows friendly paths while dealing with java executable path
    
    * add test
    
    * fix java executable name for windows
    
    ---------
    
    Co-authored-by: Anton Platonov <anton@vaadin.com>

--- packages/java/engine-core/src/test/java/dev/hilla/engine/commandrunner/CommandRunnerTest.java
+++ packages/java/engine-core/src/test/java/dev/hilla/engine/commandrunner/CommandRunnerTest.java
@@ -5,7 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -101,4 +104,73 @@ public class CommandRunnerTest {
 
         assertDoesNotThrow(() -> runner.run(null));
     }
+
+    @Test
+    void runningCommands_javaExecutablePathFromCurrentRunningProcess_isUsedToSetJavaHomeOfProcessBuilder() {
+
+        TestRunner runner = new TestRunner() {
+
+            static final String JAVA_EXEC_PATH = IS_WINDOWS
+                    ? "c:\\path\\to\\java\\home\\bin\\java.exe"
+                    : "/path/to/java/home/bin/java";
+
+            @Override
+            public List<String> executables() {
+                return List.of("fakeCommand", DIR_LS);
+            }
+
+            @Override
+            public ProcessHandle.Info getCurrentProcessInfo() {
+                return new MockProcessInfo();
+            }
+
+            private static class MockProcessInfo implements ProcessHandle.Info {
+
+                @Override
+                public Optional<String> command() {
+                    return Optional.of(JAVA_EXEC_PATH);
+                }
+
+                @Override
+                public Optional<String> commandLine() {
+                    return Optional.empty();
+                }
+
+                @Override
+                public Optional<String[]> arguments() {
+                    return Optional.empty();
+                }
+
+                @Override
+                public Optional<Instant> startInstant() {
+                    return Optional.empty();
+                }
+
+                @Override
+                public Optional<Duration> totalCpuDuration() {
+                    return Optional.empty();
+                }
+
+                @Override
+                public Optional<String> user() {
+                    return Optional.empty();
+                }
+            }
+        };
+
+        final String expectedJavaHome = TestRunner.IS_WINDOWS
+                ? "c:\\path\\to\\java\\home"
+                : "/path/to/java/home";
+
+        assertEquals(expectedJavaHome, runner.environment().get("JAVA_HOME"));
+
+        assertEquals(expectedJavaHome,
+                runner.createProcessBuilder(List.of(), false).environment()
+                        .get("JAVA_HOME"));
+
+        var ProcessBuilder = runner.createProcessBuilder(List.of(), false);
+        assertEquals(expectedJavaHome,
+                ProcessBuilder.environment().get("JAVA_HOME"));
+    }
+
 }
