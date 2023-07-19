73de52999e96c87a3dd7525ab6dab6235f726441
taefi <taefi.soroosh@gmail.com>
2023-07-19 16:02:23 +0300

    fix: provide push endpoint to atmosphere handler to make it available in prod
    Fixes: #786

--- packages/java/endpoint/src/main/java/dev/hilla/push/PushConfigurer.java
+++ packages/java/endpoint/src/main/java/dev/hilla/push/PushConfigurer.java
@@ -1,13 +1,17 @@
 package dev.hilla.push;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-import org.atmosphere.cpr.ApplicationConfig;
-import org.atmosphere.cpr.AtmosphereServlet;
-import org.atmosphere.cpr.ContainerInitializer;
+import org.atmosphere.client.TrackMessageSizeInterceptor;
+import org.atmosphere.cpr.*;
+import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor;
+import org.atmosphere.interceptor.SuspendTrackerInterceptor;
+import org.atmosphere.util.SimpleBroadcaster;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -24,32 +28,48 @@ import dev.hilla.EndpointInvoker;
 @ConditionalOnFeatureFlag(PushMessageHandler.PUSH_FEATURE_FLAG)
 public class PushConfigurer {
 
+    private static final String HILLA_PUSH_PATH = "/HILLA/push";
+
     @Bean
-    public PushMessageHandler pushMessageHandler(
-            EndpointInvoker endpointInvoker) {
+    PushEndpoint pushEndpoint() {
+        return new PushEndpoint();
+    }
+
+    @Bean
+    PushMessageHandler pushMessageHandler(EndpointInvoker endpointInvoker) {
         return new PushMessageHandler(endpointInvoker);
     }
 
     @Bean
-    public EmbeddedAtmosphereInitializer atmosphereInitializer() {
+    EmbeddedAtmosphereInitializer atmosphereInitializer() {
         return new EmbeddedAtmosphereInitializer();
     }
 
     @Bean
-    public ServletRegistrationBean<AtmosphereServlet> atmosphereServlet() {
+    ServletRegistrationBean<AtmosphereServlet> atmosphereServlet(
+            PushEndpoint pushEndpoint) {
+        AtmosphereServlet atmosphereServlet = new AtmosphereServlet();
         ServletRegistrationBean<AtmosphereServlet> registration = new ServletRegistrationBean<>(
-                new AtmosphereServlet(), "/HILLA/push");
+                atmosphereServlet, HILLA_PUSH_PATH);
+
+        List<AtmosphereInterceptor> interceptors = Arrays.asList(
+                new AtmosphereResourceLifecycleInterceptor(),
+                new TrackMessageSizeInterceptor(),
+                new SuspendTrackerInterceptor());
+        AtmosphereFramework fw = atmosphereServlet.framework();
+        fw.setDefaultBroadcasterClassName(SimpleBroadcaster.class.getName());
+        fw.addAtmosphereHandler(HILLA_PUSH_PATH, pushEndpoint, interceptors);
 
         // Override the global mapping set by Flow
         registration.addInitParameter(ApplicationConfig.JSR356_MAPPING_PATH,
-                "/HILLA/push");
+                HILLA_PUSH_PATH);
         registration.setLoadOnStartup(0);
         registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
         return registration;
     }
 
-    private static class EmbeddedAtmosphereInitializer
-            extends ContainerInitializer implements ServletContextInitializer {
+    static class EmbeddedAtmosphereInitializer extends ContainerInitializer
+            implements ServletContextInitializer {
 
         @Override
         public void onStartup(ServletContext servletContext)
