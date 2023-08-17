b5336d8dd580a4b7b198c56c094e8106709c5bab
Luciano Vernaschi <luciano@vaadin.com>
2023-08-17 13:49:42 +0200

    Fix DoS-related issues

--- packages/ts/form/test/Validators.test.ts
+++ packages/ts/form/test/Validators.test.ts
@@ -292,7 +292,7 @@ describe('@hilla/form', () => {
     // });
 
     it('Pattern', () => {
-      let validator = new Pattern(/^(\+\d+)?([ -]?\d+){4,14}$/u);
+      let validator = new Pattern(/^\+?\d(?:[ -]?\d){3,13}$/u);
       assert.isNotTrue(validator.impliesRequired);
       assert.isFalse(validator.validate(''));
       assert.isFalse(validator.validate('123'));
@@ -303,10 +303,10 @@ describe('@hilla/form', () => {
       validator = new Pattern('\\d+');
       assert.isTrue(validator.validate('1'));
       assert.isFalse(validator.validate('a'));
-      validator = new Pattern({ regexp: '\\w+\\\\' });
+      validator = new Pattern({ regexp: '\\w{1,10}\\\\' });
       assert.isFalse(validator.validate('a'));
       assert.isTrue(validator.validate('a\\'));
-      validator = new Pattern({ regexp: /\w+\\/u });
+      validator = new Pattern({ regexp: /\w{1,10}\\/u });
       assert.isFalse(validator.validate('a'));
       assert.isTrue(validator.validate('a\\'));
       validator = new Pattern({ regexp: "^[\\p{L}\\s\\.,']+$" });
