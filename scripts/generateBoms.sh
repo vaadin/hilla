c7303ee75ddb3333dfd0255bddbf9d9ac61c62e0
Zhe Sun <zhe@vaadin.com>
2023-08-22 13:35:38 +0300

    chore: add script to generate lumo/material theme file

--- scripts/generateBoms.sh
+++ scripts/generateBoms.sh
@@ -16,3 +16,21 @@ $cmd || exit 1
 # copy generated poms to the final place
 cp scripts/generator/results/hilla-versions.json packages/java/hilla/hilla-versions.json
 cp scripts/generator/results/hilla-react-versions.json packages/java/hilla-react/hilla-react-versions.json
+
+echo "Copied the theme file from flow-components to hilla and hilla-react"
+# download the file from flow-components
+lumoFile="https://raw.githubusercontent.com/vaadin/flow-components/main/vaadin-lumo-theme-flow-parent/vaadin-lumo-theme-flow/src/main/java/com/vaadin/flow/theme/lumo/Lumo.java"
+materialFile="https://raw.githubusercontent.com/vaadin/flow-components/main/vaadin-material-theme-flow-parent/vaadin-material-theme-flow/src/main/java/com/vaadin/flow/theme/material/Material.java"
+curl -l -s $lumoFile > ./scripts/generator/results/Lumo.java
+curl -l -s $materialFile > ./scripts/generator/results/Material.java
+
+# remove @JsModule and @NpmPackage annotation and their import lines
+sed -i '/JsModule\|NpmPackage/d' ./scripts/generator/results/Lumo.java ./scripts/generator/results/Material.java
+
+# copy the theme files to hilla and hilla-react
+mkdir -p ./packages/java/hilla-react/src/main/java/dev/hilla/
+mkdir -p ./packages/java/hilla/src/main/java/dev/hilla
+cp scripts/generator/results/Lumo.java packages/java/hilla-react/src/main/java/dev/hilla/Lumo.java
+cp scripts/generator/results/Lumo.java packages/java/hilla/src/main/java/dev/hilla/Lumo.java
+cp scripts/generator/results/Material.java packages/java/hilla-react/src/main/java/dev/hilla/Material.java
+cp scripts/generator/results/Material.java packages/java/hilla/src/main/java/dev/hilla/Material.java
