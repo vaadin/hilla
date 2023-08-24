7f1b404ca19df925666d2ad136cf60b5ad6255aa
Artur Signell <artur@vaadin.com>
2023-08-24 10:12:07 +0300

    feat: Callback method for hotswap solutions
    Hotswapper.onHotswap can be called by a list of changed classes (as strings) by any hotswap solution and Hilla will react to the changed classes in an appropriate way, for instance by re-generating the endpoint metadata and TypeScript. The hotswap solution is not supposed to do any pre-filtering of the changed classes, this is done by the method itself.

---
+++ packages/java/endpoint/src/main/java/dev/hilla/Hotswapper.java
@@ -0,0 +1,78 @@
+package dev.hilla;
+
+import java.io.IOException;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Takes care of updating internals of Hilla that need updates when application
+ * classes are updated.
+ */
+public class Hotswapper {
+
+    private static Logger getLogger() {
+        return LoggerFactory.getLogger(Hotswapper.class);
+    }
+
+    /**
+     * Called by hot swap solutions when one or more classes have been updated.
+     * <p>
+     * The hot swap solution should not pre-filter the classes but pass
+     * everything to this method.
+     * 
+     * @param changedClasses
+     *            the classes that have been added or modified
+     */
+    public static void onHotswap(String[] changedClasses) {
+        try {
+            if (affectsEndpoints(changedClasses)) {
+                EndpointCodeGenerator.getInstance().update();
+            }
+        } catch (IOException e) {
+            getLogger().error("Failed to re-generated TypeScript code");
+        }
+    }
+
+    /**
+     * Checks if changes in the given classes can affect the generated
+     * TypeScript for endpoints.
+     * 
+     * @param changedClasses
+     *            the changed classes
+     * @return {@code true} if the classes can affect endpoint generation,
+     *         {@code false} otherwise
+     * @throws IOException
+     */
+    private static boolean affectsEndpoints(String[] changedClasses)
+            throws IOException {
+        Set<String> changedClassesSet = Set.of(changedClasses);
+        Set<String> classesUsedInEndpoints = EndpointCodeGenerator.getInstance()
+                .getClassesUsedInOpenApi();
+        for (String classUsedInEndpoints : classesUsedInEndpoints) {
+            if (changedClassesSet.contains(classUsedInEndpoints)) {
+                getLogger().debug("The changed class " + classesUsedInEndpoints
+                        + " is used in an endpoint");
+                return true;
+            }
+        }
+
+        for (String changedClass : changedClasses) {
+            try {
+                Class<?> cls = Class.forName(changedClass);
+                if (cls.getAnnotation(Endpoint.class) != null
+                        || cls.getAnnotation(EndpointExposed.class) != null) {
+                    getLogger().debug(
+                            "An endpoint annotation has been added to the class "
+                                    + classesUsedInEndpoints);
+                    return true;
+                }
+
+            } catch (ClassNotFoundException e) {
+                getLogger().error("Unable to find class " + changedClass, e);
+            }
+        }
+        return false;
+    }
+}
--- packages/java/endpoint/src/main/java/dev/hilla/OpenAPIUtil.java
+++ packages/java/endpoint/src/main/java/dev/hilla/OpenAPIUtil.java
@@ -1,6 +1,8 @@
 package dev.hilla;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,6 +11,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import dev.hilla.engine.EngineConfiguration;
+import dev.hilla.engine.ParserProcessor;
 
 /**
  * Utilities for interacting with the generated openapi.json.
@@ -16,8 +20,42 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class OpenAPIUtil {
 
     /**
-     * Parses the given open api and finds all used classes (endpoints,
-     * parameter and return types).
+     * Reads the open api file from the build directory.
+     * 
+     * @param buildDirectory
+     *            the build directory, {@code target} if running with Maven
+     * @return the contents of the generated openapi.json
+     * @throws IOException
+     *             if something went wrong
+     */
+    public static String getCurrentOpenAPI(Path buildDirectory)
+            throws IOException {
+        EngineConfiguration engineConfiguration = EngineConfiguration
+                .loadDirectory(buildDirectory);
+        return Files.readString(engineConfiguration.getOpenAPIFile());
+    }
+
+    /**
+     * Generate a new openapi.json and return it, based on the classes in the
+     * build directory.
+     * 
+     * @param buildDirectory
+     *            the build directory, {@code target} if running with Maven
+     * @return the contents of the generated openapi.json
+     * @throws IOException
+     *             if something went wrong
+     */
+    public static String generateOpenAPI(Path buildDirectory)
+            throws IOException {
+        EngineConfiguration engineConfiguration = EngineConfiguration
+                .loadDirectory(buildDirectory);
+        ParserProcessor parserProcessor = new ParserProcessor(
+                engineConfiguration, OpenAPIUtil.class.getClassLoader());
+        return parserProcessor.createOpenAPI();
+    }
+
+    /**
+     * Parses the given open api and finds all used classes.
      *
      * @param openApiAsText
      *            the open api JSON as text
