d4c42e8621bf179993a3c3ee35d36bc48e50a810
Manolo Carrasco <manolo@vaadin.com>
2023-08-29 10:11:53 +0200

    move dependencies of generator, and simplify script

--- scripts/generator/generate.js
+++ scripts/generator/generate.js
@@ -1,24 +1,14 @@
 const path = require('path');
 const fs = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
 
 const writer = require('./src/writer');
 const transformer = require('./src/transformer');
 
-if (!argv['platform']) {
-    console.log('Specify platform version as \'--platform=11.12.13\'');
-    process.exit(1);
-}
-
-if (!argv['versions']) {
-    console.log('Specify product version file as \'--versions=versions.json\'');
-    process.exit(1);
-}
-
-const versionsFileName = path.resolve(argv['versions']);
+const hillaVersion = process.argv[2];
+const versionsFileName = process.argv[3];
 const resultsDir = path.resolve(`${__dirname}/results`);
 
-const inputVersions = require(versionsFileName);
+const inputVersions = JSON.parse(fs.readFileSync(versionsFileName));
 
 function getTemplateFilePath(filename) {
     return path.resolve(`${__dirname}/templates/${filename}`);
@@ -34,8 +24,7 @@ const hillaJsonResultFileName = getResultsFilePath('hilla-versions.json');
 const hillaReactJsonTemplateFileName = getTemplateFilePath('template-hilla-react-versions.json');
 const hillaReactJsonResultFileName = getResultsFilePath('hilla-react-versions.json');
 
-const platform=argv['platform'];
-const versions = transformer.transformVersions(inputVersions, platform, argv['useSnapshots']);
+const versions = transformer.transformVersions(inputVersions, hillaVersion, false);
 
 if (!fs.existsSync(resultsDir)) {
     fs.mkdirSync(resultsDir);
@@ -58,9 +47,8 @@ writer.writeReleaseNotes(versions, releaseNotesTemplateFileName, releaseNotesRes
 writer.writeReleaseNotes(versions, releaseNotesMaintenanceTemplateFileName, releaseNotesMaintenanceResultFileName);
 writer.writeReleaseNotes(versions, releaseNotesPrereleaseTemplateFileName, releaseNotesPrereleaseResultFileName);
 
-const hilla = process.env.HILLA || platform.replace(/^24/, 2);
-versions.core.hilla = {javaVersion: hilla};
+versions.core.hilla = {javaVersion: hillaVersion};
 // write hilla version to hilla-react-versions.json as platform
-const hillaVersions = {platform : hilla};
+const hillaVersions = {platform : hillaVersion};
 writer.writeSeparateJson(hillaVersions.platform, hillaJsonTemplateFileName, hillaJsonResultFileName, "platform");
 writer.writeSeparateJson(hillaVersions.platform, hillaReactJsonTemplateFileName, hillaReactJsonResultFileName, "platform");
