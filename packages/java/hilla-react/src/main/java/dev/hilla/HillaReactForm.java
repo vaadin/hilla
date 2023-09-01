a88821b0547902d066f2c5bc41e63f69c9db8062
Anton Platonov <anton@vaadin.com>
2023-09-01 16:49:48 +0300

    feat(hilla-react): auto-install @hilla/react-form npm package (#1200)
    Fixes #1181

---
+++ packages/java/hilla-react/src/main/java/dev/hilla/HillaReactForm.java
@@ -0,0 +1,10 @@
+package dev.hilla;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+/**
+ * Empty class that adds <code>@hilla/react-form</code> npm package dependency.
+ */
+@NpmPackage(value = "@hilla/react-form", version = "2.2.0-alpha10")
+public class HillaReactForm {
+}
---
+++ packages/java/hilla/src/main/java/dev/hilla/HillaForm.java
@@ -0,0 +1,10 @@
+package dev.hilla;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+/**
+ * Empty class that adds <code>@hilla/form</code> npm package dependency.
+ */
+@NpmPackage(value = "@hilla/form", version = "2.2.0-alpha10")
+public class HillaForm {
+}
