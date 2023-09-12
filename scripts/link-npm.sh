cc2356338b8edd3fc3ef53df32d87e65d1b155e3
Artur <artur@vaadin.com>
2023-09-12 09:39:41 +0300

    chore: Helper script to link test app to local checkout (#1243)

---
+++ scripts/link-npm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run this script from your project directory to link all Hilla npm packages to this checkout of Hilla
+# e.g.
+# cd /tmp
+# npx @hilla/cli init  myproj
+# cd myproj
+# ~/projects/hilla/scripts/link-npm.sh
+
+hillaDir=`dirname $0`/..
+
+pkgs=""
+for tsPkg in $hillaDir/packages/ts/*
+do
+  pkgs+=" "@hilla/`basename $tsPkg`@$tsPkg
+done
+
+npm i $pkgs
