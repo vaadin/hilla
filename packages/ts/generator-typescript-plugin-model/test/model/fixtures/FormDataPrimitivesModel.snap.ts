bc60d04ecd4051a246572bee643b6116aa4f75c3
Vlad Rindevich <vladrin@vaadin.com>
2023-09-12 11:17:39 +0300

    refactor(generator-typescript-plugin-model): update code according to #1252

--- packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormDataPrimitivesModel.snap.ts
+++ packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormDataPrimitivesModel.snap.ts
@@ -1,39 +1,39 @@
-import { _getPropertyModel as _getPropertyModel_1, BooleanModel as BooleanModel_1, NumberModel as NumberModel_1, ObjectModel as ObjectModel_1, StringModel as StringModel_1 } from "@hilla/form";
+import { _getPropertyModel as _getPropertyModel_1, BooleanModel as BooleanModel_1, makeObjectEmptyValueCreator as makeObjectEmptyValueCreator_1, NumberModel as NumberModel_1, ObjectModel as ObjectModel_1, StringModel as StringModel_1 } from "@hilla/form";
 import type FormDataPrimitives_1 from "./FormDataPrimitives.js";
 class FormDataPrimitivesModel<T extends FormDataPrimitives_1 = FormDataPrimitives_1> extends ObjectModel_1<T> {
-    declare static createEmptyValue: () => FormDataPrimitives_1;
+    static override createEmptyValue = makeObjectEmptyValueCreator_1(FormDataPrimitivesModel);
     get stringProp(): StringModel_1 {
-        return this[_getPropertyModel_1]("stringProp", StringModel_1, [false]) as StringModel_1;
+        return this[_getPropertyModel_1]("stringProp", (parent, key) => new StringModel_1(parent, key, false));
     }
     get longWrapperProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("longWrapperProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("longWrapperProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get longProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("longProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("longProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get integerWrapperProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("integerWrapperProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("integerWrapperProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get integerProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("integerProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("integerProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get doubleWrapperProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("doubleWrapperProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("doubleWrapperProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get doubleProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("doubleProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("doubleProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get floatWrapperProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("floatWrapperProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("floatWrapperProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get floatProp(): NumberModel_1 {
-        return this[_getPropertyModel_1]("floatProp", NumberModel_1, [false]) as NumberModel_1;
+        return this[_getPropertyModel_1]("floatProp", (parent, key) => new NumberModel_1(parent, key, false));
     }
     get booleanWrapperProp(): BooleanModel_1 {
-        return this[_getPropertyModel_1]("booleanWrapperProp", BooleanModel_1, [false]) as BooleanModel_1;
+        return this[_getPropertyModel_1]("booleanWrapperProp", (parent, key) => new BooleanModel_1(parent, key, false));
     }
     get booleanProp(): BooleanModel_1 {
-        return this[_getPropertyModel_1]("booleanProp", BooleanModel_1, [false]) as BooleanModel_1;
+        return this[_getPropertyModel_1]("booleanProp", (parent, key) => new BooleanModel_1(parent, key, false));
     }
 }
 export default FormDataPrimitivesModel;
--- packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormRecordTypesModel.snap.ts
+++ packages/ts/generator-typescript-plugin-model/test/model/fixtures/FormRecordTypesModel.snap.ts
@@ -1,26 +1,26 @@
-import { _getPropertyModel as _getPropertyModel_1, ObjectModel as ObjectModel_1 } from "@hilla/form";
+import { _getPropertyModel as _getPropertyModel_1, makeObjectEmptyValueCreator as makeObjectEmptyValueCreator_1, ObjectModel as ObjectModel_1 } from "@hilla/form";
 import type FormEntityHierarchy_1 from "./FormEntityHierarchy.js";
 import type FormOptionalTypes_1 from "./FormOptionalTypes.js";
 import type FormRecordTypes_1 from "./FormRecordTypes.js";
 class FormRecordTypesModel<T extends FormRecordTypes_1 = FormRecordTypes_1> extends ObjectModel_1<T> {
-    declare static createEmptyValue: () => FormRecordTypes_1;
+    static override createEmptyValue = makeObjectEmptyValueCreator_1(FormRecordTypesModel);
     get stringMap(): ObjectModel_1<Record<string, string>> {
-        return this[_getPropertyModel_1]("stringMap", ObjectModel_1, [false]) as ObjectModel_1<Record<string, string>>;
+        return this[_getPropertyModel_1]("stringMap", (parent, key) => new ObjectModel_1(parent, key, false));
     }
     get entityHierarchyMap(): ObjectModel_1<Record<string, FormEntityHierarchy_1>> {
-        return this[_getPropertyModel_1]("entityHierarchyMap", ObjectModel_1, [false]) as ObjectModel_1<Record<string, FormEntityHierarchy_1>>;
+        return this[_getPropertyModel_1]("entityHierarchyMap", (parent, key) => new ObjectModel_1(parent, key, false));
     }
     get stringListMap(): ObjectModel_1<Record<string, ReadonlyArray<string>>> {
-        return this[_getPropertyModel_1]("stringListMap", ObjectModel_1, [false]) as ObjectModel_1<Record<string, ReadonlyArray<string>>>;
+        return this[_getPropertyModel_1]("stringListMap", (parent, key) => new ObjectModel_1(parent, key, false));
     }
     get selfReferenceMap(): ObjectModel_1<Record<string, FormRecordTypes_1>> {
-        return this[_getPropertyModel_1]("selfReferenceMap", ObjectModel_1, [false]) as ObjectModel_1<Record<string, FormRecordTypes_1>>;
+        return this[_getPropertyModel_1]("selfReferenceMap", (parent, key) => new ObjectModel_1(parent, key, false));
     }
     get complexMap(): ObjectModel_1<Record<string, Record<string, ReadonlyArray<FormOptionalTypes_1>>>> {
-        return this[_getPropertyModel_1]("complexMap", ObjectModel_1, [false]) as ObjectModel_1<Record<string, Record<string, ReadonlyArray<FormOptionalTypes_1>>>>;
+        return this[_getPropertyModel_1]("complexMap", (parent, key) => new ObjectModel_1(parent, key, false));
     }
     get objectMap(): ObjectModel_1<Record<string, unknown>> {
-        return this[_getPropertyModel_1]("objectMap", ObjectModel_1, [false]) as ObjectModel_1<Record<string, unknown>>;
+        return this[_getPropertyModel_1]("objectMap", (parent, key) => new ObjectModel_1(parent, key, false));
     }
 }
 export default FormRecordTypesModel;
