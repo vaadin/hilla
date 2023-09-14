6bb54f8525587be56d9dbe836720ab3140196fc2
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-09-14 15:15:14 +0200

    feat: customize visible columns in auto grid

--- packages/ts/react-grid/test/TestModels.ts
+++ packages/ts/react-grid/test/TestModels.ts
@@ -3,6 +3,7 @@ import { ObjectModel, StringModel, _getPropertyModel } from '@hilla/form';
 export interface Person {
   firstName: string;
   lastName: string;
+  email: string;
 }
 
 export class PersonModel<T extends Person = Person> extends ObjectModel<T> {
@@ -14,5 +15,9 @@ export class PersonModel<T extends Person = Person> extends ObjectModel<T> {
 
   get lastName(): StringModel {
     return this[_getPropertyModel]('firstName', StringModel, [false]);
+  }
+
+  get email(): StringModel {
+    return this[_getPropertyModel]('email', StringModel, [false]);
   }
 }
