b89075ac521ca2be4b97ec050cf9badc145374e8
Artur Signell <artur@vaadin.com>
2023-09-20 14:40:24 +0300

    fix: Use correct version for auth

--- packages/java/hilla-react/src/main/java/dev/hilla/HillaReactAuth.java
+++ packages/java/hilla-react/src/main/java/dev/hilla/HillaReactAuth.java
@@ -5,6 +5,6 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 /**
  * Empty class that adds <code>@hilla/react-auth</code> npm package dependency.
  */
-@NpmPackage(value = "@hilla/react-auth", version = "2.2.0-beta1")
+@NpmPackage(value = "@hilla/react-auth", version = "2.3.0-alpha3")
 public class HillaReactAuth {
 }
