105f59c74c01cb85b1e814e25e47a757309ea423
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-09-20 11:04:52 +0200

    remove commercial licence header

--- packages/ts/react-auth/src/ProtectedRoute.tsx
+++ packages/ts/react-auth/src/ProtectedRoute.tsx
@@ -1,12 +1,3 @@
-/* -
- * Copyright (C) 2023 Vaadin Ltd
- *
- * This program is available under Vaadin Commercial License and Service Terms.
- *
- *
- * See <https://vaadin.com/commercial-license-and-service-terms> for the full
- * license.
- */
 import { useContext } from 'react';
 import type { RouteObject } from 'react-router-dom';
 import { type IndexRouteObject, Navigate, type NonIndexRouteObject, useLocation } from 'react-router-dom';
