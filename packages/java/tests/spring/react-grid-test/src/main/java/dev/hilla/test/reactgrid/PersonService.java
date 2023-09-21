b77fcbc4dfcf3f46ca3652cec1f03319494e982a
Artur Signell <artur@vaadin.com>
2023-09-21 08:34:55 +0300

    fix imports

--- packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/PersonService.java
+++ packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/PersonService.java
@@ -1,12 +1,7 @@
 package dev.hilla.test.reactgrid;
 
-import java.util.List;
-
 import dev.hilla.BrowserCallable;
-import dev.hilla.Nullable;
 import dev.hilla.crud.CrudRepositoryService;
-import dev.hilla.crud.filter.Filter;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
