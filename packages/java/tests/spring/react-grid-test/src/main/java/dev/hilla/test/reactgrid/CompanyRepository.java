5531ab5e28c2455b8fdeb169f76afbc7336acd01
Artur Signell <artur@vaadin.com>
2023-09-21 11:20:05 +0300

    format

--- packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/CompanyRepository.java
+++ packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/CompanyRepository.java
@@ -3,7 +3,7 @@ package dev.hilla.test.reactgrid;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CompanyRepository
-        extends JpaRepository<Company, Long>, JpaSpecificationExecutor<Company> {
+public interface CompanyRepository extends JpaRepository<Company, Long>,
+        JpaSpecificationExecutor<Company> {
 
 }
