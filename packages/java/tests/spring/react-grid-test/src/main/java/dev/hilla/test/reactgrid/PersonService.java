181d72f45d3eca78ca24f1348d8f2c99e60189bc
taefi <taefi.soroosh@gmail.com>
2023-09-22 09:56:13 +0300

    remove unused imports

--- packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/PersonService.java
+++ packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/PersonService.java
@@ -2,7 +2,6 @@ package dev.hilla.test.reactgrid;
 
 import dev.hilla.BrowserCallable;
 import dev.hilla.crud.CrudRepositoryService;
-import org.springframework.stereotype.Service;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 
