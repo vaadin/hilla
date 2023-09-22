ae31ce5b9d0f5665604ce529e99b08e67f74e9c3
Artur Signell <artur@vaadin.com>
2023-09-22 18:40:25 +0300

    test: Remove old csrf tests
    It is my understanding that the endpoint tests with and without a context path already test that the csrf token is set correctly

--- packages/java/tests/csrf/frontend/views/helloworld/hello-world-view.ts
+++
@@ -1,9 +0,0 @@
-import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
-
-@customElement('hello-world-view')
-export class HelloWorldView extends LitElement {
-  render() {
-    return html` <div>HelloWorldView</div> `;
-  }
-}
