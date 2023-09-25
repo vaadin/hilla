adb25d747b964ffc1921bc6156ca756105da6dea
Vlad Rindevich <vladrin@vaadin.com>
2023-09-25 13:18:46 +0300

    fix: update type name

--- packages/java/tests/spring/react-grid-test/frontend/views/ReadOnlyGridWithHeaderFilter.tsx
+++ packages/java/tests/spring/react-grid-test/frontend/views/ReadOnlyGridWithHeaderFilter.tsx
@@ -1,7 +1,4 @@
-import {
-  AbstractModel,
-  type EmptyModelConstructor,
-} from '@hilla/form';
+import { AbstractModel, type DetachedModelConstructor } from '@hilla/form';
 import { Button } from '@hilla/react-components/Button.js';
 import { AutoGrid, CrudService } from '@hilla/react-grid';
 import CompanyModel from 'Frontend/generated/dev/hilla/test/reactgrid/CompanyModel';
@@ -10,7 +7,7 @@ import { CompanyService, PersonService } from 'Frontend/generated/endpoints';
 import { useState } from 'react';
 
 export function ReadOnlyGridWithHeaderFilters() {
-  const [model, setModel] = useState<EmptyModelConstructor<AbstractModel>[]>([PersonModel]);
+  const [model, setModel] = useState<DetachedModelConstructor<AbstractModel>[]>([PersonModel]);
   const [service, setService] = useState<CrudService<any>>(PersonService);
   const [headerFilters, setHeaderFilters] = useState(true);
   return (
