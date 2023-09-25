3d8dbcdda38c29e7ce4c243017f72193d1eca44e
Luciano Vernaschi <cromoteca@users.noreply.github.com>
2023-09-25 17:31:15 +0200

    chore: update test model (#1347)

--- packages/ts/generator-typescript-plugin-subtypes/test/push-type/fixtures/AddEventModel.snap.ts
+++ packages/ts/generator-typescript-plugin-subtypes/test/push-type/fixtures/AddEventModel.snap.ts
@@ -1,10 +1,10 @@
-import { _getPropertyModel as _getPropertyModel_1, StringModel as StringModel_1 } from "@hilla/form";
+import { _getPropertyModel as _getPropertyModel_1, makeObjectEmptyValueCreator as makeObjectEmptyValueCreator_1, StringModel as StringModel_1 } from "@hilla/form";
 import type AddEvent_1 from "./AddEvent.js";
 import BaseEventModel_1 from "./BaseEventModel.js";
 class AddEventModel<T extends AddEvent_1 = AddEvent_1> extends BaseEventModel_1<T> {
-    declare static createEmptyValue: () => AddEvent_1;
+    static override createEmptyValue = makeObjectEmptyValueCreator_1(AddEventModel);
     get item(): StringModel_1 {
-        return this[_getPropertyModel_1]("item", StringModel_1, [true]) as StringModel_1;
+        return this[_getPropertyModel_1]("item", (parent, key) => new StringModel_1(parent, key, true));
     }
 }
 export default AddEventModel;
