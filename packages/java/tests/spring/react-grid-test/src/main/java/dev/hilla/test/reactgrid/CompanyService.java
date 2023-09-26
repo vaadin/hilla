77a34c2c77e438fe0afdaf17cf22cc7128f493a8
taefi <taefi.soroosh@gmail.com>
2023-09-26 14:35:07 +0300

    retain removed classes

---
+++ packages/java/tests/spring/react-grid-test/src/main/java/dev/hilla/test/reactgrid/CompanyService.java
@@ -0,0 +1,16 @@
+package dev.hilla.test.reactgrid;
+
+import dev.hilla.BrowserCallable;
+import dev.hilla.crud.CrudRepositoryService;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@BrowserCallable
+@AnonymousAllowed
+public class CompanyService
+        extends CrudRepositoryService<Company, Long, CompanyRepository> {
+
+    public CompanyService(CompanyRepository repository) {
+        super(repository);
+    }
+}
