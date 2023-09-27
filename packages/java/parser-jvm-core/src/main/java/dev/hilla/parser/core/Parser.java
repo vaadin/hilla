0b9f6a0d2b67f4242188a204d53f321ff2bf70a0
taefi <taefi.soroosh@gmail.com>
2023-09-27 18:04:06 +0300

    expand imports

--- packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/Parser.java
+++ packages/java/parser-jvm-core/src/main/java/dev/hilla/parser/core/Parser.java
@@ -1,7 +1,16 @@
 package dev.hilla.parser.core;
 
 import java.io.IOException;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Consumer;
 
 import javax.annotation.Nonnull;
