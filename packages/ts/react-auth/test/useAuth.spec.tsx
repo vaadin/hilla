2bde02badf61b3596f88f2aad5108b1bc3f356d4
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-09-27 11:05:47 +0200

    feat(react-auth): allow customizing user type (#1327)
    * feat(react-auth): allow customizing user type
    
    * fix ProtectedRoute
    
    * allow customizing roles accessor

--- packages/ts/react-auth/test/useAuth.spec.tsx
+++ packages/ts/react-auth/test/useAuth.spec.tsx
@@ -1,39 +1,61 @@
 import { expect } from '@esm-bundle/chai';
-import { render, waitFor } from '@testing-library/react';
-import { AuthContext, useAuth } from '../src';
-
-function SuccessfulLoginComponent() {
-  const getAuthenticatedUser = async () => Promise.resolve({ name: 'John', roles: ['admin'] });
-  const auth = useAuth(getAuthenticatedUser);
-
-  return (
-    <AuthContext.Provider value={auth}>
-      <div>{auth.state.user?.name}</div>
-    </AuthContext.Provider>
-  );
-}
-
-function FailedLoginComponent() {
-  const getAuthenticatedUser = async () => Promise.resolve(undefined);
-  const auth = useAuth(getAuthenticatedUser);
-
-  return (
-    <AuthContext.Provider value={auth}>
-      <div>{auth.state.user ? auth.state.user.name : 'Not logged in'}</div>
-    </AuthContext.Provider>
-  );
-}
+import { renderHook, waitFor } from '@testing-library/react';
+import { configureAuth } from '../src';
 
 describe('@hilla/react-auth', () => {
   describe('useAuth', () => {
-    it('should be able to access user information after login', async () => {
-      const { getByText } = render(<SuccessfulLoginComponent />);
-      await waitFor(() => expect(getByText('John')).to.exist);
+    it('should provide user in state', async () => {
+      const user = { customRoles: ['admin'] };
+      const { AuthProvider, useAuth } = configureAuth(async () => Promise.resolve(user));
+      const { result } = renderHook(() => useAuth(), { wrapper: AuthProvider });
+
+      await waitFor(() => expect(result.current.state.user).to.equal(user));
     });
 
-    it('should not be able to access user information after login', async () => {
-      const { getByText } = render(<FailedLoginComponent />);
-      await waitFor(() => expect(getByText('Not logged in')).to.exist);
+    describe('hasAccess', () => {
+      it('should not give access if user has no roles', async () => {
+        const user = {};
+        const { AuthProvider, useAuth } = configureAuth(async () => Promise.resolve(user));
+
+        const { result } = renderHook(() => useAuth(), { wrapper: AuthProvider });
+
+        await waitFor(() => expect(result.current.state.user).to.equal(user));
+        expect(result.current.hasAccess({ rolesAllowed: ['admin'] })).to.be.false;
+        expect(result.current.hasAccess({ rolesAllowed: ['superadmin'] })).to.be.false;
+      });
+
+      it('should handle incompatible roles property', async () => {
+        const user = { roles: 'admin' };
+        const { AuthProvider, useAuth } = configureAuth(async () => Promise.resolve(user));
+
+        const { result } = renderHook(() => useAuth(), { wrapper: AuthProvider });
+
+        await waitFor(() => expect(result.current.state.user).to.equal(user));
+        expect(result.current.hasAccess({ rolesAllowed: ['admin'] })).to.be.false;
+        expect(result.current.hasAccess({ rolesAllowed: ['superadmin'] })).to.be.false;
+      });
+
+      it('should use roles property by convention', async () => {
+        const user = { roles: ['admin'] };
+        const { AuthProvider, useAuth } = configureAuth(async () => Promise.resolve(user));
+        const { result } = renderHook(() => useAuth(), { wrapper: AuthProvider });
+
+        await waitFor(() => expect(result.current.state.user).to.equal(user));
+        expect(result.current.hasAccess({ rolesAllowed: ['admin'] })).to.be.true;
+        expect(result.current.hasAccess({ rolesAllowed: ['superadmin'] })).to.be.false;
+      });
+
+      it('should use custom roles accessor when configured', async () => {
+        const user = { roles: ['superadmin'], customRoles: ['admin'] };
+        const { AuthProvider, useAuth } = configureAuth(async () => Promise.resolve(user), {
+          getRoles: (authenticatedUser) => authenticatedUser.customRoles,
+        });
+        const { result } = renderHook(() => useAuth(), { wrapper: AuthProvider });
+
+        await waitFor(() => expect(result.current.state.user).to.equal(user));
+        expect(result.current.hasAccess({ rolesAllowed: ['admin'] })).to.be.true;
+        expect(result.current.hasAccess({ rolesAllowed: ['superadmin'] })).to.be.false;
+      });
     });
   });
 });
