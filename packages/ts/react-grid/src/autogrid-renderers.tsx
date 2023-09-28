87a790cd64291bf0095291d8674097b67623afd7
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-09-28 12:30:56 +0200

    refactor

--- packages/ts/react-grid/src/autogrid-renderers.tsx
+++ packages/ts/react-grid/src/autogrid-renderers.tsx
@@ -35,8 +35,9 @@ export function AutoGridBooleanRenderer<TItem>({ item }: RendererOptions<TItem>)
   return <Icon aria-label="true" style={{ color: 'var(--lumo-secondary-text-color)' }} icon="lumo:minus" />;
 }
 
-function tryFormatDateTime(value: string, format: Intl.DateTimeFormat): string {
+function tryFormatDateTime(value: string, options?: Intl.DateTimeFormatOptions): string {
   try {
+    const format = new Intl.DateTimeFormat(undefined, options);
     return format.format(new Date(value));
   } catch (e) {
     return '';
@@ -46,32 +47,33 @@ function tryFormatDateTime(value: string, format: Intl.DateTimeFormat): string {
 export function AutoGridDateRenderer<TItem>({ item }: RendererOptions<TItem>): JSX.Element {
   const context = useContext(ColumnContext)!;
   const value = getColumnValue(context, item);
-  const format = new Intl.DateTimeFormat();
-  const formatted = value ? tryFormatDateTime(value, format) : '';
+  const formatted = value ? tryFormatDateTime(value) : '';
   return <span style={{ fontVariantNumeric: 'tabular-nums' }}>{formatted}</span>;
 }
 
 export function AutoGridTimeRenderer<TItem>({ item }: RendererOptions<TItem>): JSX.Element {
   const context = useContext(ColumnContext)!;
   const value = getColumnValue(context, item) as string;
-  const format = new Intl.DateTimeFormat(undefined, {
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-  const formatted = value ? tryFormatDateTime(`2000-01-01T${value}`, format) : '';
+  const formatted = value
+    ? tryFormatDateTime(`2000-01-01T${value}`, {
+        hour: 'numeric',
+        minute: 'numeric',
+      })
+    : '';
   return <span style={{ fontVariantNumeric: 'tabular-nums' }}>{formatted}</span>;
 }
 
 export function AutoGridDateTimeRenderer<TItem>({ item }: RendererOptions<TItem>): JSX.Element {
   const context = useContext(ColumnContext)!;
   const value = getColumnValue(context, item);
-  const format = new Intl.DateTimeFormat(undefined, {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-  const formatted = value ? tryFormatDateTime(value, format) : '';
+  const formatted = value
+    ? tryFormatDateTime(value, {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+      })
+    : '';
   return <span style={{ fontVariantNumeric: 'tabular-nums' }}>{formatted}</span>;
 }
