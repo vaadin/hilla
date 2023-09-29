22b813f5b1953a856bef714c745ea216b6b62452
Anton Platonov <anton@vaadin.com>
2023-09-29 20:19:18 +0300

    update DetachedModelConstructor type

--- packages/ts/form/src/Models.ts
+++ packages/ts/form/src/Models.ts
@@ -43,11 +43,14 @@ export interface ModelOptions<T> {
   meta?: ModelMetadata;
 }
 
-export type DetachedModelConstructor<M> = new (
-  parent: typeof modelDetachedParent,
-  key: '$value$',
-  optional: boolean,
-) => M;
+export type DetachedModelConstructor<M> = {
+  prototype: M;
+  new (
+    parent: typeof modelDetachedParent,
+    key: '$value$',
+    optional: boolean,
+  ): M;
+};
 
 export function createDetachedModel<M extends AbstractModel>(type: DetachedModelConstructor<M>): M {
   return new type(modelDetachedParent, '$value$', false);
