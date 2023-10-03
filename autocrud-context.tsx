64dd5d9cfee030f0ace8acce7a2bb6b3c230006d
Artur Signell <artur@vaadin.com>
2023-10-03 21:41:17 +0300

    delete file

--- autocrud-context.tsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-export interface AutoCrudContextType<TItem> {
-  refreshGrid: () => void;
-}
-
-export const AutoCrudContext = createContext<AutoCrudContextType<any> | undefined>(undefined);
