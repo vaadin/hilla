51d2212e6f34ba0d3eeb53f33325dd0b8cfd6dda
Anton Platonov <platosha@gmail.com>
2023-10-03 20:29:07 +0300

    fix(auto-grid): remove css file import
    Non-JS imports break unpredictably depending on the user frontend build
    system. One example is the Hilla docs website, which uses webpack, which
    does not support CSS modules.

--- packages/ts/react-grid/src/header-filter.tsx
+++ packages/ts/react-grid/src/header-filter.tsx
@@ -5,8 +5,23 @@ import { Select, type SelectElement } from '@hilla/react-components/Select.js';
 import { TextField, type TextFieldElement } from '@hilla/react-components/TextField.js';
 import { type ReactElement, type RefObject, useContext, useEffect, useRef, useState } from 'react';
 import { ColumnContext } from './autogrid-column-context.js';
-import css from './header-filter.module.css';
 import Matcher from './types/dev/hilla/crud/filter/PropertyStringFilter/Matcher';
+
+const autoGridFilterWithLessGreaterEqualsStyle = document.createElement('style');
+autoGridFilterWithLessGreaterEqualsStyle.id = 'autoGridFilterWithLessGreaterEquals';
+autoGridFilterWithLessGreaterEqualsStyle.textContent = `
+.autoGridFilterWithLessGreaterEquals {
+  --vaadin-field-default-width: 2em;
+  margin-right: 3px;
+}
+.autoGridFilterWithLessGreaterEquals > vaadin-select-value-button {
+  --_lumo-text-field-overflow-mask-image: none !important;
+}
+
+.autoGridFilterWithLessGreaterEquals::part(toggle-button) {
+  display: none;
+}`;
+document.head.appendChild(autoGridFilterWithLessGreaterEqualsStyle);
 
 function useFilterState(initialMatcher: Matcher) {
   const context = useContext(ColumnContext)!;
@@ -82,7 +97,7 @@ export function NumberHeaderFilter(): ReactElement {
             </Item>
           </ListBox>
         )}
-        className={css.filterWithLessGreaterEquals}
+        className={autoGridFilterWithLessGreaterEqualsStyle.id}
         value={matcher}
       ></Select>
       <NumberField
--- packages/ts/react-grid/src/types.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/unambiguous */
-declare module '*.module.css' {
-  declare const styles: Record<string, string>;
-  export default styles;
-}
