e331e96aa14b7f6bc3a9a2bd649a6b4674d45574
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-10-09 13:05:57 +0200

    feat: add auto grid filter support for java.util.Date (#1488)

--- packages/java/endpoint/src/main/java/dev/hilla/crud/PropertyStringFilterSpecification.java
+++ packages/java/endpoint/src/main/java/dev/hilla/crud/PropertyStringFilterSpecification.java
@@ -1,8 +1,10 @@
 package dev.hilla.crud;
 
+import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
@@ -79,6 +81,23 @@ public class PropertyStringFilterSpecification<T> implements Specification<T> {
             default:
                 break;
             }
+        } else if (isDate(javaType)) {
+            var path = root.<Date> get(filter.getPropertyId());
+            var dateValue = Date.from(LocalDate.parse(value)
+                    .atStartOfDay(ZoneId.systemDefault()).toInstant());
+            switch (filter.getMatcher()) {
+            case EQUALS:
+                return criteriaBuilder.equal(path, dateValue);
+            case CONTAINS:
+                throw new IllegalArgumentException(
+                        "A date cannot be filtered using contains");
+            case GREATER_THAN:
+                return criteriaBuilder.greaterThan(path, dateValue);
+            case LESS_THAN:
+                return criteriaBuilder.lessThan(path, dateValue);
+            default:
+                break;
+            }
         } else if (isLocalDate(javaType)) {
             var path = root.<LocalDate> get(filter.getPropertyId());
             var dateValue = LocalDate.parse(value);
@@ -142,6 +161,10 @@ public class PropertyStringFilterSpecification<T> implements Specification<T> {
 
     private boolean isBoolean(Class<?> javaType) {
         return javaType == boolean.class || javaType == Boolean.class;
+    }
+
+    private boolean isDate(Class<?> javaType) {
+        return javaType == java.util.Date.class;
     }
 
     private boolean isLocalDate(Class<?> javaType) {
