f9759191243d863942abb19f2a09e3a9d850c1b1
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-10-25 14:36:27 +0200

    address review comments

--- packages/ts/react-crud/src/header-filter.tsx
+++ packages/ts/react-crud/src/header-filter.tsx
@@ -74,7 +74,7 @@ function ComparationSelection({ onMatcherChanged, value }: ComparationSelectionP
           </Item>
         </ListBox>
       )}
-      className="autoGridFilterWithLessGreaterEquals"
+      className="auto-grid-comparation-selection"
     ></Select>
   );
 }
