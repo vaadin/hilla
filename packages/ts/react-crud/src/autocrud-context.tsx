ddccb5553f322a145228010fc3260ab8b81f4a71
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-10-26 12:02:39 +0200

    feat: add actions column to auto crud

--- packages/ts/react-crud/src/autocrud-context.tsx
+++ packages/ts/react-crud/src/autocrud-context.tsx
@@ -3,9 +3,8 @@ import type { CrudService } from './crud.js';
 import type { PropertyInfo } from './property-info.js';
 
 export interface AutoCrudContextType<TItem> {
-  service: CrudService<TItem>;
-  properties: PropertyInfo[];
-  refreshGrid(): void;
+  editItem(itemToEdit: TItem): void;
+  deleteItem(itemToDelete: TItem): void;
 }
 
 export const AutoCrudContext = createContext<AutoCrudContextType<any> | undefined>(undefined);
