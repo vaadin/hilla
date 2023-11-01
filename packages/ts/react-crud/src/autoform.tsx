b01a3af990aedb8932545138a854059ab29be16d
Soroosh Taefi <taefi.soroosh@gmail.com>
2023-11-01 23:07:25 +0200

    fix: define autoform field key by propertyInfo name (#1638)

--- packages/ts/react-crud/src/autoform.tsx
+++ packages/ts/react-crud/src/autoform.tsx
@@ -110,16 +110,15 @@ function CustomFormLayout(customFormLayoutProps: CustomFormLayoutProps): JSX.Ele
   weightedTemplate.forEach((row: FieldColSpan[]) => {
     row.forEach((fieldColSpan: FieldColSpan) => {
       const fieldProps = fieldsByPropertyName.get(fieldColSpan.property)!;
-      const spannedField = // React.cloneElement(field, { colspan: fieldColSpan.colSpan });
-        (
-          <AutoFormField
-            key={fieldProps.name}
-            propertyInfo={fieldProps.propertyInfo}
-            form={fieldProps.form}
-            disabled={fieldProps.disabled}
-            colSpan={fieldColSpan.colSpan}
-          />
-        );
+      const spannedField = (
+        <AutoFormField
+          key={fieldProps.propertyInfo.name}
+          propertyInfo={fieldProps.propertyInfo}
+          form={fieldProps.form}
+          disabled={fieldProps.disabled}
+          colSpan={fieldColSpan.colSpan}
+        />
+      );
       spannedFields.push(spannedField);
     });
   });
