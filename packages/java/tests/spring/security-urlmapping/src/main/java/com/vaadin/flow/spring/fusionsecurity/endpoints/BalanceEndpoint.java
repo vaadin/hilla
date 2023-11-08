bedd0cc02d25fb60be8768af1a392fa503137307
Anton Platonov <platosha@gmail.com>
2023-11-08 15:55:08 +0200

    fix(endpoint): support prefix and context path with reactive endpoints (#1543) (CP: 2.2) (#1602)
    fix(endpoint): support prefix and context path with reactive endpoints (#1543)
    
    Fixes #1354
    
    (cherry picked from commit ebe67654f70703c70028230634cef56884914fb2)

--- packages/java/tests/spring/security-urlmapping/src/main/java/com/vaadin/flow/spring/fusionsecurity/endpoints/BalanceEndpoint.java
+++ packages/java/tests/spring/security-urlmapping/src/main/java/com/vaadin/flow/spring/fusionsecurity/endpoints/BalanceEndpoint.java
@@ -2,11 +2,14 @@ package com.vaadin.flow.spring.fusionsecurity.endpoints;
 
 import jakarta.annotation.security.PermitAll;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.flow.spring.fusionsecurity.service.BankService;
 import dev.hilla.Endpoint;
+import reactor.core.publisher.Flux;
 
 @Endpoint
 @PermitAll
@@ -21,5 +24,9 @@ public class BalanceEndpoint {
 
     public void applyForLoan() {
         bankService.applyForLoan();
+    }
+
+    public Flux<BigDecimal> getBalanceUpdates() {
+        return Flux.fromStream(Stream.of(bankService.getBalance()));
     }
 }
