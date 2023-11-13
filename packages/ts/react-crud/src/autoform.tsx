a966d587393f3ea77f203f46eeb539efdd768ba3
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-11-13 09:24:51 +0100

    refactor: expose auto form event types

--- packages/ts/react-crud/src/autoform.tsx
+++ packages/ts/react-crud/src/autoform.tsx
@@ -15,16 +15,16 @@ document.adoptedStyleSheets.unshift(css);
 
 export const emptyItem = Symbol();
 
-type SubmitErrorEvent = {
+export type SubmitErrorEvent = {
   error: unknown;
 };
-type SubmitEvent<TItem> = {
+export type SubmitEvent<TItem> = {
   item: TItem;
 };
-type DeleteErrorEvent = {
+export type DeleteErrorEvent = {
   error: unknown;
 };
-type DeleteEvent<TItem> = {
+export type DeleteEvent<TItem> = {
   item: TItem;
 };
 
