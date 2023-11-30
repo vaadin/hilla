1d8c8273e8c19cd2a3e8f37ffd9a4a03245d8084
Anton Platonov <platosha@gmail.com>
2023-11-30 13:25:29 +0200

    fix(generator-typescript-plugin-model): fix TypeScript type of nullable map and array values in models (#1793)
    Fixes #1724

---
+++ packages/java/tests/spring/endpoints/src/main/java/com/vaadin/flow/connect/KnownTypesEndpoint.java
@@ -0,0 +1,22 @@
+package com.vaadin.flow.connect;
+
+import dev.hilla.Endpoint;
+
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.router.Location;
+
+/**
+ * Endpoint referencing data types known to work with Hilla TypeScript
+ * generator.
+ */
+@Endpoint
+@AnonymousAllowed
+public class KnownTypesEndpoint {
+    /**
+     * @see https://github.com/vaadin/hilla/issues/1724
+     * @return
+     */
+    public Location getLocation() {
+        return new Location("/");
+    }
+}
