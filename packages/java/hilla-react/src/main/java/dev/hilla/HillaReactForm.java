a91313c9822a002f25078dbd88d1ba9640503ff4
Anton Platonov <platosha@gmail.com>
2023-12-07 14:27:33 +0200

    chore(release): npm version 2.5.0-alpha13 (#1866)

--- packages/java/hilla-react/src/main/java/dev/hilla/HillaReactForm.java
+++ packages/java/hilla-react/src/main/java/dev/hilla/HillaReactForm.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 /**
  * Empty class that adds <code>@hilla/react-form</code> npm package dependency.
  */
-@NpmPackage(value = "@hilla/react-form", version = "2.5.0-alpha12")
-@NpmPackage(value = "@hilla/form", version = "2.5.0-alpha12")
+@NpmPackage(value = "@hilla/react-form", version = "2.5.0-alpha13")
+@NpmPackage(value = "@hilla/form", version = "2.5.0-alpha13")
 public class HillaReactForm {
 }
