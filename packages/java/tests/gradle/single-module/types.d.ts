f689e148ac996f3f6efeaecba690573dc0a11984
Luciano Vernaschi <luciano@vaadin.com>
2024-01-11 09:38:33 +0100

    Update types.d.ts in test

--- packages/java/tests/gradle/single-module/types.d.ts
+++ packages/java/tests/gradle/single-module/types.d.ts
@@ -59,3 +59,8 @@ declare module '*.webp' {
   const ref: string;
   export default ref;
 }
+declare module '*.css?inline' {
+  import { CSSResultGroup } from 'lit';
+  const content: CSSResultGroup;
+  export default content;
+}
