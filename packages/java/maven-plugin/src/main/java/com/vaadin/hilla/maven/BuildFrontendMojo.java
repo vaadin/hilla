be93adbd41b230ff5e39f87c8adc38323be57f6a
Manuel Carrasco Moñino <manolo@vaadin.com>
2024-01-30 13:06:15 +0100

    chore: reenable flow specific goals in hilla-maven-plugin (#1999)
    Since platform repackages hilla-maven-plugin instead of flow-maven-plugin
    we should not disable flow specific goals
    There should be code in flow or hilla skiping certain goals based on
    configured or computed flags

--- packages/java/maven-plugin/src/main/java/com/vaadin/hilla/maven/BuildFrontendMojo.java
+++ packages/java/maven-plugin/src/main/java/com/vaadin/hilla/maven/BuildFrontendMojo.java
@@ -9,16 +9,4 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 @Execute(goal = "configure")
 public class BuildFrontendMojo
         extends com.vaadin.flow.plugin.maven.BuildFrontendMojo {
-    /**
-     * Override this to not clean generated frontend files after the build. For
-     * Hilla, the generated files can still be useful for developers after the
-     * build. For example, a developer can use {@code vite.generated.ts} to run
-     * tests with vitest in CI.
-     *
-     * @return {@code false}
-     */
-    @Override
-    protected boolean cleanFrontendFiles() {
-        return false;
-    }
 }
