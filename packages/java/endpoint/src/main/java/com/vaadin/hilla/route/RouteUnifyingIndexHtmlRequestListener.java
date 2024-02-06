2fb508944867588a6a435e73069f7ea0a4dba95d
Kriss Seglins <kriss@vaadin.com>
2024-02-06 13:17:19 +0200

    Add missing import

--- packages/java/endpoint/src/main/java/com/vaadin/hilla/route/RouteUnifyingIndexHtmlRequestListener.java
+++ packages/java/endpoint/src/main/java/com/vaadin/hilla/route/RouteUnifyingIndexHtmlRequestListener.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Index HTML request listener for extracting server-side and client-side views
