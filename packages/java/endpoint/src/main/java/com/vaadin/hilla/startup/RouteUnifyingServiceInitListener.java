f8b564b29f93ec8533cbcd2b3dddc27d0412bfa8
taefi <taefi.soroosh@gmail.com>
2024-03-24 22:31:07 +0200

    centralize client route registration logic in ClientRouteRegistry

--- packages/java/endpoint/src/main/java/com/vaadin/hilla/startup/RouteUnifyingServiceInitListener.java
+++ packages/java/endpoint/src/main/java/com/vaadin/hilla/startup/RouteUnifyingServiceInitListener.java
@@ -16,27 +16,19 @@
 
 package com.vaadin.hilla.startup;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.hilla.route.ClientRouteRegistry;
-import com.vaadin.hilla.route.RouteExtractionIndexHtmlRequestListener;
-import com.vaadin.hilla.route.records.ClientViewConfig;
+import com.vaadin.hilla.route.RouteUnifyingIndexHtmlRequestListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 /**
  * Service init listener to add the
- * {@link RouteExtractionIndexHtmlRequestListener} to the service and to
- * register client routes to {@link ClientRouteRegistry}.
+ * {@link RouteUnifyingIndexHtmlRequestListener} to the service and to register
+ * client routes to {@link ClientRouteRegistry}.
  */
 @Component
 public class RouteUnifyingServiceInitListener
@@ -44,73 +36,33 @@ public class RouteUnifyingServiceInitListener
     private static final Logger LOGGER = LoggerFactory
             .getLogger(RouteUnifyingServiceInitListener.class);
 
-    private final RouteExtractionIndexHtmlRequestListener routeExtractionIndexHtmlRequestListener;
     private final ClientRouteRegistry clientRouteRegistry;
-    private final ObjectMapper mapper = new ObjectMapper();
 
     /**
      * Creates a new instance of the listener.
      *
-     * @param routeExtractionIndexHtmlRequestListener
-     *            the listener to add
      * @param clientRouteRegistry
      *            the registry to add the client routes to
      */
     @Autowired
     public RouteUnifyingServiceInitListener(
-            RouteExtractionIndexHtmlRequestListener routeExtractionIndexHtmlRequestListener,
             ClientRouteRegistry clientRouteRegistry) {
-        this.routeExtractionIndexHtmlRequestListener = routeExtractionIndexHtmlRequestListener;
         this.clientRouteRegistry = clientRouteRegistry;
     }
 
     @Override
     public void serviceInit(ServiceInitEvent event) {
-        registerClientRoutes(event.getSource().getDeploymentConfiguration());
+        var deploymentConfiguration = event.getSource()
+                .getDeploymentConfiguration();
+        var routeExtractionIndexHtmlRequestListener = new RouteUnifyingIndexHtmlRequestListener(
+                clientRouteRegistry, deploymentConfiguration);
+        var deploymentMode = deploymentConfiguration.isProductionMode()
+                ? "PRODUCTION"
+                : "DEVELOPMENT";
         event.addIndexHtmlRequestListener(
                 routeExtractionIndexHtmlRequestListener);
-    }
-
-    protected void registerClientRoutes(
-            DeploymentConfiguration deploymentConfiguration) {
-        try (var source = getViewsJsonAsResource(deploymentConfiguration)
-                .openStream()) {
-            if (source != null) {
-                clientRouteRegistry.clearRoutes();
-                registerAndRecurseChildren("",
-                        mapper.readValue(source, new TypeReference<>() {
-                        }));
-            } else {
-                LOGGER.warn("Failed to find views.json");
-            }
-        } catch (IOException e) {
-            LOGGER.warn("Failed extract client views from views.json", e);
-        }
-    }
-
-    private URL getViewsJsonAsResource(
-            DeploymentConfiguration deploymentConfiguration)
-            throws MalformedURLException {
-        var isProductionMode = deploymentConfiguration.isProductionMode();
-        if (isProductionMode) {
-            return getClass().getResource("/META-INF/VAADIN/views.json");
-        }
-        return deploymentConfiguration.getFrontendFolder().toPath()
-                .resolve("generated").resolve("views.json").toUri().toURL();
-    }
-
-    private void registerAndRecurseChildren(String basePath,
-            ClientViewConfig view) {
-        var path = view.getRoute() == null || view.getRoute().isEmpty()
-                ? basePath
-                : basePath + '/' + view.getRoute();
-        if (view.getChildren() == null || view.getChildren().isEmpty()) {
-            clientRouteRegistry.addRoute(path, view);
-        } else {
-            view.getChildren().forEach(child -> {
-                child.setParent(view);
-                registerAndRecurseChildren(path, child);
-            });
-        }
+        LOGGER.debug(
+                "{} mode: Registered RouteUnifyingIndexHtmlRequestListener.",
+                deploymentMode);
     }
 }
--- packages/java/endpoint/src/test/java/com/vaadin/hilla/startup/RouteUnifyingServiceInitListenerTest.java
+++ packages/java/endpoint/src/test/java/com/vaadin/hilla/startup/RouteUnifyingServiceInitListenerTest.java
@@ -1,15 +1,7 @@
 package com.vaadin.hilla.startup;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vaadin.flow.function.DeploymentConfiguration;
-import org.apache.commons.io.IOUtils;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -17,12 +9,11 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mockito;
 
+import com.vaadin.flow.function.DeploymentConfiguration;
 import com.vaadin.flow.server.ServiceInitEvent;
 import com.vaadin.flow.server.VaadinService;
 import com.vaadin.hilla.route.ClientRouteRegistry;
-import com.vaadin.hilla.route.RouteExtractionIndexHtmlRequestListener;
-import com.vaadin.hilla.route.records.ClientViewConfig;
-import com.vaadin.hilla.route.records.RouteParamType;
+import com.vaadin.hilla.route.RouteUnifyingIndexHtmlRequestListener;
 
 public class RouteUnifyingServiceInitListenerTest {
 
@@ -30,7 +21,6 @@ public class RouteUnifyingServiceInitListenerTest {
     private ServiceInitEvent event;
     private ClientRouteRegistry clientRouteRegistry;
     private DeploymentConfiguration mockDeploymentConfiguration;
-    private ObjectMapper mapper = new ObjectMapper();
 
     @Rule
     public TemporaryFolder projectRoot = new TemporaryFolder();
@@ -39,7 +29,6 @@ public class RouteUnifyingServiceInitListenerTest {
     public void setup() throws IOException {
         clientRouteRegistry = new ClientRouteRegistry();
         routeUnifyingServiceInitListener = new RouteUnifyingServiceInitListener(
-                Mockito.mock(RouteExtractionIndexHtmlRequestListener.class),
                 clientRouteRegistry);
         VaadinService mockVaadinService = Mockito.mock(VaadinService.class);
         mockDeploymentConfiguration = Mockito
@@ -61,83 +50,9 @@ public class RouteUnifyingServiceInitListenerTest {
                 eventHasAddedRouteIndexHtmlRequestListener(event));
     }
 
-    @Test
-    public void should_extractClientViews_fromResources_inProdMode() {
-        routeUnifyingServiceInitListener.registerClientRoutes(
-                event.getSource().getDeploymentConfiguration());
-        Map<String, ClientViewConfig> allRoutes = clientRouteRegistry
-                .getAllRoutes();
-
-        MatcherAssert.assertThat(allRoutes, Matchers.aMapWithSize(12));
-        MatcherAssert.assertThat(allRoutes.get("/about").getTitle(),
-                Matchers.is("About"));
-        MatcherAssert.assertThat(allRoutes.get("/profile/friends/list")
-                .getOther().get("unknown"), Matchers.notNullValue());
-        MatcherAssert.assertThat(
-                allRoutes.get("/profile/friends/:user?/edit")
-                        .getRouteParameters(),
-                Matchers.is(Map.of(":user?", RouteParamType.OPTIONAL)));
-        MatcherAssert.assertThat(
-                allRoutes.get("/profile/friends/:user").getRouteParameters(),
-                Matchers.is(Map.of(":user", RouteParamType.REQUIRED)));
-        MatcherAssert.assertThat(
-                allRoutes.get("/profile/messages/*").getRouteParameters(),
-                Matchers.is(Map.of("wildcard", RouteParamType.WILDCARD)));
-
-    }
-
-    @Test
-    public void should_extractClientViews_fromFrontendGenerated_inDevMode()
-            throws IOException {
-        Mockito.when(mockDeploymentConfiguration.isProductionMode())
-                .thenReturn(false);
-        var frontendGeneratedDir = projectRoot.newFolder("frontend/generated");
-        Mockito.when(mockDeploymentConfiguration.getFrontendFolder())
-                .thenReturn(frontendGeneratedDir.getParentFile());
-
-        createMockedDevModeViewsJson();
-
-        routeUnifyingServiceInitListener.registerClientRoutes(
-                event.getSource().getDeploymentConfiguration());
-        Map<String, ClientViewConfig> allRoutes = clientRouteRegistry
-                .getAllRoutes();
-
-        MatcherAssert.assertThat(allRoutes, Matchers.aMapWithSize(12));
-        MatcherAssert.assertThat(allRoutes.get("/dev/about").getTitle(),
-                Matchers.is("About"));
-        MatcherAssert.assertThat(allRoutes.get("/dev/profile/friends/list")
-                .getOther().get("unknown"), Matchers.notNullValue());
-        MatcherAssert.assertThat(
-                allRoutes.get("/dev/profile/friends/:user?/edit")
-                        .getRouteParameters(),
-                Matchers.is(Map.of(":user?", RouteParamType.OPTIONAL)));
-        MatcherAssert.assertThat(
-                allRoutes.get("/dev/profile/friends/:user")
-                        .getRouteParameters(),
-                Matchers.is(Map.of(":user", RouteParamType.REQUIRED)));
-        MatcherAssert.assertThat(
-                allRoutes.get("/dev/profile/messages/*").getRouteParameters(),
-                Matchers.is(Map.of("wildcard", RouteParamType.WILDCARD)));
-    }
-
-    private void createMockedDevModeViewsJson() throws IOException {
-        var viewsJsonProdAsResource = getClass()
-                .getResource("/META-INF/VAADIN/views.json");
-        assert viewsJsonProdAsResource != null;
-        String hierarchicalRoutesAsString = IOUtils.toString(
-                viewsJsonProdAsResource.openStream(), StandardCharsets.UTF_8);
-        String addedDevToRootRoute = hierarchicalRoutesAsString
-                .replaceFirst("\"route\": \"\",", "\"route\": \"dev\",");
-        var viewsJsonFile = projectRoot
-                .newFile("frontend/generated/views.json");
-        try (PrintWriter writer = new PrintWriter(viewsJsonFile)) {
-            writer.println(addedDevToRootRoute);
-        }
-    }
-
     private boolean eventHasAddedRouteIndexHtmlRequestListener(
             ServiceInitEvent event) {
         return event.getAddedIndexHtmlRequestListeners().anyMatch(
-                indexHtmlRequestListener -> indexHtmlRequestListener instanceof RouteExtractionIndexHtmlRequestListener);
+                indexHtmlRequestListener -> indexHtmlRequestListener instanceof RouteUnifyingIndexHtmlRequestListener);
     }
 }
