46cb1bd0a49b0926558611f3cfc5c2d01d8cc5f1
taefi <taefi.soroosh@gmail.com>
2024-03-25 11:44:12 +0200

    test: amend .mocharc.cjs and package.json
    This adds loader side-by-side to the import
    to let the tests run without upgrading node
    version to 20+. This also add tsx to the list
    of extensions that taken into account when
    executing tests.

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -5,8 +5,9 @@ const karmaMochaConfig = {
 }
 
 module.exports = {
-  extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs'],
+  extensions: ['ts', 'mts', 'cts', 'js', 'mjs', 'cjs', 'tsx'],
   import: 'tsx',
+  loader: 'tsx',
   exit: true,
   karmaMochaConfig,
   ...karmaMochaConfig,
