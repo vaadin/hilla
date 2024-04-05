d959614cdb134e3c9a5a0736b0a810efe8f69adb
Luciano Vernaschi <luciano@vaadin.com>
2024-04-05 11:13:24 +0300

    fix!: require at least one role in `rolesAllowed` (#2295)

--- packages/ts/file-router/src/types.d.ts
+++ packages/ts/file-router/src/types.d.ts
@@ -9,7 +9,7 @@ export type ViewConfig = Readonly<{
   /**
    * Same as in the explicit React Router configuration.
    */
-  rolesAllowed?: readonly string[];
+  rolesAllowed?: readonly [string, ...string[]];
 
   /**
    * Set to true to require the user to be logged in to access the view.
