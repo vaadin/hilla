c9bc1777f4ab5f3daee2a335a7927e62d1840a37
taefi <taefi.soroosh@gmail.com>
2024-04-24 10:33:49 +0300

    fix javadocs

--- packages/java/endpoint/src/main/java/com/vaadin/hilla/route/ClientRouteRegistry.java
+++ packages/java/endpoint/src/main/java/com/vaadin/hilla/route/ClientRouteRegistry.java
@@ -221,13 +221,13 @@ public class ClientRouteRegistry implements ClientRoutesProvider {
 
     /**
      * Checks if the route registry has a main layout.
-     * <ul>
+     * <p>
      * A main layout is an route that:
+     * <ul>
+     *   <li>has an empty route path</li>
+     *   <li>has a non-null children list (can be empty)</li>
+     *   <li>has no parent</li>
      * </ul>
-     * <li>has an empty route path</li>
-     * <li>has a non-null children list (can be empty)</li>
-     * <li>has no parent</li>
-     *
      * @return {@code true} if the registry has a main layout, {@code false}
      *         otherwise
      */
