976929a68832b9ad91a44959059fd590de4a6d76
Vlad Rindevich <vladrin@vaadin.com>
2024-05-06 14:53:30 +0300

    fix(file-router): get back compile-time type

--- packages/ts/file-router/src/shared/internal.d.ts
+++ packages/ts/file-router/src/shared/internal.d.ts
@@ -1,5 +1,15 @@
 import type { ViewConfig } from '../types.js';
 
+/**
+ * Internal type used for server communication and menu building. It extends the
+ * view configuration with the route parameters.
+ */
+export type ServerViewConfig = Readonly<{
+  children?: readonly ServerViewConfig[];
+  params?: Readonly<Record<string, RouteParamType>>;
+}> &
+  ViewConfig;
+
 export type VaadinObject = Readonly<{
   views: Readonly<Record<string, ViewConfig>>;
 }>;
