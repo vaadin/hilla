f8fb27d8eef2b55b5c2161b513dba926c4ee6735
taefi <taefi.soroosh@gmail.com>
2024-06-10 18:02:10 +0300

    add SignalHandler.ts so no generation is needed

---
+++ packages/ts/react-signals/src/handler/SignalsHandler.ts
@@ -0,0 +1,17 @@
+import { ConnectClient, type EndpointRequestInit, type Subscription } from "@vaadin/hilla-frontend";
+
+export default class SignalsHandler {
+  private readonly client: ConnectClient;
+
+  constructor(client: ConnectClient) {
+    this.client = client;
+  }
+
+  subscribe(signalProviderEndpointMethod: string, clientSignalId: string): Subscription<string> {
+    return this.client.subscribe("SignalsHandler", "subscribe", { signalProviderEndpointMethod, clientSignalId });
+  }
+
+  async update(clientSignalId: string, event: string, init?: EndpointRequestInit): Promise<void> {
+    return this.client.call("SignalsHandler", "update", { clientSignalId, event }, init);
+  }
+}
