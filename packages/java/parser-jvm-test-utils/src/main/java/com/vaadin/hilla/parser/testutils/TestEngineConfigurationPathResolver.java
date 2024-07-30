6c702af2cf17fc609bc8c43460710dafea7f8aeb
Luciano Vernaschi <luciano@vaadin.com>
2024-07-30 11:44:51 +0200

    Revert import change

--- packages/java/parser-jvm-test-utils/src/main/java/com/vaadin/hilla/parser/testutils/TestEngineConfigurationPathResolver.java
+++ packages/java/parser-jvm-test-utils/src/main/java/com/vaadin/hilla/parser/testutils/TestEngineConfigurationPathResolver.java
@@ -8,7 +8,7 @@ import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import jakarta.annotation.Nonnull;
+import javax.annotation.Nonnull;
 
 /**
  * A converter for relative paths of
