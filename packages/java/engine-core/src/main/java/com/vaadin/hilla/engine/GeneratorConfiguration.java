c73f3799bf4daeee1c4bf727923fdc2f8db18ea8
taefi <taefi.soroosh@gmail.com>
2024-08-02 11:20:46 +0300

    include generator-plugin-signals in ITs package.json

--- packages/java/engine-core/src/main/java/com/vaadin/hilla/engine/GeneratorConfiguration.java
+++ packages/java/engine-core/src/main/java/com/vaadin/hilla/engine/GeneratorConfiguration.java
@@ -143,8 +143,8 @@ public final class GeneratorConfiguration {
                 new Plugin("@vaadin/hilla-generator-plugin-barrel"),
                 new Plugin("@vaadin/hilla-generator-plugin-model"),
                 new Plugin("@vaadin/hilla-generator-plugin-push"),
-                new Plugin("@vaadin/hilla-generator-plugin-subtypes"),
-                new Plugin("@vaadin/hilla-generator-plugin-signals"));
+                new Plugin("@vaadin/hilla-generator-plugin-signals"),
+                new Plugin("@vaadin/hilla-generator-plugin-subtypes"));
 
         PluginsProcessor() {
             super(DEFAULTS);
