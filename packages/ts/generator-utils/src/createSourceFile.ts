05be3a1759ca7128818a686565be3bdc8da5ed6c
Vlad Rindevich <vladrin@vaadin.com>
2024-08-12 18:09:14 +0300

    feat(generator-utils): add traverse function for TS AST

--- packages/ts/generator-utils/src/createSourceFile.ts
+++ packages/ts/generator-utils/src/createSourceFile.ts
@@ -1,6 +1,6 @@
 import ts, { type SourceFile, type Statement } from 'typescript';
 
 export default function createSourceFile(statements: readonly Statement[], fileName: string): SourceFile {
-  const sourceFile = ts.createSourceFile(fileName, '', ts.ScriptTarget.ES2019, undefined, ts.ScriptKind.TS);
+  const sourceFile = ts.createSourceFile(fileName, '', ts.ScriptTarget.ES2021, undefined, ts.ScriptKind.TS);
   return ts.factory.updateSourceFile(sourceFile, statements);
 }
