f64730d4deaf3c73c7f105c480c63b4175425a5b
taefi <taefi.soroosh@gmail.com>
2024-08-27 17:47:45 +0300

    add TSDocs for ValueSignal.ts

--- packages/ts/react-signals/src/ValueSignal.ts
+++ packages/ts/react-signals/src/ValueSignal.ts
@@ -8,6 +8,9 @@ type PendingRequestsRecord<T> = Readonly<{
   canceled: boolean;
 }>;
 
+/**
+ * An operation subscription that can be canceled.
+ */
 export interface OperationSubscription {
   cancel(): void;
 }
@@ -18,14 +21,44 @@ export interface OperationSubscription {
 export class ValueSignal<T> extends FullStackSignal<T> {
   readonly #pendingRequests = new Map<string, PendingRequestsRecord<T>>();
 
+  /**
+   * Sets the value.
+   * Note that the value change event that is propagated to the server as the
+   * result of this operation is not taking the last seen value into account and
+   * will overwrite the shared value on the server unconditionally (AKA: "Last
+   * Write Wins"). If you need to perform a conditional update, use the
+   * `replace` method instead.
+   *
+   * @param value - The new value.
+   */
   set(value: T): void {
     this.value = value;
   }
 
+  /**
+   * Replaces the value with a new one only if the current value is equal to the
+   * expected value.
+   *
+   * @param expected - The expected value.
+   * @param newValue - The new value.
+   */
   replace(expected: T, newValue: T): void {
     this[$update](createReplaceStateEvent(expected, newValue));
   }
 
+  /**
+   * Tries to update the value by applying the callback function to the current
+   * value. In case of a concurrent change, the callback is run again with an
+   * updated input value. This is repeated until the result can be applied
+   * without concurrent changes, or the operation is canceled.
+   *
+   * Note that there is no guarantee that cancel() will be effective always,
+   * since a succeeding operation might already be on its way to the server.
+   *
+   * @param callback - The function that is applied on the current value to
+   *                   produce the new value.
+   * @returns An operation subscription that can be canceled.
+   */
   update(callback: (value: T) => T): OperationSubscription {
     const newValue = callback(this.value);
     const event = createReplaceStateEvent(this.value, newValue);
