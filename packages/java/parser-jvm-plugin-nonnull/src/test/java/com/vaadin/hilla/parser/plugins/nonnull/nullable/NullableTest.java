a8d91e774dc44de2e8b8395c4b0f6da200c19b9e
Krisjanis Seglins <kriss@vaadin.com>
2024-10-15 11:34:10 +0300

    Resolve jakarta persistence id and version annotations as nullable by default Fixes: #2809

---
+++ packages/java/parser-jvm-plugin-nonnull/src/test/java/com/vaadin/hilla/parser/plugins/nonnull/nullable/NullableTest.java
@@ -0,0 +1,47 @@
+package com.vaadin.hilla.parser.plugins.nonnull.nullable;
+
+import com.vaadin.hilla.parser.core.Parser;
+import com.vaadin.hilla.parser.plugins.backbone.BackbonePlugin;
+import com.vaadin.hilla.parser.plugins.nonnull.AnnotationMatcher;
+import com.vaadin.hilla.parser.plugins.nonnull.NonnullPlugin;
+import com.vaadin.hilla.parser.plugins.nonnull.NonnullPluginConfig;
+import com.vaadin.hilla.parser.plugins.nonnull.test.helpers.TestHelper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Set;
+
+public class NullableTest {
+    private final TestHelper helper = new TestHelper(getClass());
+
+    @Test
+    public void should_ApplyNullableAnnotation()
+            throws IOException, URISyntaxException {
+        var plugin = new NonnullPlugin();
+        plugin.setConfiguration(new NonnullPluginConfig());
+
+        var openAPI = new Parser().classLoader(getClass().getClassLoader())
+                .classPath(Set.of(helper.getTargetDir().toString()))
+                .endpointAnnotation(Endpoint.class.getName())
+                .endpointExposedAnnotation(EndpointExposed.class.getName())
+                .addPlugin(new BackbonePlugin()).addPlugin(plugin).execute();
+
+        helper.executeParserWithConfig(openAPI);
+    }
+
+    @Test
+    public void annotationMatcher_shouldHaveDefaultConstructorAndSetter() {
+        // to enable maven initialize instances of AnnotationMatcher from
+        // pom.xml configurations, properly, it should have the default
+        // constructor and setter methods:
+        AnnotationMatcher annotationMatcher = new AnnotationMatcher();
+        annotationMatcher.setName("name");
+        annotationMatcher.setScore(100);
+        annotationMatcher.setMakesNullable(true);
+        Assertions.assertEquals("name", annotationMatcher.getName());
+        Assertions.assertEquals(100, annotationMatcher.getScore());
+        Assertions.assertTrue(annotationMatcher.doesMakeNullable());
+    }
+}
