5b19b4ad8ef0ec1e04771a1da4290237db770408
Luciano Vernaschi <luciano@vaadin.com>
2024-11-19 11:06:33 +0100

    Revert "test(parser-jvm-plugin-transfertypes): add annotations module"
    This reverts commit 68c8e90d80296a27c4e357768285973f4a7c6db3.

--- packages/java/parser-jvm-plugin-transfertypes/src/main/java/module-info.java
+++ packages/java/parser-jvm-plugin-transfertypes/src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module com.vaadin.hilla.parser.plugins.transfertypes {
     requires com.fasterxml.jackson.databind;
-    requires com.vaadin.hilla.annotations;
     requires com.vaadin.hilla.parser.core;
     requires com.vaadin.hilla.parser.plugins.backbone;
     requires com.vaadin.hilla.parser.utils;
     requires com.vaadin.hilla.runtime.transfertypes;
+    requires hilla.annotations;
     requires jsr305;
     requires jakarta.annotation;
     requires io.swagger.v3.oas.models;
