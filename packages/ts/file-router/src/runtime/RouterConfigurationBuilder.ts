72550a21580e879246175de1a997ddb06d50e061
Vlad Rindevich <vladrin@vaadin.com>
2024-11-20 18:16:58 +0200

    refactor(file-router): use better array creation

--- packages/ts/file-router/src/runtime/RouterConfigurationBuilder.ts
+++ packages/ts/file-router/src/runtime/RouterConfigurationBuilder.ts
@@ -73,35 +73,32 @@ function split<T extends readonly RouteSplittingRule[]>(
   // a server layout.
   return transformTree<RouteGroup, readonly RouteGroup[]>(originalRoutes, (routes, next) =>
     // Split single routes list onto two filtered lists
-    routes.reduce<readonly WritableRouteGroup[]>(
-      (groups, route) => {
-        for (let i = 0; i < groups.length; i++) {
-          if (rules[i]?.(route)) {
-            groups[i].push(route);
-            return groups;
-          }
-        }
-
-        if (!route.children?.length) {
-          groups.at(-1)?.push(route);
+    routes.reduce<readonly WritableRouteGroup[]>((groups, route) => {
+      for (let i = 0; i < groups.length; i++) {
+        if (rules[i]?.(route)) {
+          groups[i].push(route);
           return groups;
         }
+      }
 
-        const childrenGroups = next(...route.children);
-
-        for (let i = 0; i < groups.length; i++) {
-          if (childrenGroups[i].length) {
-            groups[i].push({
-              ...route,
-              children: childrenGroups[i],
-            } as RouteObject);
-          }
-        }
-
+      if (!route.children?.length) {
+        groups.at(-1)?.push(route);
         return groups;
-      },
-      Array.from({ length: rules.length + 1 }, () => []),
-    ),
+      }
+
+      const childrenGroups = next(...route.children);
+
+      for (let i = 0; i < groups.length; i++) {
+        if (childrenGroups[i].length) {
+          groups[i].push({
+            ...route,
+            children: childrenGroups[i],
+          } as RouteObject);
+        }
+      }
+
+      return groups;
+    }, new Array(rules.length).fill([])),
   ) as readonly [...ReadonlyTuple<RouteGroup, T['length']>, RouteGroup];
 }
 
