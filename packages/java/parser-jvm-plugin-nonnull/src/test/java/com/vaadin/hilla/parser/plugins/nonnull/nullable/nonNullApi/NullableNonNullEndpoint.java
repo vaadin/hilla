e2af5372656df4a3599dba000dc2ec73521e1ca4
Kriss Seglins <101052651+krissvaa@users.noreply.github.com>
2024-11-26 18:37:16 +0200

    fix: show autoform non-rendered property errors (#2833)
    * Shows hidden property errors to the form and scroll to it
    Fixes: #2217
    
    * Resolve jakarta persistence id and version annotations as nullable by default
    Fixes: #2809
    
    * fix formatting
    
    * Fix type check
    Fixes: #2217
    
    * Add test for nonnull and nullable hierarchy
    Fixes: #2833
    
    * Add test for nonnull and nullable hierarchy
    Fixes: #2833
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Anton Platonov <platosha@gmail.com>

---
+++ packages/java/parser-jvm-plugin-nonnull/src/test/java/com/vaadin/hilla/parser/plugins/nonnull/nullable/nonNullApi/NullableNonNullEndpoint.java
@@ -0,0 +1,26 @@
+package com.vaadin.hilla.parser.plugins.nonnull.nullable.nonNullApi;
+
+import com.vaadin.hilla.parser.plugins.nonnull.nullable.Endpoint;
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
+@Endpoint
+public class NullableNonNullEndpoint {
+
+    public NullableNonNullFieldModel nullableNonNullFieldModel(
+            NullableNonNullFieldModel nullableNonNullFieldModel) {
+        return nullableNonNullFieldModel;
+    }
+
+    public static class NullableNonNullFieldModel {
+        public String required;
+        @Id
+        public String id;
+        @Version
+        public Long version;
+        @Version
+        @Nonnull
+        public Long notNullVersion;
+    }
+}
