73956697bbfed1e08895f80bfdc21602696fe912
Vlad Rindevich <vladrin@vaadin.com>
2024-11-28 12:30:31 +0200

    test(file-router): update fixture

--- packages/ts/file-router/test/vite-plugin/createRoutesFromMeta.spec.ts
+++ packages/ts/file-router/test/vite-plugin/createRoutesFromMeta.spec.ts
@@ -48,44 +48,45 @@ describe('@vaadin/hilla-file-router', () => {
         .equal(`import { createRoute as createRoute_1, extendModule as extendModule_1 } from "@vaadin/hilla-file-router/runtime.js";
 import type { AgnosticRoute as AgnosticRoute_1 } from "@vaadin/hilla-file-router/types.js";
 import * as Page_1 from "../issue-2928-flow-auto-layout.js";
-import * as Page_2 from "../views/nameToReplace.js";
-import * as Page_3 from "../views/profile/@index.js";
+import * as Page_2 from "../mod-extension-only-child.js";
+import * as Page_3 from "../views/nameToReplace.js";
+import * as Page_4 from "../views/profile/@index.js";
 import * as Layout_1 from "../views/profile/account/@layout.js";
-import * as Page_4 from "../views/profile/account/security/password.js";
-import * as Page_5 from "../views/profile/account/security/two-factor-auth.js";
-import * as Page_6 from "../views/profile/friends/{user}.js";
+import * as Page_5 from "../views/profile/account/security/password.js";
+import * as Page_6 from "../views/profile/account/security/two-factor-auth.js";
+import * as Page_7 from "../views/profile/friends/{user}.js";
 import * as Layout_2 from "../views/profile/friends/@layout.js";
-import * as Page_7 from "../views/profile/friends/list.js";
-import * as Page_8 from "../views/test/{...wildcard}.js";
-import * as Page_9 from "../views/test/{{optional}}.js";
-import * as Page_10 from "../views/test/issue-002378/{requiredParam}/edit.js";
+import * as Page_8 from "../views/profile/friends/list.js";
+import * as Page_9 from "../views/test/{...wildcard}.js";
+import * as Page_10 from "../views/test/{{optional}}.js";
+import * as Page_11 from "../views/test/issue-002378/{requiredParam}/edit.js";
 import * as Layout_3 from "../views/test/issue-002571-empty-layout/@layout.js";
-import * as Page_11 from "../views/test/issue-002879-config-below.js";
+import * as Page_12 from "../views/test/issue-002879-config-below.js";
 const routes: readonly AgnosticRoute_1[] = [
-    createRoute_1("nameToReplace", Page_2),
+    createRoute_1("nameToReplace", Page_3),
     createRoute_1("profile", [
-        createRoute_1("", Page_3),
+        createRoute_1("", Page_4),
         createRoute_1("account", Layout_1, [
             createRoute_1("security", [
-                createRoute_1("password", Page_4),
-                createRoute_1("two-factor-auth", Page_5)
+                createRoute_1("password", Page_5),
+                createRoute_1("two-factor-auth", Page_6)
             ])
         ]),
         createRoute_1("friends", Layout_2, [
-            createRoute_1("list", Page_7),
-            createRoute_1(":user", Page_6)
+            createRoute_1("list", Page_8),
+            createRoute_1(":user", Page_7)
         ])
     ]),
     createRoute_1("test", [
-        createRoute_1(":optional?", Page_9),
-        createRoute_1("*", Page_8),
+        createRoute_1(":optional?", Page_10),
+        createRoute_1("*", Page_9),
         createRoute_1("issue-002378", [
             createRoute_1(":requiredParam", [
-                createRoute_1("edit", Page_10)
+                createRoute_1("edit", Page_11)
             ])
         ]),
         createRoute_1("issue-002571-empty-layout", Layout_3, []),
-        createRoute_1("issue-002879-config-below", Page_11)
+        createRoute_1("issue-002879-config-below", Page_12)
     ]),
     createRoute_1("issue-2928-flow-auto-layout", extendModule_1(Page_1, { "flowLayout": true })),
     createRoute_1("mod-extension-only", extendModule_1(null, { "flowLayout": true }), [
