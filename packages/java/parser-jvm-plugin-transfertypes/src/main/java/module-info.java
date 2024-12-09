a79453e39c414c194b3903dffef6c548a2aeabb6
Luciano Vernaschi <luciano@vaadin.com>
2024-12-09 16:48:43 +0100

    Rename annotation package

--- packages/java/parser-jvm-plugin-transfertypes/src/main/java/module-info.java
+++ packages/java/parser-jvm-plugin-transfertypes/src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.vaadin.hilla.parser.plugins.transfertypes {
     requires com.vaadin.hilla.parser.plugins.backbone;
     requires com.vaadin.hilla.parser.utils;
     requires com.vaadin.hilla.runtime.transfertypes;
-    requires hilla.annotations;
+    requires hilla.types;
     requires jsr305;
     requires jakarta.annotation;
     requires io.swagger.v3.oas.models;
