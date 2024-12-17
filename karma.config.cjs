cccb8cef7c42fc5a9ad157a87042f88db5602c89
Artur <artur@vaadin.com>
2024-12-17 12:00:53 +0200

    test: Define window size (#3028)
    This reduces randomness when tests are run on different machines and resolves the current issue of a date field going into "mobile mode" and not having an input field on GitHub Actions

--- karma.config.cjs
+++ karma.config.cjs
@@ -117,7 +117,7 @@ module.exports = (config) => {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
+        flags: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1024x768'],
       },
     },
 
