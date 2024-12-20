4fe3541d947cd030e23d4414589b8f13fb54383a
Artur Signell <artur@vaadin.com>
2024-12-20 08:42:18 +0200

    Update test to use Flow crud classes

--- packages/java/tests/spring/react-grid-test/src/main/java/com/vaadin/hilla/test/reactgrid/AppointmentService.java
+++ packages/java/tests/spring/react-grid-test/src/main/java/com/vaadin/hilla/test/reactgrid/AppointmentService.java
@@ -1,14 +1,18 @@
 package com.vaadin.hilla.test.reactgrid;
 
-import com.vaadin.hilla.BrowserCallable;
-import com.vaadin.hilla.crud.CrudRepositoryService;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Service;
 
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import com.vaadin.flow.spring.data.jpa.CrudRepositoryService;
+import com.vaadin.hilla.BrowserCallable;
 
 @BrowserCallable
 @Service
 @AnonymousAllowed
 public class AppointmentService extends
         CrudRepositoryService<Appointment, Long, AppointmentRepository> {
+    AppointmentService(AppointmentRepository repository) {
+        super(repository);
+    }
 }
