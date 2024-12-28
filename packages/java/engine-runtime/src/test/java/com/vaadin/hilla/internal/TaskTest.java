a45ecddef2ff63b8175e5c4dd1d39669c39af55b
Artur Signell <artur@vaadin.com>
2024-12-28 15:42:14 +0200

    test: Use project .npmrc for tests
    Makes it possible to use a custom registry for dependency testing

--- packages/java/engine-runtime/src/test/java/com/vaadin/hilla/internal/TaskTest.java
+++ packages/java/engine-runtime/src/test/java/com/vaadin/hilla/internal/TaskTest.java
@@ -41,21 +41,25 @@ public class TaskTest {
                 .resolve(getFrontendDirectory());
         Files.createDirectories(frontendDir);
 
-        var packagesDirectory = Path
+        Path packagesPath = Path
                 .of(getClass().getClassLoader().getResource("").toURI())
                 .getParent() // target
                 .getParent() // engine-runtime
                 .getParent() // java
-                .getParent() // packages
+                .getParent(); // packages
+
+        Path projectRoot = packagesPath.getParent();
+        Files.copy(projectRoot.resolve(".npmrc"), temporaryDirectory.resolve(".npmrc"));
+        var tsPackagesDirectory = packagesPath
                 .resolve("ts");
 
         var shellCmd = FrontendUtils.isWindows() ? Stream.of("cmd.exe", "/c")
-                : Stream.<String> empty();
+                : Stream.<String>empty();
 
         var npmCmd = Stream.of("npm", "--no-update-notifier", "--no-audit",
                 "install", "--no-save", "--install-links");
 
-        var generatorFiles = Files.list(packagesDirectory).map(Path::toString);
+        var generatorFiles = Files.list(tsPackagesDirectory).map(Path::toString);
 
         var command = Stream.of(shellCmd, npmCmd, generatorFiles)
                 .flatMap(Function.identity()).toList();
