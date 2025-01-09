c6494c1b74023c47b8162be0e1627675acf4a2ca
Luciano Vernaschi <luciano@vaadin.com>
2025-01-09 21:15:02 +0100

    fix: use `Qualifier` to force bean dependency (#3098)
    * Use `dependsOn` to force bean dependency
    
    * Add qualifier
    
    * Add qualifier
    
    * Remove `@DependsOn`
    
    * Update test to match new code
    
    ---------
    
    Co-authored-by: Artur <artur@vaadin.com>

--- packages/java/endpoint/src/test/java/com/vaadin/hilla/EndpointControllerConfigurationTest.java
+++ packages/java/endpoint/src/test/java/com/vaadin/hilla/EndpointControllerConfigurationTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jackson.JacksonProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -25,9 +26,32 @@ public class EndpointControllerConfigurationTest {
     @Autowired
     private EndpointAccessChecker endpointAccessChecker;
 
+    @Autowired
+    private ConfigurableApplicationContext context;
+
     @Test
     public void dependenciesAvailable() {
         Assert.assertNotNull(endpointRegistry);
         Assert.assertNotNull(endpointAccessChecker);
+    }
+
+    @Test
+    public void testEndpointInvokerUsesQualifiedObjectMapper()
+            throws NoSuchFieldException, IllegalAccessException {
+        var endpointInvoker = context.getBean(EndpointInvoker.class);
+        var objectMapper = context.getBean("hillaEndpointObjectMapper");
+
+        Assert.assertNotNull("EndpointInvoker should not be null",
+                endpointInvoker);
+        Assert.assertNotNull("hillaEndpointObjectMapper should not be null",
+                objectMapper);
+
+        var field = EndpointInvoker.class
+                .getDeclaredField("endpointObjectMapper");
+        field.setAccessible(true);
+
+        Assert.assertSame(
+                "EndpointInvoker should use the qualified hillaEndpointObjectMapper",
+                objectMapper, field.get(endpointInvoker));
     }
 }
