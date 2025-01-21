ddf69701a40f320b07ef032b4c6f1831c6c5863d
Vlad Rindevich <vladrin@vaadin.com>
2025-01-21 13:02:38 +0200

    refactor(generator-plugin-client): revert some changes

--- packages/ts/generator-plugin-client/src/index.ts
+++ packages/ts/generator-plugin-client/src/index.ts
@@ -4,25 +4,25 @@ import Plugin from '@vaadin/hilla-generator-core/Plugin.js';
 import type { SharedStorage } from '@vaadin/hilla-generator-core/SharedStorage.t.js';
 import ClientProcessor from './ClientProcessor.js';
 
-async function checkForCustomClientFile(path?: string): Promise<boolean> {
-  const dir = path?.startsWith('file:') ? fileURLToPath(path) : path;
-
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    return !!(dir && (await open(`${dir}/${ClientPlugin.CUSTOM_CLIENT_FILE_NAME}.ts`, 'r')));
-  } catch {
-    return false;
-  }
-}
-
-async function getClientFileName(path?: string): Promise<string> {
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  return (await checkForCustomClientFile(path)) ? ClientPlugin.CUSTOM_CLIENT_FILE_NAME : ClientPlugin.CLIENT_FILE_NAME;
-}
-
 export default class ClientPlugin extends Plugin {
   static readonly CLIENT_FILE_NAME = 'connect-client.default';
   static readonly CUSTOM_CLIENT_FILE_NAME = '../connect-client';
+
+  static async getClientFileName(path?: string): Promise<string> {
+    return (await ClientPlugin.#checkForCustomClientFile(path))
+      ? ClientPlugin.CUSTOM_CLIENT_FILE_NAME
+      : ClientPlugin.CLIENT_FILE_NAME;
+  }
+
+  static async #checkForCustomClientFile(path?: string): Promise<boolean> {
+    const dir = path?.startsWith('file:') ? fileURLToPath(path) : path;
+
+    try {
+      return !!(dir && (await open(`${dir}/${ClientPlugin.CUSTOM_CLIENT_FILE_NAME}.ts`, 'r')));
+    } catch {
+      return false;
+    }
+  }
 
   declare ['constructor']: typeof ClientPlugin;
 
@@ -32,7 +32,7 @@ export default class ClientPlugin extends Plugin {
 
   override async execute({ outputDir, sources }: SharedStorage): Promise<void> {
     // the client file is created only if a custom client file is not found
-    if (!(outputDir && (await checkForCustomClientFile(outputDir)))) {
+    if (!(outputDir && (await ClientPlugin.#checkForCustomClientFile(outputDir)))) {
       const clientFile = new ClientProcessor(this.constructor.CLIENT_FILE_NAME, this).process();
       sources.push(clientFile);
     }
