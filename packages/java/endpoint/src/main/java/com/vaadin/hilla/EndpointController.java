24cc53a601e2fa66fe23f86d92eb94305cea3077
Luciano Vernaschi <luciano@vaadin.com>
2025-01-27 21:20:32 +0100

    Reduce differences in imports

--- packages/java/endpoint/src/main/java/com/vaadin/hilla/EndpointController.java
+++ packages/java/endpoint/src/main/java/com/vaadin/hilla/EndpointController.java
@@ -27,7 +27,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
