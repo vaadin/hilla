745287cf525f5f60d22f57427c2a18b84a665a4c
Vlad Rindevich <vladrin@vaadin.com>
2025-02-01 18:53:43 +0200

    refactor(react-crud): disable test failures if an error is thrown

--- packages/ts/react-crud/vitest.config.ts
+++ packages/ts/react-crud/vitest.config.ts
@@ -1,3 +1,9 @@
+import { mergeConfig } from 'vitest/config.js';
 import sharedConfig from '../../../scripts/vite/browser.vitest.config.js';
 
-export default sharedConfig;
+export default mergeConfig(sharedConfig, {
+  test: {
+    // TODO: Remove this option when all errors are properly handled
+    dangerouslyIgnoreUnhandledErrors: true,
+  },
+});
