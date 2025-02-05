23bdf7bdf0813711f357719eac93a9fa65760331
Vlad Rindevich <vladrin@vaadin.com>
2025-02-05 15:48:08 +0200

    chore: remove unnecessary console.log

--- scripts/vite/browser.vitest.config.ts
+++ scripts/vite/browser.vitest.config.ts
@@ -11,8 +11,6 @@ export { root, cwd, isCI, packageJson };
 function getBrowserProviderOptions(): BrowserProviderOptions {
   const { PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD, CHROME_BIN } = process.env;
 
-  console.log({ PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD, CHROME_BIN });
-
   if (PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD === '1') {
     if (typeof CHROME_BIN === 'string') {
       return {
