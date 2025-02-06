cd869a598fc3087ac157d89c6cd1503d8bb54f58
Vlad Rindevich <vladrin@vaadin.com>
2025-02-06 22:29:22 +0200

    test(generator-cli): add more tests [2]

--- packages/ts/generator-cli/test/cli.spec.ts
+++ packages/ts/generator-cli/test/cli.spec.ts
@@ -1,6 +1,9 @@
 import { exec } from 'node:child_process';
 import { promisify } from 'node:util';
-import { describe, expect, it } from 'vitest';
+import chaiAsPromised from 'chai-as-promised';
+import { describe, expect, it, chai } from 'vitest';
+
+chai.use(chaiAsPromised);
 
 const execAsync = promisify(exec);
 
@@ -13,5 +16,9 @@ describe('cli', () => {
   it('should print version', async () => {
     const { stdout } = await execAsync(`npx tsx src/index.ts --version`);
     await expect(stdout).toMatchFileSnapshot('fixtures/version.snap');
+  });
+
+  it('should throw an error if input file is not provided', async () => {
+    await expect(execAsync(`npx tsx src/index.ts`)).to.eventually.be.rejectedWith('Input file is required');
   });
 });
