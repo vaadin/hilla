efb7ec8148895f27de69bc4df2ce7cfbdbbb80f8
Artur Signell <artur@vaadin.com>
2025-02-19 10:58:12 +0200

    feat: useComboBoxDataProvider

---
+++ packages/java/tests/spring/react-grid-test/frontend/views/ComboBoxUseComboBoxDataProviderHook.tsx
@@ -0,0 +1,28 @@
+import { useComboBoxDataProvider } from '@vaadin/hilla-react-crud';
+import { Button, ComboBox } from '@vaadin/react-components';
+import { PersonCustomService } from 'Frontend/generated/endpoints';
+import Direction from 'Frontend/generated/org/springframework/data/domain/Sort/Direction';
+import NullHandling from 'Frontend/generated/org/springframework/data/domain/Sort/NullHandling';
+
+export default function ComboBoxUseComboBoxDataProviderHook(): React.JSX.Element {
+  const dataProvider = useComboBoxDataProvider(PersonCustomService.listPersonsLazyWithFilter);
+  const dataProviderLastName = useComboBoxDataProvider(PersonCustomService.listPersonsLazyWithFilter);
+  dataProviderLastName.setSort({
+    orders: [{ property: 'lastName', direction: Direction.ASC, ignoreCase: true, nullHandling: NullHandling.NATIVE }],
+  });
+
+  return (
+    <div className="p-m flex flex-col gap-m">
+      <ComboBox label="Default sort" dataProvider={dataProvider} itemLabelPath="lastName" />
+      <ComboBox label="Sorted using last name" dataProvider={dataProviderLastName} itemLabelPath="lastName" />
+      <Button
+        onClick={() => {
+          dataProvider.refresh();
+          dataProviderLastName.refresh();
+        }}
+      >
+        Refresh
+      </Button>
+    </div>
+  );
+}
