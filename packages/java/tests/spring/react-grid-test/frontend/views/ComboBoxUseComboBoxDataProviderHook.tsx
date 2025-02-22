280ce2e847204cb259ee00149f2a31b75598de1d
Artur Signell <artur@vaadin.com>
2025-02-22 14:11:45 +0200

    Add a dependency list to useGridDataProvider and useComboBoxDataProvider
    This is to support using state and signals in the fetch callback

--- packages/java/tests/spring/react-grid-test/frontend/views/ComboBoxUseComboBoxDataProviderHook.tsx
+++ packages/java/tests/spring/react-grid-test/frontend/views/ComboBoxUseComboBoxDataProviderHook.tsx
@@ -1,4 +1,5 @@
 import { useComboBoxDataProvider } from '@vaadin/hilla-react-crud';
+import { useSignal } from '@vaadin/hilla-react-signals';
 import { Button, ComboBox } from '@vaadin/react-components';
 import { useState } from 'react';
 import { PersonCustomService } from 'Frontend/generated/endpoints';
@@ -12,10 +13,35 @@ export default function ComboBoxUseComboBoxDataProviderHook(): React.JSX.Element
   const dataProvider = useComboBoxDataProvider(PersonCustomService.listPersonsLazyWithFilter);
   const dataProviderLastName = useComboBoxDataProvider(PersonCustomService.listPersonsLazyWithFilter, { sort });
 
+  const filterSignal = useSignal('');
+  const dataProviderLastNameFiltered = useComboBoxDataProvider(
+    async (pageable, filter) => PersonCustomService.listPersonsLazyWithFilter(pageable, filterSignal.value + filter),
+    { sort },
+    [filterSignal.value],
+  );
+
   return (
     <div className="p-m flex flex-col gap-m">
-      <ComboBox label="Default sort" dataProvider={dataProvider} itemLabelPath="lastName" />
-      <ComboBox label="Sorted using last name" dataProvider={dataProviderLastName} itemLabelPath="lastName" />
+      <ComboBox id="defaultSort" label="Default sort" dataProvider={dataProvider} itemLabelPath="lastName" />
+      <ComboBox
+        id="sortLastName"
+        label="Sorted using last name"
+        dataProvider={dataProviderLastName}
+        itemLabelPath="lastName"
+      />
+      <input
+        id="filter"
+        type="text"
+        onInput={(e) => {
+          filterSignal.value = (e.target as HTMLInputElement).value;
+        }}
+      />
+      <ComboBox
+        id="prependFilter"
+        label="Prepending filter with"
+        dataProvider={dataProviderLastNameFiltered}
+        itemLabelPath="lastName"
+      />
       <Button
         onClick={() => {
           dataProvider.refresh();
