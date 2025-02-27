cf61359b40f1fc70d12b5c36e77dae32c415b232
Vlad Rindevich <vladrin@vaadin.com>
2025-02-27 16:40:04 +0200

    feat(generator-plugin-transfertypes): implement injecting library types with import

--- packages/ts/generator-plugin-transfertypes/src/index.ts
+++ packages/ts/generator-plugin-transfertypes/src/index.ts
@@ -1,19 +1,72 @@
 import Plugin from '@vaadin/hilla-generator-core/Plugin.js';
-import type { SharedStorage } from '@vaadin/hilla-generator-core/SharedStorage.js';
+import type { SharedStorage, TransferTypeMaker } from '@vaadin/hilla-generator-core/SharedStorage.js';
+import type { OpenAPIV3 } from 'openapi-types';
 import type { Writable } from 'type-fest';
-import ts from 'typescript';
+import { factory, type Identifier } from 'typescript';
+
+type ReplacedTypeContext = Readonly<{
+  schema?: OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject;
+}>;
+
+type ReplacedTypeMaker = (context: ReplacedTypeContext) => TransferTypeMaker;
+
+type FromModule = Readonly<{
+  module: string;
+  named?: string;
+  default?: string;
+}>;
+
+function createReplacedTypeMaker(name: string): ReplacedTypeMaker {
+  return ({ schema }) =>
+    ({ dependencies: { imports }, typeArguments }) => {
+      let id: Identifier | undefined;
+
+      if (schema && 'x-from-module' in schema) {
+        const fromModule = schema['x-from-module'] as FromModule;
+
+        if (fromModule.named) {
+          id =
+            imports.named.getIdentifier(fromModule.module, fromModule.named) ??
+            imports.named.add(fromModule.module, fromModule.named);
+        }
+
+        if (fromModule.default) {
+          id =
+            imports.default.getIdentifier(fromModule.module) ??
+            imports.default.add(fromModule.module, fromModule.default);
+        }
+      }
+
+      return factory.createTypeReferenceNode(id ?? name, typeArguments);
+    };
+}
+
+type ReplacedTypes = Readonly<Record<string, (context: ReplacedTypeContext) => TransferTypeMaker>>;
+
+const replacedTypes: ReplacedTypes = Object.fromEntries(
+  ['File', 'Signal', 'NumberSignal', 'ValueSignal', 'ListSignal'].map((name) => [
+    `com.vaadin.hilla.runtime.transfertypes.${name}`,
+    createReplacedTypeMaker(name),
+  ]),
+);
 
 export default class TransferTypesPlugin extends Plugin {
+  declare ['constructor']: typeof TransferTypesPlugin;
+
   override get path(): string {
     return import.meta.url;
   }
 
   override execute({ api: { components }, transferTypes }: SharedStorage): void {
-    transferTypes.set('com.vaadin.hilla.runtime.transfertypes.File', () => ts.factory.createTypeReferenceNode('File'));
+    for (const [key, value] of Object.entries(replacedTypes)) {
+      transferTypes.set(key, value({ schema: components?.schemas?.[key] }));
+    }
 
     if (components?.schemas) {
+      const keys = Object.keys(replacedTypes);
+
       (components as Writable<typeof components>).schemas = Object.fromEntries(
-        Object.entries(components.schemas).filter(([key]) => key !== 'com.vaadin.hilla.runtime.transfertypes.File'),
+        Object.entries(components.schemas).filter(([key]) => !keys.includes(key)),
       );
     }
   }
