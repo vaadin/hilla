4431eac8f4686e4236454e2186ee7a6329950ec5
Vlad Rindevich <vladrin@vaadin.com>
2025-03-26 16:27:10 +0200

    feat(generator-plugin-transfertypes): add generics & importing from the correct module (#3318)
    * refactor(parser-jvm-transfertypes): implement adding "x-from-module" metadata to transfer types
    
    * refactor(parser-jvm-transfertypes): make namedSpecifier a single string instead of an array
    
    * feat(generator-plugin-transfertypes): implement injecting library types with import
    
    * refactor(generator-plugin-transfertypes): simplify implementation
    
    * fix(parser-jvm-core): revert changes in favor of new solution
    
    * fix: use correct package name
    
    * style(parser-jvm-transfertypes): format
    
    * fix(parser-jvm-transfertypes): address review comments
    
    * refactor(generator-utils): allow returning any value from traverse
    
    * feat(react-signals): add method options type
    
    * refactor(generator-plugin-signals): update plugin to support the changes in TransferTypes plugin
    
    * docs(generator-plugin-signals): add more documentation comments
    
    * chore: update broken dependency
    
    * fix(react-crud): update failing types
    
    * refactor(generator-plugin-signals): use switch instead of if chain
    
    * chore: update package-lock.json
    
    * chore: fix dependency versioning
    
    * refactor(parser-jvm-plugin-transfertypes): address review comments

--- packages/ts/generator-plugin-signals/src/index.ts
+++ packages/ts/generator-plugin-signals/src/index.ts
@@ -2,6 +2,7 @@ import Plugin from '@vaadin/hilla-generator-core/Plugin.js';
 import type { SharedStorage } from '@vaadin/hilla-generator-core/SharedStorage.js';
 import type { OpenAPIV3 } from 'openapi-types';
 import SignalProcessor from './SignalProcessor.js';
+import { SIGNALS } from './utils.js';
 
 // Polyfill for iterator helpers (Stage 3 proposal)
 if (!('Iterator' in globalThis)) {
@@ -9,53 +10,44 @@ if (!('Iterator' in globalThis)) {
   installIntoGlobal();
 }
 
-export type PathSignalType = Readonly<{
-  path: string;
-  signalType: string;
-}>;
+const SIGNAL_CLASSES = SIGNALS.map((signal) => `#/components/schemas/com.vaadin.hilla.runtime.transfertypes.${signal}`);
 
-const SIGNAL_CLASSES = [
-  '#/components/schemas/com.vaadin.hilla.signals.NumberSignal',
-  '#/components/schemas/com.vaadin.hilla.signals.ValueSignal',
-  '#/components/schemas/com.vaadin.hilla.signals.ListSignal',
-];
+function mapPathToSignalType(storage: SharedStorage): Map<string, string> {
+  return new Map(
+    Object.entries(storage.api.paths)
+      .filter(([_, pathObject]) => {
+        const response200 = pathObject?.post?.responses['200'];
+        return response200 && !('$ref' in response200);
+      })
+      .flatMap(([path, pathObject]) => {
+        const response200 = pathObject?.post?.responses['200'];
+        const responseSchema = (response200 as OpenAPIV3.ResponseObject).content?.['application/json']?.schema;
 
-function extractEndpointMethodsWithSignalsAsReturnType(storage: SharedStorage): PathSignalType[] {
-  return Object.entries(storage.api.paths)
-    .filter(([_, pathObject]) => {
-      const response200 = pathObject?.post?.responses['200'];
-      return response200 && !('$ref' in response200);
-    })
-    .flatMap(([path, pathObject]) => {
-      const response200 = pathObject?.post?.responses['200'];
-      const responseSchema = (response200 as OpenAPIV3.ResponseObject).content?.['application/json']?.schema;
-
-      return responseSchema && 'anyOf' in responseSchema
-        ? responseSchema.anyOf
-            ?.filter((c) => '$ref' in c && c.$ref && SIGNAL_CLASSES.includes(c.$ref))
-            .map((c: OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject) => ({
-              path,
-              signalType: '$ref' in c ? c.$ref : '',
-            }))
-        : [];
-    })
-    .filter((signalArray) => signalArray != null);
+        return responseSchema && 'anyOf' in responseSchema
+          ? responseSchema.anyOf
+              ?.filter(
+                (c): c is OpenAPIV3.ReferenceObject => '$ref' in c && !!c.$ref && SIGNAL_CLASSES.includes(c.$ref),
+              )
+              .map((c) => [path, c.$ref] as const)
+          : null;
+      })
+      .filter((signalArray) => signalArray != null),
+  );
 }
 
-function groupByService(signals: readonly PathSignalType[]): Map<string, Map<string, string>> {
-  return signals.reduce((serviceMap, signal) => {
-    const [_, service, method] = signal.path.split('/');
+function groupByService(signalMap: Map<string, string>): Map<string, Map<string, string>> {
+  return signalMap.entries().reduce((serviceMap, [path, type]) => {
+    const [_, service, method] = path.split('/');
     const serviceMethods = serviceMap.get(service) ?? new Map<string, string>();
-    serviceMethods.set(method, signal.signalType);
+    serviceMethods.set(method, type);
     serviceMap.set(service, serviceMethods);
     return serviceMap;
   }, new Map<string, Map<string, string>>());
 }
 
 export default class SignalsPlugin extends Plugin {
-  // eslint-disable-next-line @typescript-eslint/require-await
-  override async execute(sharedStorage: SharedStorage): Promise<void> {
-    const methodsWithSignals = extractEndpointMethodsWithSignalsAsReturnType(sharedStorage);
+  override execute(sharedStorage: SharedStorage): void {
+    const methodsWithSignals = mapPathToSignalType(sharedStorage);
     const services = groupByService(methodsWithSignals);
     services.forEach((methods, service) => {
       const index = sharedStorage.sources.findIndex((source) => source.fileName === `${service}.ts`);
