a4f36b5ae4f23634d9cada55da3af5dd81b8a6d5
tamasmak <maktamas@gmail.com>
2025-03-27 08:44:39 +0100

    Create logout form request

--- packages/ts/frontend/src/CsrfUtils.ts
+++ packages/ts/frontend/src/CsrfUtils.ts
@@ -18,6 +18,12 @@ function extractContentFromMetaTag(element: HTMLMetaElement | null): string | un
 }
 
 /** @internal */
+function getSpringCsrfParameterFromMetaTag(doc: Document): string | undefined {
+  const csrfParameter = doc.head.querySelector<HTMLMetaElement>('meta[name="_csrf_parameter"]');
+  return extractContentFromMetaTag(csrfParameter);
+}
+
+/** @internal */
 function getSpringCsrfHeaderFromMetaTag(doc: Document): string | undefined {
   const csrfHeader = doc.head.querySelector<HTMLMetaElement>('meta[name="_csrf_header"]');
   return extractContentFromMetaTag(csrfHeader);
@@ -31,18 +37,35 @@ function getSpringCsrfTokenFromMetaTag(doc: Document): string | undefined {
 
 /** @internal */
 export function getSpringCsrfInfo(doc: Document): Record<string, string> {
+  const csrfParameter = getSpringCsrfParameterFromMetaTag(doc);
   const csrfHeader = getSpringCsrfHeaderFromMetaTag(doc);
   let csrf = CookieManager.get(SPRING_CSRF_COOKIE_NAME);
   if (!csrf || csrf.length === 0) {
     csrf = getSpringCsrfTokenFromMetaTag(doc);
   }
   const headers: Record<string, string> = {};
-  if (csrf && csrfHeader) {
+  if (csrf && (csrfParameter || csrfHeader)) {
     headers._csrf = csrf;
-    // eslint-disable-next-line camelcase
-    headers._csrf_header = csrfHeader;
+    if (csrfParameter) {
+      // eslint-disable-next-line camelcase
+      headers._csrf_parameter = csrfParameter;
+    }
+    if (csrfHeader) {
+      // eslint-disable-next-line camelcase
+      headers._csrf_header = csrfHeader;
+    }
   }
   return headers;
+}
+
+/** @internal */
+export function getSpringCsrfTokenParametersForAuthRequest(doc: Document): Record<string, string> {
+  const csrfInfo = getSpringCsrfInfo(doc);
+  const parameters: Record<string, string> = {};
+  if (csrfInfo._csrf && csrfInfo._csrf_parameter) {
+    parameters[csrfInfo._csrf_parameter] = csrfInfo._csrf;
+  }
+  return parameters;
 }
 
 /** @internal */
