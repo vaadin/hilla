7ae0a1e215df730cae25c9c20ceb9c515c627c51
Luciano Vernaschi <luciano@vaadin.com>
2025-04-03 21:01:17 +0200

    Add tests form Lookup finder

--- packages/java/engine-core/src/main/java/com/vaadin/hilla/engine/LookupBrowserCallableFinder.java
+++ packages/java/engine-core/src/main/java/com/vaadin/hilla/engine/LookupBrowserCallableFinder.java
@@ -1,7 +1,9 @@
 package com.vaadin.hilla.engine;
 
+import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.vaadin.flow.server.ExecutionFailedException;
 
@@ -9,9 +11,43 @@ public class LookupBrowserCallableFinder {
 
     public static List<Class<?>> find(EngineConfiguration engineConfiguration)
             throws ExecutionFailedException {
-        return engineConfiguration.getEndpointAnnotations().stream()
-                .map(engineConfiguration.getClassFinder()::getAnnotatedClasses)
-                .flatMap(Set::stream).distinct().toList();
+        try {
+            var annotations = engineConfiguration.getEndpointAnnotations();
+
+            return annotations.stream()
+                    .map(engineConfiguration
+                            .getClassFinder()::getAnnotatedClasses)
+                    .flatMap(Set::stream).distinct()
+                    .collect(Collectors.toMap(
+                            clazz -> findEndpointName(annotations, clazz),
+                            clazz -> clazz, (existing, duplicate) -> {
+                                throw new IllegalStateException(
+                                        "Duplicate key found: "
+                                                + findEndpointName(annotations,
+                                                        existing));
+                            }))
+                    .values().stream().collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ExecutionFailedException(e.getMessage());
+        }
+    }
+
+    private static String findEndpointName(
+            List<Class<? extends Annotation>> annotations, Class<?> clazz) {
+        return annotations.stream()
+                .filter(annotation -> clazz.isAnnotationPresent(annotation))
+                .findAny().map(annotation -> {
+                    String name = null;
+                    try {
+                        name = (String) annotation.getMethod("value")
+                                .invoke(clazz.getAnnotation(annotation));
+                    } catch (Exception e) {
+                    }
+                    return name == null || name.isEmpty()
+                            ? clazz.getSimpleName()
+                            : name;
+                }).orElseThrow(() -> new IllegalStateException(
+                        "No endpoint annotation found for " + clazz.getName()));
     }
 
 }
