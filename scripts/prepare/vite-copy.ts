8f1849fd76d45b0f56f01e46d085f27393b6ff91
Anton Platonov <platosha@gmail.com>
2025-04-07 14:10:00 +0300

    build(scripts): use explicit check in vite-copy.ts

--- scripts/prepare/vite-copy.ts
+++ scripts/prepare/vite-copy.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { cpSync, mkdirSync } from 'fs';
+import { cpSync, mkdirSync, statSync } from 'fs';
 import process from 'node:process';
 import { pathToFileURL } from 'node:url';
 import { root, workspaceFiles } from './config';
@@ -16,13 +16,8 @@ for (const file of targetWorkspaceFiles) {
   // Make sure Vite is findable
   mkdirSync(nodeModulesDir, { recursive: true });
   const viteCopyDir = new URL('vite/', nodeModulesDir);
-  try {
-    cpSync(hoistedVitePackageDir, viteCopyDir, { recursive: true });
-  } catch (err) {
-    if (typeof err === 'object' && (err as { readonly code: string }).code === 'EEXIST') {
-      // ignore errors caused by existing installation
-    } else {
-      throw err;
-    }
+  if (statSync(viteCopyDir, { throwIfNoEntry: false })) {
+    continue;
   }
+  cpSync(hoistedVitePackageDir, viteCopyDir, { recursive: true });
 }
