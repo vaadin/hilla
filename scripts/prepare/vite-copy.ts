6566dcdd35f5abf2a9f9ad851cd7535ad014b356
Anton Platonov <platosha@gmail.com>
2025-04-07 14:28:28 +0300

    test: skip unsupported no-endpoints module (#3387)
    * test: skip unsupported no-endpoints module
    
    * build(scripts): make vite-copy.ts more reliable
    
    * build(scripts): use explicit check in vite-copy.ts

--- scripts/prepare/vite-copy.ts
+++ scripts/prepare/vite-copy.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { cp, mkdir } from 'node:fs/promises';
+import { cpSync, mkdirSync, statSync } from 'fs';
 import process from 'node:process';
 import { pathToFileURL } from 'node:url';
 import { root, workspaceFiles } from './config';
@@ -9,24 +9,15 @@ const hoistedVitePackageDir = new URL('node_modules/vite/', root);
 const cwd = pathToFileURL(process.env['INIT_CWD'] ?? process.cwd()).toString();
 const cwdPathPrefix = cwd.startsWith(root.toString()) ? decodeURIComponent(cwd.substring(root.toString().length)) : '';
 
-await Promise.all(
-  workspaceFiles
-    .filter((file) => file.startsWith(cwdPathPrefix))
-    .map(async (file) => {
-      const workspaceFile = new URL(file, root);
-
-      const nodeModulesDir = new URL('node_modules/', workspaceFile);
-      // Make sure Vite is findable
-      await mkdir(nodeModulesDir, { recursive: true });
-      const viteCopyDir = new URL('vite/', nodeModulesDir);
-      try {
-        await cp(hoistedVitePackageDir, viteCopyDir, { recursive: true });
-      } catch (err) {
-        if (typeof err === 'object' && (err as { readonly code: string }).code === 'EEXIST') {
-          // ignore errors caused by existing installation
-        } else {
-          throw err;
-        }
-      }
-    }),
-);
+const targetWorkspaceFiles = workspaceFiles.filter((file) => file.startsWith(cwdPathPrefix));
+for (const file of targetWorkspaceFiles) {
+  const workspaceFile = new URL(file, root);
+  const nodeModulesDir = new URL('node_modules/', workspaceFile);
+  // Make sure Vite is findable
+  mkdirSync(nodeModulesDir, { recursive: true });
+  const viteCopyDir = new URL('vite/', nodeModulesDir);
+  if (statSync(viteCopyDir, { throwIfNoEntry: false })) {
+    continue;
+  }
+  cpSync(hoistedVitePackageDir, viteCopyDir, { recursive: true });
+}
