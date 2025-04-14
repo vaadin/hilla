46bba3332996c92fc17661faf58e7292eb3ba7c3
Anton Platonov <platosha@gmail.com>
2025-04-14 13:05:18 +0300

    build: fix updates for root package.json (#3400)
    * build: fix updates for root package.json
    
    * chore: update package.json files
    
    * chore: fix typo
    
    * build: keep node_modules workspace cleanup for now
    
    * update package.json in grid-test
    
    * chore: regenerate package-lock.json
    
    * fix(auto-grid): add vaadin-lumo-styles dependency
    
    ---------
    
    Co-authored-by: Zhe Sun <zhe@vaadin.com>
    (cherry picked from commit f568dd51d4038819e7f736106251f3351a1e8aeb)

--- scripts/prepare/vite-copy.ts
+++ scripts/prepare/vite-copy.ts
@@ -1,32 +1,26 @@
 /* eslint-disable no-console */
-import { cp, mkdir } from 'node:fs/promises';
+import { cpSync, mkdirSync, statSync } from 'fs';
 import process from 'node:process';
 import { pathToFileURL } from 'node:url';
-import { root, workspaceFiles } from './config';
+import { glob } from 'glob';
+import { root } from './config';
 
 const hoistedVitePackageDir = new URL('node_modules/vite/', root);
 
 const cwd = pathToFileURL(process.env['INIT_CWD'] ?? process.cwd()).toString();
 const cwdPathPrefix = cwd.startsWith(root.toString()) ? decodeURIComponent(cwd.substring(root.toString().length)) : '';
 
-await Promise.all(
-  workspaceFiles
-    .filter((file) => file.startsWith(cwdPathPrefix))
-    .map(async (file) => {
-      const workspaceFile = new URL(file, root);
-
-      const nodeModulesDir = new URL('node_modules/', workspaceFile);
-      // Make sure Vite is findable
-      await mkdir(nodeModulesDir, { recursive: true });
-      const viteCopyDir = new URL('vite/', nodeModulesDir);
-      try {
-        await cp(hoistedVitePackageDir, viteCopyDir, { recursive: true });
-      } catch (err) {
-        if (typeof err === 'object' && (err as { readonly code: string }).code === 'EEXIST') {
-          // ignore errors caused by existing installation
-        } else {
-          throw err;
-        }
-      }
-    }),
-);
+const workspacesToMaintain = ['packages/java/tests/*', 'packages/java/tests/gradle/*', 'packages/java/tests/spring/*'];
+const workspaceFiles = await glob(workspacesToMaintain, { cwd: root });
+const targetWorkspaceFiles = workspaceFiles.filter((file) => file.startsWith(cwdPathPrefix));
+for (const file of targetWorkspaceFiles) {
+  const workspaceFile = new URL(file, root);
+  const nodeModulesDir = new URL('node_modules/', workspaceFile);
+  // Make sure Vite is findable
+  mkdirSync(nodeModulesDir, { recursive: true });
+  const viteCopyDir = new URL('vite/', nodeModulesDir);
+  if (statSync(viteCopyDir, { throwIfNoEntry: false })) {
+    continue;
+  }
+  cpSync(hoistedVitePackageDir, viteCopyDir, { recursive: true });
+}
