42398a18e2cb7d88e17847168a0f68c55881eb64
Anton Platonov <platosha@gmail.com>
2025-05-05 13:57:46 +0300

    build: more debug output for Gradle validation

--- scripts/prepare/vite-copy.ts
+++ scripts/prepare/vite-copy.ts
@@ -16,15 +16,22 @@ const workspacesToMaintain = [
   'packages/java/tests/spring/*'
 ].map(workspacePathPattern => `${workspacePathPattern}/package.json`);
 const workspaceFiles = await glob(workspacesToMaintain, { cwd: root });
+console.log('[vite-copy] root: ', root);
+console.log('[vite-copy] cwdPathPrefix: ', cwdPathPrefix);
+console.log('[vite-copy] workspaces: ', workspaceFiles);
 const targetWorkspaceFiles = workspaceFiles.filter((file) => file.startsWith(cwdPathPrefix));
+console.log('[vite-copy] targets: ', targetWorkspaceFiles);
 for (const file of targetWorkspaceFiles) {
   const workspaceFile = new URL(file, root);
   const nodeModulesDir = new URL('node_modules/', workspaceFile);
+  console.log('[vite-copy] processing workspace: ', workspaceFile);
   // Make sure Vite is findable
   mkdirSync(nodeModulesDir, { recursive: true });
   const viteCopyDir = new URL('vite/', nodeModulesDir);
   if (statSync(viteCopyDir, { throwIfNoEntry: false })) {
+    console.log('[vite-copy] skipping existing Vite: ', viteCopyDir);
     continue;
   }
+  console.log('[vite-copy] copy:', hoistedVitePackageDir, viteCopyDir);
   cpSync(hoistedVitePackageDir, viteCopyDir, { recursive: true });
 }
