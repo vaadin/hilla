f4a3351dfcfeeda38d474e4e8d5d80e71a0703b4
Luciano Vernaschi <luciano@vaadin.com>
2025-05-07 15:29:31 +0200

    Improve comment

--- packages/java/endpoint/src/main/java/com/vaadin/hilla/EndpointInvocationException.java
+++ packages/java/endpoint/src/main/java/com/vaadin/hilla/EndpointInvocationException.java
@@ -104,6 +104,8 @@ public abstract class EndpointInvocationException extends Exception {
 
     /**
      * Allows to specify the HTTP status code and message to return as error.
+     * While most common specialized exceptions are already provided, other can
+     * be created by extending this class.
      */
     public static abstract class EndpointHttpException
             extends EndpointInvocationException {
