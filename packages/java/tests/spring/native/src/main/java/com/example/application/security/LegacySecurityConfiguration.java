e4c25f13b5c52674ab1f695f7df0d8cec356e240
Marco Collovati <mcollovati@gmail.com>
2025-05-08 13:37:11 +0200

    chore: replace VaadinWebSecurity with VaadinSecurityConfigurer
    Replaces VaadinWebSecurity with the new VaadinSecurityConfigurer introduced
    in Flow 24.8. Legacy configuration is preserved but tightened to a specific
    Spring profile for manual and potentially automated testing (with profile
    activation).

--- packages/java/tests/spring/native/src/main/java/com/example/application/security/LegacySecurityConfiguration.java
+++ packages/java/tests/spring/native/src/main/java/com/example/application/security/LegacySecurityConfiguration.java
@@ -1,36 +1,30 @@
 package com.example.application.security;
 
-import com.vaadin.flow.spring.security.RequestUtil;
-import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.stream.Stream;
 
-import static com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin;
-
 @EnableWebSecurity
 @Configuration
-@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
-class SecurityConfiguration {
+@Profile("legacy-vaadin-web-security")
+class LegacySecurityConfiguration extends VaadinWebSecurity {
 
-    @Bean
-    SecurityFilterChain vaadinSecurityFilterChain(HttpSecurity http,
-            RequestUtil requestUtil) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(
-                authorize -> authorize
-                        .requestMatchers(new AntPathRequestMatcher(
-                                requestUtil.applyUrlMapping("/grid")))
-                        .permitAll());
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(
+                        new AntPathRequestMatcher(applyUrlMapping("/grid")))
+                .permitAll());
 
         http.authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(new AntPathRequestMatcher("/images/*.png"))
@@ -41,8 +35,8 @@ class SecurityConfiguration {
                         new AntPathRequestMatcher("/line-awesome/**/*.svg"))
                 .permitAll());
 
-        http.with(vaadin(), cfg -> cfg.loginView("login"));
-        return http.build();
+        super.configure(http);
+        setLoginView(http, "login");
     }
 
     @Bean
--- packages/java/tests/spring/security/src/main/java/com/vaadin/flow/spring/fusionsecurity/LegacySecurityConfig.java
+++ packages/java/tests/spring/security/src/main/java/com/vaadin/flow/spring/fusionsecurity/LegacySecurityConfig.java
@@ -7,13 +7,11 @@ import javax.crypto.spec.SecretKeySpec;
 import com.vaadin.flow.spring.fusionsecurity.data.UserInfo;
 import com.vaadin.flow.spring.fusionsecurity.data.UserInfoRepository;
 import com.vaadin.flow.spring.security.RequestUtil;
-import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategyConfiguration;
-import com.vaadin.flow.spring.security.stateless.VaadinStatelessSecurityConfigurer;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,22 +23,21 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jose.jws.JwsAlgorithms;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import static com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin;
 
 @EnableWebSecurity
 @Configuration
-@Import(VaadinAwareSecurityContextHolderStrategyConfiguration.class)
-@Profile("default")
-public class SecurityConfig {
+@Profile("legacy-vaadin-web-security")
+public class LegacySecurityConfig extends VaadinWebSecurity {
 
     public static String ROLE_USER = "user";
     public static String ROLE_ADMIN = "admin";
 
     @Autowired
     private UserInfoRepository userInfoRepository;
+
+    @Autowired
+    protected RequestUtil requestUtil;
 
     @Value("${springSecurityTestApp.security.stateless:false}")
     private boolean stateless;
@@ -50,42 +47,38 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    SecurityFilterChain vaadinSecurityFilterChain(HttpSecurity http, RequestUtil requestUtil)
-        throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         // Public access
-        http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers(new AntPathRequestMatcher("/public/**"))
-                .permitAll()
-                .requestMatchers(
-                    new AntPathRequestMatcher(requestUtil.applyUrlMapping("/")))
-                .permitAll().requestMatchers(
-                    new AntPathRequestMatcher(requestUtil.applyUrlMapping("/form")))
-                .permitAll()
-                .requestMatchers(
-                    new AntPathRequestMatcher(requestUtil.applyUrlMapping("/proxied-service")))
-                .permitAll();
-
+        http.authorizeHttpRequests(auth -> auth
+            .requestMatchers(new AntPathRequestMatcher("/public/**"))
+            .permitAll()
+            .requestMatchers(
+                new AntPathRequestMatcher(applyUrlMapping("/")))
+            .permitAll()
+            .requestMatchers(
+                new AntPathRequestMatcher(applyUrlMapping("/form")))
+            .permitAll()
+            .requestMatchers(new AntPathRequestMatcher(
+                applyUrlMapping("/proxied-service")))
+            .permitAll()
             // Admin only access
-            auth
-                .requestMatchers(new AntPathRequestMatcher("/admin-only/**"))
-                .hasAnyRole(ROLE_ADMIN)
-                .requestMatchers(new AntPathRequestMatcher("/error/**"))
-                .permitAll();
-        });
+            .requestMatchers(new AntPathRequestMatcher("/admin-only/**"))
+            .hasAnyRole(ROLE_ADMIN)
+            .requestMatchers(new AntPathRequestMatcher("/error/**"))
+            .permitAll());
 
-        http.with(vaadin(), cfg -> cfg.loginView("/login", requestUtil.applyUrlMapping("/")));
-        http.logout(cfg -> cfg.logoutUrl(requestUtil.applyUrlMapping("/logout")));
+        super.configure(http);
+        setLoginView(http, "/login", applyUrlMapping("/"));
+        http.logout(cfg -> cfg.logoutUrl(applyUrlMapping("/logout")));
 
         if (stateless) {
-            VaadinStatelessSecurityConfigurer.apply(http,
-                cfg -> cfg.withSecretKey(
-                    keyCfg -> keyCfg.secretKey(new SecretKeySpec(Base64.getUrlDecoder().decode(
-                        "I72kIcB1UrUQVHVUAzgweE-BLc0bF8mLv9SmrgKsQAk"),
-                        JwsAlgorithms.HS256))).issuer("statelessapp")
-            );
+            setStatelessAuthentication(http,
+                new SecretKeySpec(Base64.getUrlDecoder().decode(
+                    "I72kIcB1UrUQVHVUAzgweE-BLc0bF8mLv9SmrgKsQAk"),
+                    JwsAlgorithms.HS256),
+                "statelessapp");
         }
-        return http.build();
     }
 
     @Bean
@@ -93,18 +86,18 @@ public class SecurityConfig {
         return new InMemoryUserDetailsManager() {
             @Override
             public UserDetails loadUserByUsername(String username)
-                throws UsernameNotFoundException {
+                    throws UsernameNotFoundException {
                 UserInfo userInfo = userInfoRepository.findByUsername(username);
                 if (userInfo == null) {
                     throw new UsernameNotFoundException(
-                        "No user present with username: " + username);
+                            "No user present with username: " + username);
                 } else {
                     return new User(userInfo.getUsername(),
-                        userInfo.getEncodedPassword(),
-                        userInfo.getRoles().stream()
-                            .map(role -> new SimpleGrantedAuthority(
-                                "ROLE_" + role))
-                            .collect(Collectors.toList()));
+                            userInfo.getEncodedPassword(),
+                            userInfo.getRoles().stream()
+                                    .map(role -> new SimpleGrantedAuthority(
+                                            "ROLE_" + role))
+                                    .collect(Collectors.toList()));
                 }
             }
         };
