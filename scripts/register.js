fc76a9d6e61e93c46dec880d51366579204b4062
Anton Platonov <platosha@gmail.com>
2025-05-27 11:51:13 +0300

    fix: make client-side endpoints compile in service worker context (#3438)
    * fix: make client-side endpoints compile in service worker context
    
    Depends on: vaadin/flow#21298
    Connected to: #2791
    
    * fix(frontend): make it compile in SW again
    
    * test(endpoint): verify endpoints are importable from sw.ts

--- scripts/register.js
+++
@@ -1,7 +0,0 @@
-export function __REGISTER__(feature, vaadinObj = (window.Vaadin ??= {})) {
-  vaadinObj.registrations ??= [];
-  vaadinObj.registrations.push({
-    is: feature ? `${__NAME__}/${feature}` : __NAME__,
-    version: __VERSION__,
-  });
-}
