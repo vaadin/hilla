ac38d6056cb283dd792811b9bdf0b47342a5af0c
Luciano Vernaschi <luciano@vaadin.com>
2025-07-31 12:20:58 +0200

    Add missing import

--- packages/java/tests/spring/react-signals/src/main/java/com/vaadin/hilla/test/signals/security/LegacySecurityConfiguration.java
+++ packages/java/tests/spring/react-signals/src/main/java/com/vaadin/hilla/test/signals/security/LegacySecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
