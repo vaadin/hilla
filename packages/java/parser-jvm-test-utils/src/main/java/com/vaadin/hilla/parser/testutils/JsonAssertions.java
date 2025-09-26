3189dc22c8f37943e498289c499a4d89a46e8bc3
Artur <artur@vaadin.com>
2025-09-26 13:40:48 +0300

    chore: Use JacksonException to prepare for Jackson 3 (#4082)

--- packages/java/parser-jvm-test-utils/src/main/java/com/vaadin/hilla/parser/testutils/JsonAssertions.java
+++ packages/java/parser-jvm-test-utils/src/main/java/com/vaadin/hilla/parser/testutils/JsonAssertions.java
@@ -2,7 +2,7 @@ package com.vaadin.hilla.parser.testutils;
 
 import org.junit.jupiter.api.Assertions;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JacksonException;
 
 import com.vaadin.hilla.parser.utils.JsonPrinter;
 
@@ -10,7 +10,7 @@ public final class JsonAssertions {
     private final static JsonPrinter printer = new JsonPrinter();
 
     public static void assertEquals(Object expected, Object actual)
-            throws JsonProcessingException {
+            throws JacksonException {
         Assertions.assertEquals(printer.pretty().writeAsString(expected),
                 printer.pretty().writeAsString(actual));
     }
