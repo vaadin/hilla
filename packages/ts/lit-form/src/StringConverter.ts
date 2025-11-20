32df6550d825156537b6847d946c6e6767a7b9ea
Anton Platonov <platosha@gmail.com>
2025-11-20 09:39:01 +0200

    Update packages/ts/lit-form/src/StringConverter.ts
    Co-authored-by: Vlad Rindevich <vladrin@vaadin.com>

--- packages/ts/lit-form/src/StringConverter.ts
+++ packages/ts/lit-form/src/StringConverter.ts
@@ -1,7 +1,7 @@
 /**
  * A converter for a string representation of a value of type T.
  */
-export type StringConverter<T> = {
+export interface StringConverter<T> {
   fromString(this: void, value: string): T | undefined;
   toString(this: void, value: T | undefined): string;
 };
