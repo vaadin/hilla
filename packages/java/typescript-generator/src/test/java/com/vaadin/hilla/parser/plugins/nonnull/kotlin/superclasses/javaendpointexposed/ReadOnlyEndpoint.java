0ca67c44315c0cf6d046044e707e4580406ee065
Anton Platonov <platosha@gmail.com>
2025-12-17 17:30:00 +0200

    chore: fix java formatting

--- packages/java/typescript-generator/src/test/java/com/vaadin/hilla/parser/plugins/nonnull/kotlin/superclasses/javaendpointexposed/ReadOnlyEndpoint.java
+++ packages/java/typescript-generator/src/test/java/com/vaadin/hilla/parser/plugins/nonnull/kotlin/superclasses/javaendpointexposed/ReadOnlyEndpoint.java
@@ -15,9 +15,9 @@
  */
 package com.vaadin.hilla.parser.plugins.nonnull.kotlin.superclasses.javaendpointexposed;
 
-import com.vaadin.hilla.parser.testutils.annotations.EndpointExposed;
-
 import java.util.Optional;
+
+import com.vaadin.hilla.parser.testutils.annotations.EndpointExposed;
 
 @EndpointExposed
 public class ReadOnlyEndpoint<T, ID> extends NonEndpointImpl
