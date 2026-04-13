197ed3714524362b55ddedccee894da5d8622673
Artur Signell <artur@vaadin.com>
2026-04-13 14:49:18 +0300

    test: use stylesheet instead of theme folder in all test projects (#4978)
    * test: use stylesheet instead of theme folder in all test projects
    
    * more fixes after dropping the theme folder
    
    * fix test path resolve (#5095)
    
    * fix test path resolve
    
    * fix format
    
    ---------
    
    Co-authored-by: Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
    Co-authored-by: Zhe Sun <zhe@vaadin.com>
    Co-authored-by: Anton Platonov <platosha@gmail.com>

--- packages/java/tests/spring/react-i18n/src/main/java/com/vaadin/hilla/test/i18n/Application.java
+++ packages/java/tests/spring/react-i18n/src/main/java/com/vaadin/hilla/test/i18n/Application.java
@@ -18,11 +18,13 @@ package com.vaadin.hilla.test.i18n;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
-import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 @SpringBootApplication
-@Theme("react-i18n-test")
+@StyleSheet(Lumo.STYLESHEET)
+@StyleSheet(Lumo.UTILITY_STYLESHEET)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
