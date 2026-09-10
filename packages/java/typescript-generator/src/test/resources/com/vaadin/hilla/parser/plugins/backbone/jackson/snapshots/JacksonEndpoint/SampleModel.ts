daefd5c738429c1d8062fb58d41f333ca0415c49
Anton Platonov <platosha@gmail.com>
2026-09-10 13:41:18 +0300

    test(typescript-generator): update test fixture snapshots for suffixes removal
    Multiple test files had their identifier suffixes (`_1`) removed since the new registry properly handles uniqueness.

--- packages/java/typescript-generator/src/test/resources/com/vaadin/hilla/parser/plugins/backbone/jackson/snapshots/JacksonEndpoint/SampleModel.ts
+++ packages/java/typescript-generator/src/test/resources/com/vaadin/hilla/parser/plugins/backbone/jackson/snapshots/JacksonEndpoint/SampleModel.ts
@@ -1,31 +1,31 @@
-import { _getPropertyModel as _getPropertyModel_1, makeObjectEmptyValueCreator as makeObjectEmptyValueCreator_1, StringModel as StringModel_1 } from "@vaadin/hilla-lit-form";
-import type Sample_1 from "./Sample.js";
-import SampleParentModel_1 from "./SampleParentModel.js";
-class SampleModel<T extends Sample_1 = Sample_1> extends SampleParentModel_1<T> {
-    static override createEmptyValue = makeObjectEmptyValueCreator_1(SampleModel);
-    get publicProp(): StringModel_1 {
-        return this[_getPropertyModel_1]("publicProp", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+import { _getPropertyModel, makeObjectEmptyValueCreator, StringModel } from "@vaadin/hilla-lit-form";
+import type Sample from "./Sample.js";
+import SampleParentModel from "./SampleParentModel.js";
+class SampleModel<T extends Sample = Sample> extends SampleParentModel<T> {
+    static override createEmptyValue = makeObjectEmptyValueCreator(SampleModel);
+    get publicProp(): StringModel {
+        return this[_getPropertyModel]("publicProp", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get privateProp(): StringModel_1 {
-        return this[_getPropertyModel_1]("privateProp", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get privateProp(): StringModel {
+        return this[_getPropertyModel]("privateProp", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get privateTransientPropWithGetter(): StringModel_1 {
-        return this[_getPropertyModel_1]("privateTransientPropWithGetter", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get privateTransientPropWithGetter(): StringModel {
+        return this[_getPropertyModel]("privateTransientPropWithGetter", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get propertyGetterOnly(): StringModel_1 {
-        return this[_getPropertyModel_1]("propertyGetterOnly", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get propertyGetterOnly(): StringModel {
+        return this[_getPropertyModel]("propertyGetterOnly", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get propertyWithDifferentField(): StringModel_1 {
-        return this[_getPropertyModel_1]("propertyWithDifferentField", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get propertyWithDifferentField(): StringModel {
+        return this[_getPropertyModel]("propertyWithDifferentField", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get propertySetterOnly(): StringModel_1 {
-        return this[_getPropertyModel_1]("propertySetterOnly", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get propertySetterOnly(): StringModel {
+        return this[_getPropertyModel]("propertySetterOnly", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get renamedPublicProp0(): StringModel_1 {
-        return this[_getPropertyModel_1]("renamedPublicProp0", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get renamedPublicProp0(): StringModel {
+        return this[_getPropertyModel]("renamedPublicProp0", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
-    get renamedPrivateProp0(): StringModel_1 {
-        return this[_getPropertyModel_1]("renamedPrivateProp0", (parent, key) => new StringModel_1(parent, key, true, { meta: { javaType: "java.lang.String" } }));
+    get renamedPrivateProp0(): StringModel {
+        return this[_getPropertyModel]("renamedPrivateProp0", (parent, key) => new StringModel(parent, key, true, { meta: { javaType: "java.lang.String" } }));
     }
 }
 export default SampleModel;
