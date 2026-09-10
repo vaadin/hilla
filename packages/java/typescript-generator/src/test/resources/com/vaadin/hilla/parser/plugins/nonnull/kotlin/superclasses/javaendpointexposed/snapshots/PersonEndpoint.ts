2e7873ebec01698f60c1cbbf7a9a1a4e7560b2bd
Anton Platonov <platosha@gmail.com>
2026-09-10 16:16:33 +0300

    test(typescript-generator): update test fixtures for reserved word treatment

--- packages/java/typescript-generator/src/test/resources/com/vaadin/hilla/parser/plugins/nonnull/kotlin/superclasses/javaendpointexposed/snapshots/PersonEndpoint.ts
+++ packages/java/typescript-generator/src/test/resources/com/vaadin/hilla/parser/plugins/nonnull/kotlin/superclasses/javaendpointexposed/snapshots/PersonEndpoint.ts
@@ -2,10 +2,10 @@ import { EndpointRequestInit } from "@vaadin/hilla-frontend";
 import type Person from "./com/vaadin/hilla/parser/plugins/nonnull/kotlin/superclasses/javaendpointexposed/PersonEndpoint/Person.js";
 import client from "./connect-client.default.js";
 async function get(id: number | undefined, init?: EndpointRequestInit): Promise<Person | undefined> { return client.call("PersonEndpoint", "get", { id }, init); }
-async function delete(id: number | undefined, init?: EndpointRequestInit): Promise<void> { return client.call("PersonEndpoint", "delete", { id }, init); }
+async function delete_1(id: number | undefined, init?: EndpointRequestInit): Promise<void> { return client.call("PersonEndpoint", "delete", { id }, init); }
 async function update(entity: Person | undefined, init?: EndpointRequestInit): Promise<Person | undefined> { return client.call("PersonEndpoint", "update", { entity }, init); }
 async function getNonNullablePage(pageSize: number, pageNumber: number, parameters: Record<string, Person> | undefined, init?: EndpointRequestInit): Promise<Array<Person> | undefined> { return client.call("PersonEndpoint", "getNonNullablePage", { pageSize, pageNumber, parameters }, init); }
 async function getPage(pageSize: number, pageNumber: number, init?: EndpointRequestInit): Promise<Array<Person | undefined> | undefined> { return client.call("PersonEndpoint", "getPage", { pageSize, pageNumber }, init); }
 async function size(init?: EndpointRequestInit): Promise<number> { return client.call("PersonEndpoint", "size", {}, init); }
 async function create(entity: Person, init?: EndpointRequestInit): Promise<number> { return client.call("PersonEndpoint", "create", { entity }, init); }
-export { create, delete, get, getNonNullablePage, getPage, size, update };
+export { create, delete_1 as delete, get, getNonNullablePage, getPage, size, update };
--- packages/java/typescript-generator/src/test/resources/com/vaadin/hilla/parser/plugins/nonnull/superclassmethods/snapshots/PersonEndpoint.ts
+++ packages/java/typescript-generator/src/test/resources/com/vaadin/hilla/parser/plugins/nonnull/superclassmethods/snapshots/PersonEndpoint.ts
@@ -2,9 +2,9 @@ import { EndpointRequestInit } from "@vaadin/hilla-frontend";
 import type Person from "./com/vaadin/hilla/parser/plugins/nonnull/superclassmethods/PersonEndpoint/Person.js";
 import client from "./connect-client.default.js";
 async function get(id: number | undefined, init?: EndpointRequestInit): Promise<Person | undefined> { return client.call("PersonEndpoint", "get", { id }, init); }
-async function delete(id: number | undefined, init?: EndpointRequestInit): Promise<void> { return client.call("PersonEndpoint", "delete", { id }, init); }
+async function delete_1(id: number | undefined, init?: EndpointRequestInit): Promise<void> { return client.call("PersonEndpoint", "delete", { id }, init); }
 async function update(entity: Person | undefined, init?: EndpointRequestInit): Promise<Person | undefined> { return client.call("PersonEndpoint", "update", { entity }, init); }
 async function getNonNullablePage(pageSize: number, pageNumber: number, parameters: Record<string, Person> | undefined, init?: EndpointRequestInit): Promise<Array<Person> | undefined> { return client.call("PersonEndpoint", "getNonNullablePage", { pageSize, pageNumber, parameters }, init); }
 async function getPage(pageSize: number, pageNumber: number, init?: EndpointRequestInit): Promise<Array<Person | undefined> | undefined> { return client.call("PersonEndpoint", "getPage", { pageSize, pageNumber }, init); }
 async function size(init?: EndpointRequestInit): Promise<number> { return client.call("PersonEndpoint", "size", {}, init); }
-export { delete, get, getNonNullablePage, getPage, size, update };
+export { delete_1 as delete, get, getNonNullablePage, getPage, size, update };
