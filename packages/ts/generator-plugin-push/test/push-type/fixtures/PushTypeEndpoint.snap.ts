04dec4512c56ec52776672feefa3a9c3f3fc55e4
Anton Platonov <platosha@gmail.com>
2026-09-11 16:10:22 +0300

    fix(generator-plugin-push): track aliased endpoint init parameter type to avoid name suffixing

--- packages/ts/generator-plugin-push/test/push-type/fixtures/PushTypeEndpoint.snap.ts
+++ packages/ts/generator-plugin-push/test/push-type/fixtures/PushTypeEndpoint.snap.ts
@@ -1,8 +1,8 @@
-import { EndpointRequestInit, Subscription } from "@vaadin/hilla-frontend";
+import { EndpointRequestInit as EndpointRequestInit_1, Subscription } from "@vaadin/hilla-frontend";
 import client from "./connect-client.default.js";
 function getMessageFlux(): Subscription<string | undefined> { return client.subscribe("PushTypeEndpoint", "getMessageFlux", {}); }
 function delete_1(): Subscription<string | undefined> { return client.subscribe("PushTypeEndpoint", "delete", {}); }
 function getNonNullMessageFlux(): Subscription<string> { return client.subscribe("PushTypeEndpoint", "getNonNullMessageFlux", {}); }
 function getSubscription(): Subscription<string | undefined> { return client.subscribe("PushTypeEndpoint", "getSubscription", {}); }
-async function notRelatedToPush(arg: string, init?: EndpointRequestInit): Promise<void> { return client.call("PushTypeEndpoint", "notRelatedToPush", { arg }, init); }
+async function notRelatedToPush(arg: string, EndpointRequestInit: string, init?: EndpointRequestInit_1): Promise<void> { return client.call("PushTypeEndpoint", "notRelatedToPush", { arg, EndpointRequestInit }, init); }
 export { delete_1 as delete, getMessageFlux, getNonNullMessageFlux, getSubscription, notRelatedToPush };
